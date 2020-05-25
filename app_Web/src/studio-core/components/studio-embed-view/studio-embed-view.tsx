2d5a7250def2f9a776798b9b67485229b6a0f8bc
ibizdev <ibizdev>
2020-05-25 18:47:16 +0800

    chitanda 发布系统代码

--- app_Web/src/studio-core/components/studio-embed-view/studio-embed-view.tsx
+++ app_Web/src/studio-core/components/studio-embed-view/studio-embed-view.tsx
@@ -1,4 +1,6 @@
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core/decorators/VueLifeCycleProcessing';
+import { StudioViewBase } from '../studio-view/studio-view-base';
 import './studio-embed-view.less';
 
 /**
@@ -9,76 +11,21 @@ import './studio-embed-view.less';
  * @extends {Vue}
  */
 @Component({})
-export class StudioEmbedView extends Vue {
+@VueLifeCycleProcessing()
+export class StudioEmbedView extends StudioViewBase {
 
     /**
-     * 视图名称
-     *
-     * @type {string}
-     * @memberof StudioEmbedView
-     */
-    @Prop({ default: '' })
-    public viewName!: string;
-
-    /**
-     * 视图标题
-     *
-     * @type {string}
-     * @memberof StudioEmbedView
-     */
-    @Prop({ default: '' })
-    public viewTitle!: string;
-
-    /**
-     * 是否隐藏视图头部
+     * 计算容器样式
      *
      * @protected
-     * @type {boolean}
+     * @param {{ [str: string]: boolean }} [classNames] 样式名称对象
+     * @returns {{ [str: string]: boolean }}
      * @memberof StudioEmbedView
      */
-    @Prop({ default: false })
-    protected hiddenHeader!: boolean;
-
-    /**
-     * 是否显示视图头
-     *
-     * @protected
-     * @type {boolean}
-     * @memberof StudioEmbedView
-     */
-    protected isShowHeader: boolean = true;
-
-    /**
-     * 组件创建完毕
-     *
-     * @memberof StudioEmbedView
-     */
-    public created(): void {
-        if (this.hiddenHeader) {
-            this.isShowHeader = !this.isShowHeader;
-        } else {
-            this.isShowHeader = (this.$slots.title || this.$slots.toolbar || this.$slots.quickSearch) ? true : false;
-        }
-    }
-    
-    /**
-     * 绘制内容
-     *
-     * @returns {*}
-     * @memberof StudioEmbedView
-     */
-    public render(): any {
-        return <div class={{ 'studio-view': true, 'embed-view': true, 'view-container': true, 'hidden-header': !this.isShowHeader }}>
-            <view-config-actions viewName={this.viewName} viewTitle={this.viewTitle}/>
-            {this.isShowHeader ? <div class="view-header">
-                {this.$slots.title ? <div class="title">{this.$slots.title}</div> : null}
-                {this.$slots.toolbar ? <div class="toolbar">{this.$slots.toolbar}</div> : null}
-                {this.$slots.quickSearch ? <div class="quick-search">{this.$slots.quickSearch}</div> : null}
-            </div> : null}
-            <div class="view-content">
-                {this.$slots.default}
-            </div>
-        </div>;
+    protected getContainerClass(classNames?: { [str: string]: boolean }): { [str: string]: boolean } {
+        return super.getContainerClass({
+            'embed-view': true
+        });
     }
 
 }
