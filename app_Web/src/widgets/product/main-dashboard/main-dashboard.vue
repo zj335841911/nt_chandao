234f8bde89d6077bdfca46874aaefa1edcafdc6d
ibizdev <ibizdev>
2020-05-25 11:18:01 +0800

    chitanda 发布系统代码

--- app_Web/src/widgets/product/main-dashboard/main-dashboard.vue
+++ app_Web/src/widgets/product/main-dashboard/main-dashboard.vue
@@ -3,6 +3,7 @@ import { Component } from 'vue-property-decorator';
 import MainBase from './main-dashboard-base.vue';
 import view_dashboard_container3 from '@widgets/product/main-dashboard-viewdashboard-container3-portlet/main-dashboard-viewdashboard-container3-portlet.vue';
 import view_dashboard_container2 from '@widgets/product/main-dashboard-viewdashboard-container2-portlet/main-dashboard-viewdashboard-container2-portlet.vue';
+import view_dashboard_container1 from '@widgets/product/main-dashboard-viewdashboard-container1-portlet/main-dashboard-viewdashboard-container1-portlet.vue';
 import view_dashboard_sysportlet4 from '@widgets/action/action-history-portlet/action-history-portlet.vue';
 import view_dashboard_sysportlet2 from '@widgets/action/product-trends-timeline-portlet/product-trends-timeline-portlet.vue';
 import view_dashboard_sysportlet3 from '@widgets/product/product-info-portlet/product-info-portlet.vue';
@@ -13,6 +14,7 @@ import view_dashboard_sysportlet1 from '@widgets/product-life/get-roadmap-portle
     components: {
         view_dashboard_container3, 
         view_dashboard_container2, 
+        view_dashboard_container1, 
         view_dashboard_sysportlet4, 
         view_dashboard_sysportlet2, 
         view_dashboard_sysportlet3, 
