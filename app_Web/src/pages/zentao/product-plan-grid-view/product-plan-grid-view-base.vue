969424719ac7eb287fd978a6d007f94a74d66d4e
ibizdev <ibizdev>
2020-05-27 16:47:26 +0800

    chitanda 发布系统代码

--- app_Web/src/pages/zentao/product-plan-grid-view/product-plan-grid-view-base.vue
+++ app_Web/src/pages/zentao/product-plan-grid-view/product-plan-grid-view-base.vue
@@ -685,14 +685,28 @@ export default class ProductPlanGridViewBase extends Vue {
         }
         const parameters: any[] = [
             { pathName: 'productplans', parameterName: 'productplan' },
-            { pathName: 'maintabexp', parameterName: 'maintabexp' },
         ];
         const _this: any = this;
-        const openIndexViewTab = (data: any) => {
-            const routePath = this.$viewTool.buildUpRoutePath(this.$route, curViewParam, deResParameters, parameters, args, data);
-            this.$router.push(routePath);
+        const openDrawer = (view: any, data: any) => {
+            let container: Subject<any> = this.$appdrawer.openDrawer(view, curViewParam, data);
+            container.subscribe((result: any) => {
+                if (!result || !Object.is(result.ret, 'OK')) {
+                    return;
+                }
+                if (!xData || !(xData.refresh instanceof Function)) {
+                    return;
+                }
+                xData.refresh(result.datas);
+            });
         }
-        openIndexViewTab(data);
+        const view: any = {
+            viewname: 'product-plan-main-tab-exp', 
+            height: 0, 
+            width: 0,  
+            title: this.$t('entities.productplan.views.maintabexp.title'),
+            placement: 'DRAWER_TOP',
+        };
+        openDrawer(view, data);
     }
 
 
