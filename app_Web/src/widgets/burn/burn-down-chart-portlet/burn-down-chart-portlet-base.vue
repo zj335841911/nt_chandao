9068ca2037b577d905bca597fa573d5c9baf5387
ibizdev <ibizdev>
2020-06-13 00:26:37 +0800

    lengyuace 发布系统代码，调整项目看板部件高度

--- app_Web/src/widgets/burn/burn-down-chart-portlet/burn-down-chart-portlet-base.vue
+++ app_Web/src/widgets/burn/burn-down-chart-portlet/burn-down-chart-portlet-base.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='portlet burn-down-chart ' :style="{'height': isAdaptiveSize ? 'calc(100% - 16px)' : '300px',}">
+    <div class='portlet burn-down-chart ' :style="{'height': isAdaptiveSize ? 'calc(100% - 16px)' : '370px',}">
         <p class='portlet-title'>
             <i class=''></i>
             燃尽图
