926c084afe2f3ef0d2a5570a09831b96b01b2b4c
ibizdev <ibizdev>
2020-06-14 10:57:19 +0800

    chitanda 发布系统代码

--- app_Web/vue.config.js
+++ app_Web/vue.config.js
@@ -17,6 +17,22 @@ module.exports = {
         disableHostCheck: true,
         // proxy: "http://127.0.0.1:8080/Web",
     },
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/pages/zentao/i-biz-pms/main.ts',
+            // 模板来源
+            template: 'src/template.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'iBiz软件生产管理',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            // chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+    },
     // 多核打包
     parallel: os.cpus().length > 1,
     chainWebpack: (config) => {
