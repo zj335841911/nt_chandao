26973c2ceb1d32d5bad1c9dd6575c41a84b8fe67
ibizdev <ibizdev>
2020-06-21 11:47:29 +0800

    lengyuace 发布系统代码，调整任务模块、需求模型导航树所有模型节点上下文覆盖

--- app_Web/src/widgets/project-module/module-exp-treeview/module-exp-treeview-service.ts
+++ app_Web/src/widgets/project-module/module-exp-treeview/module-exp-treeview-service.ts
@@ -487,7 +487,8 @@ export default class ModuleExpService extends ControlService {
 
             Object.assign(treeNode, { expanded: true });
             Object.assign(treeNode, { leaf: false });
-            Object.assign(treeNode, {navigateParams: {srfparentkey:"0",n_parent_eq:"0"} });
+            Object.assign(treeNode, {navigateContext: {SRFPARENTKEY:"0"} });
+            Object.assign(treeNode, {navigateParams: {n_parent_eq:"0"} });
             Object.assign(treeNode, { nodeid: treeNode.srfkey });
             Object.assign(treeNode, { nodeid2: filter.strRealNodeId });
             list.push(treeNode);
