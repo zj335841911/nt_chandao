ff15d831f9942c5bd542a611fcfec254ed9cf167
ibizdev <ibizdev>
2020-06-22 23:02:39 +0800

    chitanda 发布系统代码，修复多语言报错

--- app_Web/src/locale/lanres/entities/history/history_zh_CN.ts
+++ app_Web/src/locale/lanres/entities/history/history_zh_CN.ts
@@ -1,10 +1,10 @@
 export default {
   fields: {
-    diff: '不同',
-    field: '字段',
-    ibiznew: '新值',
-    old: '旧值',
-    id: 'id',
-    action: '关联日志',
+    diff: "不同",
+    field: "字段",
+    ibiznew: "新值",
+    old: "旧值",
+    id: "id",
+    action: "关联日志",
   },
 };
--- app_Web/src/locale/lanres/entities/project-product/project-product_zh_CN.ts
+++ app_Web/src/locale/lanres/entities/project-product/project-product_zh_CN.ts
@@ -1,22 +1,22 @@
 export default {
   fields: {
-    id: '虚拟主键',
-    productname: '产品',
-    projectname: '项目',
-    planname: '计划名称',
-    product: '产品',
-    plan: '产品计划',
-    branch: '平台/分支',
-    project: '项目',
+    id: "虚拟主键",
+    productname: "产品",
+    projectname: "项目",
+    planname: "计划名称",
+    product: "产品",
+    plan: "产品计划",
+    branch: "平台/分支",
+    project: "项目",
   },
 	views: {
 		planlistview9: {
-			caption: '关联计划',
-      title: '项目产品列表视图',
+			caption: "关联计划",
+      		title: "项目产品列表视图",
 		},
 		listview9: {
-			caption: '关联产品',
-      title: '项目产品列表视图',
+			caption: "关联产品",
+      		title: "项目产品列表视图",
 		},
 	},
 };
