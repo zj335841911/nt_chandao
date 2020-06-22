3a35c1a94923bb4167b9b2715fb703fb518466f5
ibizdev <ibizdev>
2020-06-22 14:40:16 +0800

    JunZai 发布系统代码

--- app_Web/src/widgets/project/main-form/main-form-base.vue
+++ app_Web/src/widgets/project/main-form/main-form-base.vue
@@ -66,6 +66,7 @@
             :data="{...data, ...item}" 
             :context="context"
             :viewparams="viewparams"
+            :localParam="{srfarray: %srfarray%,products: %products%,branchs: %branchs%,plans: %plans%,}"
             :disabled="detailsModel.formitemex2.disabled"  
             tag='Product' 
             codelistType='DYNAMIC'
@@ -76,6 +77,7 @@
    :data="{...data, ...item}" 
    :context="context"
    :viewparams="viewparams"
+   :localParam="{srfarray: %srfarray%,products: %products%,branchs: %branchs%,plans: %plans%,}"
    :disabled="detailsModel.formitemex2.disabled"  
    tag='ProductBranch' 
    codelistType='DYNAMIC'
@@ -91,6 +93,7 @@
             :data="{...data, ...item}" 
             :context="context"
             :viewparams="viewparams"
+            :localParam="{srfarray: %srfarray%,products: %products%,branchs: %branchs%,plans: %plans%,}"
             :disabled="detailsModel.formitemex2.disabled"  
             tag='CurProductPlan' 
             codelistType='DYNAMIC'
