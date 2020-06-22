7905a925616777e8e091b9d733a39f54887a5ce8
ibizdev <ibizdev>
2020-06-22 12:37:23 +0800

    lengyuace 发布系统代码，修复产品主页查看所有需求行为参数错误

--- app_Web/src/uiservice/product-stats/product-stats-ui-service-base.ts
+++ app_Web/src/uiservice/product-stats/product-stats-ui-service-base.ts
@@ -376,7 +376,7 @@ export default class ProductStatsUIServiceBase extends UIService {
         let parentContext:any = {};
         let parentViewParam:any = {};
         const _this: any = actionContext;
-        Object.assign(params,{product:"%id%"});
+        Object.assign(params,{product:"%productstats%"});
         const _args: any[] = Util.deepCopy(args);
         const actionTarget: string | null = 'SINGLEKEY';
         Object.assign(context, { productstats: '%productstats%' });
