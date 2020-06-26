c9ca32becc565f51da330333f5a18f63d1dea407
ibizdev <ibizdev>
2020-06-26 13:57:37 +0800

    chitanda 发布系统代码，模板修复

--- app_Web/src/widgets/case/main-grid-suit-sub-grid/main-grid-suit-sub-grid-base.tsx
+++ app_Web/src/widgets/case/main-grid-suit-sub-grid/main-grid-suit-sub-grid-base.tsx
@@ -1,2 +1,8 @@
-!!!!模版产生代码错误:Syntax error in template "TEMPLCODE_zh_CN" in line 342, column 2:
-#if is an existing directive, but the tag is malformed.  (See FreeMarker Manual / Directive Reference.)
+!!!!模版产生代码错误:----
+Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
+----
+
+----
+FTL stack trace ("~" means nesting-related):
+	- Failed at: #if hasDEExport  [in template "TEMPLCODE_zh_CN" at line 393, column 1]
+----
