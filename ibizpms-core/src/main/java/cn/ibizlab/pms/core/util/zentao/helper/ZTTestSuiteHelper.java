eb51632eb9be60f3c03178edac68ec01da2d8094
Crimson <739979063@qq.com>
2020-06-27 21:37:08 +0800

    V0.22.18 新增禅道接口：测试Bug-新建、编辑、删除、解决、关闭、激活、确认；测试套件-新建、编辑、删除

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/zentao/helper/ZTTestSuiteHelper.java
@@ -0,0 +1,198 @@
+package cn.ibizlab.pms.core.util.zentao.helper;
+
+import cn.ibizlab.pms.core.util.zentao.bean.ZTResult;
+import cn.ibizlab.pms.core.util.zentao.constants.ZenTaoConstants;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.HttpMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 【禅道接口-TestSuit】 辅助类
+ */
+final public class ZTTestSuiteHelper {
+    // ----------
+    // 接口模块
+    // ----------
+
+    /**
+     * 接口模块名
+     */
+    private final static String MODULE_NAME = "testcase";
+
+    // ----------
+    // 参数日期格式
+    // ----------
+
+    private final static Map<String, String> PARAMS_DATEFORMAT = new HashMap<>();
+
+    // ----------
+    // 接口ACTION
+    // ----------
+
+    private final static String  ACTION_INDEX = "";
+    private final static String  ACTION_BROWSE = "browse";
+    private final static String  ACTION_CREATE = "create";
+    private final static String  ACTION_VIEW = "view";
+    private final static String  ACTION_EDIT = "edit";
+    private final static String  ACTION_DELETE = "delete";
+    private final static String  ACTION_LINKCASE = "linkCase";
+    private final static String  ACTION_UNLINKCASE = "unlinkCase";
+    private final static String  ACTION_BATCHUNLINKCASES = "batchUnlinkCases";
+
+    // ----------
+    // 接口行为HTTP方法（GET、POST）
+    // ----------
+
+    private final static HttpMethod ACTION_HTTPMETHOD_INDEX = HttpMethod.GET;
+    private final static HttpMethod ACTION_HTTPMETHOD_BROWSE = HttpMethod.GET;
+    private final static HttpMethod ACTION_HTTPMETHOD_CREATE = HttpMethod.POST;
+    private final static HttpMethod ACTION_HTTPMETHOD_VIEW = HttpMethod.GET;
+    private final static HttpMethod ACTION_HTTPMETHOD_EDIT = HttpMethod.POST;
+    private final static HttpMethod ACTION_HTTPMETHOD_DELETE = HttpMethod.GET;
+    private final static HttpMethod ACTION_HTTPMETHOD_LINKCASE = HttpMethod.POST;
+    private final static HttpMethod ACTION_HTTPMETHOD_UNLINKCASE = HttpMethod.GET;
+    private final static HttpMethod ACTION_HTTPMETHOD_BATCHUNLINKCASES = HttpMethod.POST;
+
+    // ----------
+    // 接口行为POST参数
+    // ----------
+
+    private final static Map<String, Object> ACTION_PARAMS_INDEX = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_BROWSE = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_CREATE = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_VIEW = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_EDIT = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_DELETE = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_LINKCASE = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_UNLINKCASE = new HashMap<>();
+    private final static Map<String, Object> ACTION_PARAMS_BATCHUNLINKCASES = new HashMap<>();
+
+    // ----------
+    // 接口行为URL参数
+    // ----------
+
+    private final static List<String> ACTION_URL_PARAMS_INDEX = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_BROWSE = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_CREATE = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_VIEW = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_EDIT = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_DELETE = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_LINKCASE = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_UNLINKCASE = new ArrayList<>();
+    private final static List<String> ACTION_URL_PARAMS_BATCHUNLINKCASES = new ArrayList<>();
+
+    // ----------
+    // 接口行为POST参数设置
+    // ----------
+
+    static {
+        // CREATE
+        ACTION_PARAMS_CREATE.put("name", null);
+        ACTION_PARAMS_CREATE.put("desc", null);
+        ACTION_PARAMS_CREATE.put("type", null);
+
+        // EDIT
+        ACTION_PARAMS_EDIT.put("name", null);
+        ACTION_PARAMS_EDIT.put("desc", null);
+        ACTION_PARAMS_EDIT.put("type", null);
+
+    }
+
+    // ----------
+    // 接口行为URL参数设置
+    // ----------
+
+    static {
+        // CREATE
+        ACTION_URL_PARAMS_CREATE.add("product");
+
+        // EDIT
+        ACTION_URL_PARAMS_EDIT.add("id");
+
+        // DELETE
+        ACTION_URL_PARAMS_DELETE.add("id");
+        ACTION_URL_PARAMS_DELETE.add("confirm");
+
+    }
+
+    // ----------
+    // 接口行为POST参数日期格式设置
+    // ----------
+    static {
+    }
+
+    // ----------
+    // 接口实现
+    // ----------
+
+    /**
+     * create 创建
+     *
+     * @param zentaoSid
+     * @param jo
+     * @param rst
+     * @return
+     */
+    public static boolean create(String zentaoSid, JSONObject jo, ZTResult rst) {
+        // 参数赋值
+        String moduleName = MODULE_NAME;
+        String urlExt = ZenTaoConstants.ZT_URL_EXT;
+        String actionName = ACTION_CREATE;
+        HttpMethod actionHttpMethod = ACTION_HTTPMETHOD_CREATE;
+        Map<String, Object> actionParams = ACTION_PARAMS_CREATE;
+        List<String> actionUrlParams = ACTION_URL_PARAMS_CREATE;
+        String returnUrlRegexPrev = null;
+
+        return ZenTaoHttpHelper.doZTRequest(jo, rst, zentaoSid, urlExt, actionHttpMethod, moduleName, actionName, actionUrlParams, actionParams, PARAMS_DATEFORMAT, returnUrlRegexPrev);
+    }
+
+    /**
+     * edit 编辑
+     *
+     * @param zentaoSid
+     * @param jo
+     * @param rst
+     * @return
+     */
+    public static boolean edit(String zentaoSid, JSONObject jo, ZTResult rst) {
+        // 参数赋值
+        String moduleName = MODULE_NAME;
+        String urlExt = ZenTaoConstants.ZT_URL_EXT;
+        String actionName = ACTION_EDIT;
+        HttpMethod actionHttpMethod = ACTION_HTTPMETHOD_EDIT;
+        Map<String, Object> actionParams = ACTION_PARAMS_EDIT;
+        List<String> actionUrlParams = ACTION_URL_PARAMS_EDIT;
+        String returnUrlRegexPrev = null;
+
+        return ZenTaoHttpHelper.doZTRequest(jo, rst, zentaoSid, urlExt, actionHttpMethod, moduleName, actionName, actionUrlParams, actionParams, PARAMS_DATEFORMAT, returnUrlRegexPrev);
+    }
+
+    /**
+     * delete 删除
+     *
+     * @param zentaoSid
+     * @param jo
+     * @param rst
+     * @return
+     */
+    public static boolean delete(String zentaoSid, JSONObject jo, ZTResult rst) {
+        // 参数赋值
+        String moduleName = MODULE_NAME;
+        String urlExt = ZenTaoConstants.ZT_URL_EXT;
+        String actionName = ACTION_DELETE;
+        HttpMethod actionHttpMethod = ACTION_HTTPMETHOD_DELETE;
+        Map<String, Object> actionParams = ACTION_PARAMS_DELETE;
+        List<String> actionUrlParams = ACTION_URL_PARAMS_DELETE;
+        String returnUrlRegexPrev = null;
+
+        jo.put("confirm", "yes");
+
+        return ZenTaoHttpHelper.doZTRequest(jo, rst, zentaoSid, urlExt, actionHttpMethod, moduleName, actionName, actionUrlParams, actionParams, PARAMS_DATEFORMAT, returnUrlRegexPrev);
+    }
+
+}
