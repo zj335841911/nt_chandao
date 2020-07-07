d532bf165e99bf34c00b3390158ee6285c5aca8d
ibizdev <ibizdev>
2020-07-07 17:12:16 +0800

    chitanda 发布系统代码，操作列表模板更新

--- app_Web/src/widgets/action/classify-by-type-list/classify-by-type-list-base.tsx
+++ app_Web/src/widgets/action/classify-by-type-list/classify-by-type-list-base.tsx
@@ -1,6 +1,6 @@
 import { Prop, Provide, Emit, Model } from 'vue-property-decorator';
 import { Subject, Subscription } from 'rxjs';
-import { Watch, MainControlBase } from '@/studio-core';
+import { Watch, ListControllerBase } from '@/studio-core';
 import ActionService from '@/service/action/action-service';
 import ClassifyByTypeService from './classify-by-type-list-service';
 import DEFAULTService from '@/widgets/history/default-list/default-list-service';
@@ -10,10 +10,10 @@ import DEFAULTService from '@/widgets/history/default-list/default-list-service'
  * list部件基类
  *
  * @export
- * @class MainControlBase
+ * @class ListControllerBase
  * @extends {ClassifyByTypeListBase}
  */
-export class ClassifyByTypeListBase extends MainControlBase {
+export class ClassifyByTypeListBase extends ListControllerBase {
 
     /**
      * 获取部件类型
@@ -50,6 +50,31 @@ export class ClassifyByTypeListBase extends MainControlBase {
     protected appDeName: string = 'action';
 
     /**
+     * 分页条数
+     *
+     * @type {number}
+     * @memberof ClassifyByTypeListBase
+     */
+    public limit: number = 1000;
+
+    /**
+     * 排序方向
+     *
+     * @type {string}
+     * @memberof ClassifyByTypeListBase
+     */
+    public minorSortDir: string = 'ASC';
+
+    /**
+     * 排序字段
+     *
+     * @type {string}
+     * @memberof ClassifyByTypeListBase
+     */
+    public minorSortPSDEF: string = 'date';
+
+
+    /**
      * 建构部件服务对象
      *
      * @type {DEFAULTService}
@@ -82,423 +107,5 @@ export class ClassifyByTypeListBase extends MainControlBase {
         } finally {
             return items;
         }
-    }
-
-    /**
-     * 获取多项数据
-     *
-     * @returns {any[]}
-     * @memberof ClassifyByType
-     */
-    public getDatas(): any[] {
-        return this.selections;
-    }
-
-    /**
-     * 获取单项树
-     *
-     * @returns {*}
-     * @memberof ClassifyByType
-     */
-    public getData(): any {
-        return null;
-    }
-
-    /**
-     * 是否默认选中第一条数据
-     *
-     * @type {boolean}
-     * @memberof ClassifyByType
-     */
-    @Prop({ default: false }) public isSelectFirstDefault!: boolean;
-
-    /**
-     * 显示处理提示
-     *
-     * @type {boolean}
-     * @memberof ClassifyByType
-     */
-    @Prop({ default: true }) public showBusyIndicator?: boolean;
-
-    /**
-     * 部件行为--create
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */
-    @Prop() public createAction!: string;
-
-    /**
-     * 部件行为--remove
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */
-    @Prop() public removeAction!: string;
-
-    /**
-     * 部件行为--update
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */
-    @Prop() public updateAction!: string;
-
-    /**
-     * 部件行为--fetch
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */
-    @Prop() public fetchAction!: string;
-
-    /**
-     * 当前页
-     *
-     * @type {number}
-     * @memberof Main
-     */
-    public curPage: number = 1;
-
-    /**
-     * 数据
-     *
-     * @type {any[]}
-     * @memberof ClassifyByType
-     */
-    public items: any[] = [];
-
-    /**
-     * 是否支持分页
-     *
-     * @type {boolean}
-     * @memberof ClassifyByType
-     */
-    public isEnablePagingBar: boolean = true;
-
-    /**
-     * 分页条数
-     *
-     * @type {number}
-     * @memberof ClassifyByType
-     */
-    public limit: number = 1000;
-
-    /**
-     * 总条数
-     *
-     * @type {number}
-     * @memberof ClassifyByType
-     */
-    public totalRecord: number = 0;
-
-    /**
-     * 排序方向
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */    
-    public sortDir:string = 'asc';
-
-    /**
-     * 排序字段
-     *
-     * @type {string}
-     * @memberof ClassifyByType
-     */    
-    public sortField: string = 'date';
-
-    /**
-     * 选中数组
-     * @type {Array<any>}
-     * @memberof ClassifyByType
-     */
-    public selections: Array<any> = [];
-
-     /**
-     * Vue声明周期，组件挂载完毕
-     *
-     * @memberof ClassifyByType
-     */
-    public mounted () {
-        this.afterMounted();
-    }
-
-    /**
-     * 执行mounted后的逻辑
-     *
-     * @memberof ClassifyByType
-     */
-    public afterMounted () {
-        this.$el.addEventListener('scroll', ()=> {
-            if( this.$el.scrollTop +  this.$el.clientHeight  >=  this.$el.scrollHeight) {
-                this.loadMore();
-            }
-        })
-    }
-
-    /**
-     * Vue声明周期，组件创建完毕
-     *
-     * @memberof ClassifyByType
-     */
-    public created() {
-        this.afterCreated()
-    }
-
-    /**
-     * 执行created后的逻辑
-     *
-     *  @memberof ClassifyByType
-     */    
-    public afterCreated(){
-        if (this.viewState) {
-            this.viewStateEvent = this.viewState.subscribe(({ tag, action, data }) => {
-                if (!Object.is(this.name, tag)) {
-                    return;
-                }
-                if (Object.is(action,'load')) {
-                    this.refresh(data)
-                }
-            });
-        }
-    }    
-
-    /**
-     * vue 生命周期
-     *
-     * @memberof ClassifyByType
-     */
-    public destroyed() {
-        this.afterDestroy();
-    }
-
-    /**
-     * 执行destroyed后的逻辑
-     *
-     * @memberof ClassifyByType
-     */
-    public afterDestroy() {
-        if (this.viewStateEvent) {
-            this.viewStateEvent.unsubscribe();
-        }
-    }    
-
-    /**
-    * 加载更多
-    *
-    * @memberof Mob
-    */
-    public loadMore(){
-        if(this.totalRecord>this.items.length)
-        {
-            this.curPage = ++this.curPage;
-            this.load({});
-        }
-    }
-
-    /**
-     * 刷新
-     *
-     * @param {*} [opt={}]
-     * @memberof Main
-     */
-    public refresh(opt: any = {}) {
-        this.curPage = 1;
-        this.items = [];
-        this.load(opt);
-    }
-
-    /**
-     * 列表数据加载
-     *
-     * @public
-     * @param {*} [arg={}]
-     * @memberof ClassifyByType
-     */
-    public load(opt: any = {}): void {
-        if(!this.fetchAction){
-            this.$Notice.error({ title: '错误', desc: 'ActionHistoryListView视图列表fetchAction参数未配置' });
-            return;
-        }        
-        const arg: any = {...opt};
-        const page: any = {};
-        if (this.isEnablePagingBar) {
-            Object.assign(page, { page: this.curPage-1, size: this.limit });
-        }
-        // 设置排序
-        if (!Object.is(this.sortDir, '') && !Object.is(this.sortField, '')) {
-          const sort: string = this.sortField+","+this.sortDir;
-            Object.assign(page, { sort: sort });
-        }
-        Object.assign(arg, page);
-        const parentdata: any = {};
-        this.$emit('beforeload', parentdata);
-        Object.assign(arg, parentdata);
-        let tempViewParams:any = parentdata.viewparams?parentdata.viewparams:{};
-        Object.assign(tempViewParams,JSON.parse(JSON.stringify(this.viewparams)));
-        Object.assign(arg,{viewparams:tempViewParams});
-        const post: Promise<any> = this.service.search(this.fetchAction, this.context?JSON.parse(JSON.stringify(this.context)):{}, arg, this.showBusyIndicator);
-        post.then((response: any) => {
-            if (!response || response.status !== 200) {
-                if (response.errorMessage) {
-                    this.$Notice.error({ title: '错误', desc: response.errorMessage });
-                }
-                return;
-            }
-            const data: any = response.data;
-            this.items = [];
-            if (Object.keys(data).length > 0) {
-                let datas = JSON.parse(JSON.stringify(data));
-                datas.map((item: any) => {
-                    Object.assign(item, { isselected: false });
-                });
-                this.totalRecord = response.total;
-                this.items.push(...datas);
-            }
-            this.$emit('load', this.items);
-            if(this.isSelectFirstDefault){
-                this.handleClick(this.items[0]);
-            }
-        }, (response: any) => {
-            if (response && response.status === 401) {
-                return;
-            }
-            this.$Notice.error({ title: '错误', desc: response.errorMessage });
-        });
-    }
-
-    /**
-     * 删除
-     *
-     * @param {any[]} datas
-     * @returns {Promise<any>}
-     * @memberof ClassifyByType
-     */
-    public async remove(datas: any[]): Promise<any> {
-        if(!this.removeAction){
-            this.$Notice.error({ title: '错误', desc: 'ActionHistoryListView视图表格removeAction参数未配置' });
-            return;
-        }
-        if (datas.length === 0) {
-            return;
-        }
-        let dataInfo = '';
-        datas.forEach((record: any, index: number) => {
-            let srfmajortext = record.srfmajortext;
-            if (index < 5) {
-                if (!Object.is(dataInfo, '')) {
-                    dataInfo += '、';
-                }
-                dataInfo += srfmajortext;
-            } else {
-                return false;
-            }
-        });
-
-        if (datas.length < 5) {
-            dataInfo = dataInfo + ' 共' + datas.length + '条数据';
-        } else {
-            dataInfo = dataInfo + '...' + ' 共' + datas.length + '条数据';
-        }
-
-        const removeData = () => {
-            let keys: any[] = [];
-            datas.forEach((data: any) => {
-                keys.push(data.srfkey);
-            });
-            let _removeAction = keys.length > 1 ? 'removeBatch' : this.removeAction ;
-            const context:any = JSON.parse(JSON.stringify(this.context));
-            const post: Promise<any> = this.service.delete(_removeAction,Object.assign(context,{ action: keys.join(';') }),Object.assign({ action: keys.join(';') },{viewparams:this.viewparams}), this.showBusyIndicator);
-            return new Promise((resolve: any, reject: any) => {
-                post.then((response: any) => {
-                    if (!response || response.status !== 200) {
-                        this.$Notice.error({ title: '', desc: '删除数据失败,' + response.info });
-                        return;
-                    } else {
-                        this.$Notice.success({ title: '', desc: '删除成功!' });
-                    }
-                    //删除items中已删除的项
-                    datas.forEach((data: any) => {
-                      this.items.some((item:any,index:number)=>{
-                        if(Object.is(item.srfkey,data.srfkey)){
-                          this.items.splice(index,1);
-                                return true;
-                            }
-                        });
-                    });
-                    this.$emit('remove', null);
-                    this.selections = [];
-                    resolve(response);
-                }).catch((response: any) => {
-                    if (response && response.status === 401) {
-                        return;
-                    }
-                    if (!response || !response.status || !response.data) {
-                        this.$Notice.error({ title: '错误', desc: '系统异常' });
-                        reject(response);
-                        return;
-                    }
-                    reject(response);
-                });
-            });
-        }
-
-        dataInfo = dataInfo.replace(/[null]/g, '').replace(/[undefined]/g, '').replace(/[ ]/g, '');
-        this.$Modal.confirm({
-            title: '警告',
-            content: '确认要删除 ' + dataInfo + '，删除操作将不可恢复？',
-            onOk: () => {
-                removeData();
-            },
-            onCancel: () => { }
-        });
-        return removeData;
-    }
-
-    /**
-     * 选择数据
-     * @memberof ClassifyByType
-     *
-     */
-    public handleClick(args: any) {
-        this.clearSelection();
-        args.isselected = !args.isselected;
-        this.selectchange();
-    }
-
-    /**
-     * 双击数据
-     * @memberof ClassifyByType
-     *
-     */
-    public handleDblClick(args: any) {
-        this.$emit('rowdblclick', args);
-    }
-
-    /**
-     * 触发事件
-     * @memberof ClassifyByType
-     *
-     */
-    public selectchange() {
-        this.selections = [];
-        this.items.map((item: any) => {
-            if (item.isselected) {
-                this.selections.push(item);
-            }
-        });
-        this.$emit('selectionchange', this.selections);
-    }
-
-    /**
-     * 清除当前所有选中状态
-     *
-     * @memberof ClassifyByType
-     */
-    public clearSelection(){
-        this.items.map((item: any) => {
-            Object.assign(item, { isselected: false });
-        }); 
     }
 }
