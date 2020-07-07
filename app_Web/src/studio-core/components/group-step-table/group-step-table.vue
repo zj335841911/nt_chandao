596cc21df0992435cf4ca35a8b749c1b151cc526
JunZai <819209789@qq.com>
2020-07-07 16:25:32 +0800

    update

--- app_Web/src/studio-core/components/group-step-table/group-step-table.vue
+++ app_Web/src/studio-core/components/group-step-table/group-step-table.vue
@@ -6,6 +6,7 @@
                 <template v-for="(col, index) of getCols()">
                     <th :key="index" v-if="col.show"> {{col.label}} </th>
                 </template>
+                <th v-if="isEdit" :width="100">操作</th>
             </tr>
         </thead>
         <tbody>
@@ -18,14 +19,29 @@
                         <td :key="i" v-if="col.show">
                             <div class="table-item">
                                 <div  class="table-order" v-if="item.hasOwnProperty('child_order_num') && i === 0"> {{ item.child_order_num }} </div>
-                                <div class="table-td">
-                                    <slot :item="{data: item, index: index, col: col}">
+                                <div v-if="!isEdit" class="table-td">
+                                    <slot :item="{row: item, index: index, column: col}">
                                         {{ item[col.name] }}
+                                    </slot>
+                                </div>
+                                <div v-if="isEdit" class="table-td-edit">
+                                    <slot :item="{row: item, index: index, column: col}">
+                                        <i-input class="table-edit-input" v-model="item[col.name]"></i-input>
+                                        <el-select class="table-edit-group" v-if="i === 0" size="small" clearable v-model="item[groupfield]">
+                                            
+                                        </el-select>
                                     </slot>
                                 </div>
                             </div>
                         </td>
                     </template>
+                    <td v-if="isEdit">
+                        <div class="table-action">
+                            <Icon type="md-add" />
+                            <Icon type="md-move" />
+                            <Icon type="md-close" />
+                        </div>
+                    </td>
                 </tr>
             </template>
         </tbody>
@@ -41,7 +57,7 @@ export default class GroupStepTable extends Vue {
     /**
      * 列集合
      * 
-     * @type *
+     * @type {*}
      * @memberof GroupStepTable
      */
     @Prop() cols: any;
@@ -49,7 +65,7 @@ export default class GroupStepTable extends Vue {
     /**
      * 数据集合
      * 
-     * @type *
+     * @type {*}
      * @memberof GroupStepTable
      */
     @Prop() data: any;
@@ -57,15 +73,23 @@ export default class GroupStepTable extends Vue {
     /**
      * 是否启用编辑
      * 
-     * @type *
+     * @type {boolean}
      * @memberof GroupStepTable
      */
     @Prop() isEdit?: boolean;
 
     /**
+     * 分组属性
+     * 
+     * @type {string}
+     * @memberof GroupStepTable
+     */
+    @Prop() groupfield!: string;
+
+    /**
      * 获取数据集合
      * 
-     * @type *
+     * @returns {*}
      * @memberof GroupStepTable
      */
     public getItems(datas: any[]) {
@@ -78,10 +102,10 @@ export default class GroupStepTable extends Vue {
         let num: number = 1;
         let order: number = 1; // 编号
         source.forEach((data: any) => {
-            if(Object.is(data.type.toLowerCase(), 'group')) {
+            if(Object.is(data[this.groupfield].toLowerCase(), 'group')) {
                 groupNum = order;
                 data.order_num = order++;
-            } else if(Object.is(data.type.toLowerCase(), 'item') && groupNum > 0) {
+            } else if(Object.is(data[this.groupfield].toLowerCase(), 'item') && groupNum > 0) {
                 data.child_order_num = groupNum + '.' + num++;
             } else {
                 groupNum = 0;
@@ -96,7 +120,7 @@ export default class GroupStepTable extends Vue {
     /**
      * 获取数据集合
      * 
-     * @type *
+     * @returns {*}
      * @memberof GroupStepTable
      */
     public getCols() {
@@ -133,14 +157,40 @@ export default class GroupStepTable extends Vue {
         }
         .table-item {
             display: flex;
+            .table-td {
+                padding: 0 4px;
+            }
+            .table-td-edit {
+                flex-grow: 1;
+                display: flex;
+                .table-edit-input {
+                    flex-grow: 1;
+                    .ivu-input {
+                        border-radius: 0;
+                    }
+                }
+                .table-edit-group {
+                    width: 100px;
+                    flex-shrink: 0;
+                    .el-input__inner {
+                        border-radius: 0;
+                    }
+                }
+            }
         }
         .table-order {
             height: 30px;
             width: 49px;
             text-align: center;
+            flex-shrink: 0;
         }
-        .table-td {
-            padding: 0 4px;
+        .table-action {
+            text-align: center;
+            font-size: 20px;
+            .ivu-icon {
+                margin-left: 5px;
+                cursor: pointer;
+            }
         }
     }
 }
