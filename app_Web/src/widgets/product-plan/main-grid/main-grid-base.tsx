28bd4e1029061a3a0824a30a830cdccfc1337e7e
ibizdev <ibizdev>
2020-07-07 14:10:39 +0800

    huwei_0806 发布系统代码，调整表格列宽

--- app_Web/src/widgets/product-plan/main-grid/main-grid-base.tsx
+++ app_Web/src/widgets/product-plan/main-grid/main-grid-base.tsx
@@ -181,7 +181,7 @@ export class MainGridBase extends GridControllerBase {
             label: '名称',
             langtag: 'entities.productplan.main_grid.columns.title',
             show: true,
-            util: 'STAR'
+            util: 'PX'
         },
         {
             name: 'begin',
