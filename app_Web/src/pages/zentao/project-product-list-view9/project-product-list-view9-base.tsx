7fc3e825b0956e63e012d309786b71dc80f2db4d
ibizdev <ibizdev>
2020-07-08 16:13:40 +0800

    lengyuace 发布系统代码，模板支持配置缺失提示

--- app_Web/src/pages/zentao/project-product-list-view9/project-product-list-view9-base.tsx
+++ app_Web/src/pages/zentao/project-product-list-view9/project-product-list-view9-base.tsx
@@ -1,8 +1,209 @@
-!!!!模版产生代码错误:----
-Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
-----
 
-----
-FTL stack trace ("~" means nesting-related):
-	- Failed at: ${item.warn("应用实体「" + appde.getCodeNa...  [in template "TEMPLCODE_zh_CN" at line 96, column 4]
-----
+import { Subject } from 'rxjs';
+import { ListView9Base } from '@/studio-core';
+import ProjectProductService from '@/service/project-product/project-product-service';
+import ListView9Engine from '@engine/view/list-view9-engine';
+import CodeListService from "@service/app/codelist-service";
+
+
+/**
+ * 项目产品列表视图视图基类
+ *
+ * @export
+ * @class ProjectProductListView9Base
+ * @extends {ListView9Base}
+ */
+export class ProjectProductListView9Base extends ListView9Base {
+    /**
+     * 视图对应应用实体名称
+     *
+     * @protected
+     * @type {string}
+     * @memberof ProjectProductListView9Base
+     */
+    protected appDeName: string = 'projectproduct';
+
+    /**
+     * 应用实体主键
+     *
+     * @protected
+     * @type {string}
+     * @memberof ProjectProductListView9Base
+     */
+    protected appDeKey: string = 'id';
+// 应用实体「ProjectProduct」未设置主文本属性
+
+    /**
+     * 实体服务对象
+     *
+     * @type {ProjectProductService}
+     * @memberof ProjectProductListView9Base
+     */
+    protected appEntityService: ProjectProductService = new ProjectProductService;
+
+
+    /**
+     * 计数器服务对象集合
+     *
+     * @protected
+     * @type {Array<*>}
+     * @memberof ProjectProductListView9Base
+     */    
+    protected counterServiceArray: Array<any> = [];
+
+    /**
+     * 视图模型数据
+     *
+     * @protected
+     * @type {*}
+     * @memberof ProjectProductListView9Base
+     */
+    protected model: any = {
+        srfCaption: 'entities.projectproduct.views.listview9.caption',
+        srfTitle: 'entities.projectproduct.views.listview9.title',
+        srfSubTitle: 'entities.projectproduct.views.listview9.subtitle',
+        dataInfo: ''
+    }
+
+    /**
+     * 容器模型
+     *
+     * @protected
+     * @type {*}
+     * @memberof ProjectProductListView9Base
+     */
+    protected containerModel: any = {
+        view_list: { name: 'list', type: 'LIST' },
+    };
+
+
+	/**
+     * 视图唯一标识
+     *
+     * @protected
+     * @type {string}
+     * @memberof ViewBase
+     */
+	protected viewtag: string = 'd0454a2ae9eb44aca3613b27169f3b18';
+
+
+    /**
+     * 视图引擎
+     *
+     * @public
+     * @type {Engine}
+     * @memberof ProjectProductListView9Base
+     */
+    public engine: ListView9Engine = new ListView9Engine();
+
+    /**
+     * 引擎初始化
+     *
+     * @public
+     * @memberof ProjectProductListView9Base
+     */
+    public engineInit(): void {
+        this.engine.init({
+            view: this,
+            list: this.$refs.list,
+            opendata: (args: any[],fullargs?:any[],params?: any, $event?: any, xData?: any) => {
+                this.opendata(args,fullargs, params, $event, xData);
+            },
+            newdata: (args: any[],fullargs?:any[],params?: any, $event?: any, xData?: any) => {
+                this.newdata(args,fullargs, params, $event, xData);
+            },
+            keyPSDEField: 'projectproduct',
+            isLoadDefault: true,
+        });
+    }
+
+    /**
+     * list 部件 selectionchange 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof ProjectProductListView9Base
+     */
+    public list_selectionchange($event: any, $event2?: any): void {
+        this.engine.onCtrlEvent('list', 'selectionchange', $event);
+    }
+
+    /**
+     * list 部件 beforeload 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof ProjectProductListView9Base
+     */
+    public list_beforeload($event: any, $event2?: any): void {
+        this.engine.onCtrlEvent('list', 'beforeload', $event);
+    }
+
+    /**
+     * list 部件 rowdblclick 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof ProjectProductListView9Base
+     */
+    public list_rowdblclick($event: any, $event2?: any): void {
+        this.engine.onCtrlEvent('list', 'rowdblclick', $event);
+    }
+
+    /**
+     * list 部件 remove 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof ProjectProductListView9Base
+     */
+    public list_remove($event: any, $event2?: any): void {
+        this.engine.onCtrlEvent('list', 'remove', $event);
+    }
+
+    /**
+     * list 部件 load 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof ProjectProductListView9Base
+     */
+    public list_load($event: any, $event2?: any): void {
+        this.engine.onCtrlEvent('list', 'load', $event);
+    }
+
+    /**
+     * 打开新建数据视图
+     *
+     * @param {any[]} args
+     * @param {*} [params]
+     * @param {*} [fullargs]
+     * @param {*} [$event]
+     * @param {*} [xData]
+     * @memberof ProjectProductListView9
+     */
+    public newdata(args: any[],fullargs?:any[], params?: any, $event?: any, xData?: any) {
+        let localContext:any = null;
+        let localViewParam:any =null;
+    this.$Notice.warning({ title: '错误', desc: '未指定关系视图' });
+    }
+
+
+    /**
+     * 打开编辑数据视图
+     *
+     * @param {any[]} args
+     * @param {*} [params]
+     * @param {*} [fullargs]
+     * @param {*} [$event]
+     * @param {*} [xData]
+     * @memberof ProjectProductListView9
+     */
+    public opendata(args: any[],fullargs?:any[],params?: any, $event?: any, xData?: any) {
+        let localContext:any = null;
+        let localViewParam:any =null;
+    this.$Notice.warning({ title: '错误', desc: '未指定关系视图' });
+    }
+
+
+}
