53e52953948e8a5104706a4b88c24274968f768b
ibizdev <ibizdev>
2020-07-11 15:18:59 +0800

    huwei_0806 发布系统代码，bug模块调整

--- app_Web/src/locale/lanres/entities/test-module/test-module_en_US.ts
+++ app_Web/src/locale/lanres/entities/test-module/test-module_en_US.ts
@@ -69,17 +69,15 @@ export default {
         remove: "Remove",
 		},
 	},
-	main_grid: {
+	mainrow_grid: {
 		columns: {
+			name: "名称",
+			branch: "平台",
+			short: "简称",
+			uagridcolumn1: "操作",
 		},
 		uiactions: {
-		},
-	},
-	default_searchform: {
-		details: {
-			formpage1: "常规条件", 
-		},
-		uiactions: {
+        remove: "Remove",
 		},
 	},
 	gridviewbranchtoolbar_toolbar: {
@@ -93,62 +91,6 @@ export default {
 		},
 	},
 	gridviewtoolbar_toolbar: {
-		tbitem3: {
-			caption: "New",
-			tip: "New",
-		},
-		tbitem4: {
-			caption: "Edit",
-			tip: "Edit {0}",
-		},
-		tbitem6: {
-			caption: "Copy",
-			tip: "Copy {0}",
-		},
-		tbitem7: {
-			caption: "-",
-			tip: "",
-		},
-		tbitem8: {
-			caption: "Remove",
-			tip: "Remove {0}",
-		},
-		tbitem9: {
-			caption: "-",
-			tip: "",
-		},
-		tbitem13: {
-			caption: "Export",
-			tip: "Export {0} Data To Excel",
-		},
-		tbitem10: {
-			caption: "-",
-			tip: "",
-		},
-		tbitem16: {
-			caption: "其它",
-			tip: "其它",
-		},
-		tbitem21: {
-			caption: "Export Data Model",
-			tip: "导出数据模型",
-		},
-		tbitem23: {
-			caption: "数据导入",
-			tip: "数据导入",
-		},
-		tbitem17: {
-			caption: "-",
-			tip: "",
-		},
-		tbitem19: {
-			caption: "Filter",
-			tip: "Filter",
-		},
-		tbitem18: {
-			caption: "Help",
-			tip: "Help",
-		},
 	},
 	editviewtoolbar_toolbar: {
 		tbitem3: {
