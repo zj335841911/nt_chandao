e6856f3d581c32b319adf24748731d1aec7bcfe6
ibizdev <ibizdev>
2020-07-11 15:46:24 +0800

    huwei_0806 发布系统代码，bug编辑页面调整

--- app_Web/src/codelist/bug-module.ts
+++ app_Web/src/codelist/bug-module.ts
@@ -101,7 +101,7 @@ export default class BugModule {
     public getItems(context: any={}, data: any={}, isloading?: boolean): Promise<any> {
         return new Promise((resolve, reject) => {
             data = this.handleQueryParam(data);
-            const promise: Promise<any> = this.moduleService.FetchDefault(context, data, isloading);
+            const promise: Promise<any> = this.moduleService.FetchStoryModule(context, data, isloading);
             promise.then((response: any) => {
                 if (response && response.status === 200) {
                     const data =  response.data;
--- app_Web/src/locale/lanres/entities/test-module/test-module_zh_CN.ts
+++ app_Web/src/locale/lanres/entities/test-module/test-module_zh_CN.ts
@@ -90,6 +90,14 @@ export default {
 		},
 	},
 	gridviewtoolbar_toolbar: {
+		deuiaction2: {
+			caption: "新建行",
+			tip: "新建行",
+		},
+		deuiaction3: {
+			caption: "保存行",
+			tip: "保存行",
+		},
 	},
 	editviewtoolbar_toolbar: {
 		deuiaction1: {
