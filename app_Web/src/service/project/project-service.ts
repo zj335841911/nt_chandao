6c78a9194726f0aa90c69cecc4378182764f489f
JunZai <819209789@qq.com>
2020-07-14 14:11:47 +0800

    update

--- app_Web/src/service/project/project-service.ts
+++ app_Web/src/service/project/project-service.ts
@@ -21,5 +21,38 @@ export default class ProjectService extends ProjectServiceBase {
         super(opts);
     }
 
-
+    /**
+     * UpdateProjectCycle接口方法
+     *
+     * @param {*} [context={}]
+     * @param {*} [data={}]
+     * @param {boolean} [isloading]
+     * @returns {Promise<any>}
+     * @memberof ProjectService
+     */
+    public async UpdateProjectCycle(context: any = {},data: any = {}, isloading?: boolean): Promise<any> {
+        let res: any = { status: 200, data: {} };
+        if(!data && data.begin && data.period) {
+            return res;
+        }
+        let begin: Date = new Date(data.begin);
+        let period = parseInt(data.period);
+        let days: number = 0;
+        let curWeek: number = begin.getDay();
+        begin.setDate(begin.getDate() + period);
+        for(; period > 0; period--, curWeek++) {
+            curWeek = curWeek > 6 ? (curWeek - 7) : curWeek;
+            if(curWeek > 0 && curWeek < 6) {
+                days++;
+            }
+        }
+        let year = begin.getFullYear();
+        let month = begin.getMonth() + 1;
+        let day = begin.getDate();
+        Object.assign(res.data, {
+            end: `${year}-${month < 10 ? ('0' + month) : month}-${day < 10 ? ('0' + day) : day}`,
+            days: days
+        });
+        return res;
+    }
 }
