b8c3240ee945e1021074b7b9feb3dda597df3a38
ibizdev <ibizdev>
2020-07-14 11:11:03 +0800

    huwei_0806 发布系统代码，需求描述调整

--- app_Web/src/widgets/story/story-spec-form/story-spec-form-model.ts
+++ app_Web/src/widgets/story/story-spec-form/story-spec-form-model.ts
@@ -56,9 +56,15 @@ export default class StorySpecModel {
         dataType: 'PICKUP',
       },
       {
+        name: 'formitem',
+      },
+      {
         name: 'id',
         prop: 'id',
         dataType: 'ACID',
+      },
+      {
+        name: 'formitem1',
       },
       {
         name: 'title',
