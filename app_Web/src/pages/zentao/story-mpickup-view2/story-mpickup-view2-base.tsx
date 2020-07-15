561d11b061e44131d346d9e6d91cda020d66fe46
ibizdev <ibizdev>
2020-07-15 18:26:40 +0800

    huwei_0806 发布系统代码，添加项目下属需求实体数据多项选择视图视图参数

--- app_Web/src/pages/zentao/story-mpickup-view2/story-mpickup-view2-base.tsx
+++ app_Web/src/pages/zentao/story-mpickup-view2/story-mpickup-view2-base.tsx
@@ -66,6 +66,17 @@ export class StoryMPickupView2Base extends MPickupViewBase {
      */    
     protected counterServiceArray: Array<any> = [];
 
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof StoryMPickupView2Base
+	 */
+    protected customViewParams: any = {
+        'project': { isRawValue: false, value: 'project' }
+    };
+
     /**
      * 视图模型数据
      *
