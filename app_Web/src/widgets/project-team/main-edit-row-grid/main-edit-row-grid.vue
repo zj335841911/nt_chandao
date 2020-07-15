982789000b38297c3118d98b923351c7536efaef
JunZai <819209789@qq.com>
2020-07-15 16:52:07 +0800

    项目团队行编辑保存一次性提交所有数据

--- app_Web/src/widgets/project-team/main-edit-row-grid/main-edit-row-grid.vue
+++ app_Web/src/widgets/project-team/main-edit-row-grid/main-edit-row-grid.vue
@@ -3,7 +3,8 @@
 import { Component } from 'vue-property-decorator';
 import { VueLifeCycleProcessing } from '@/studio-core';
 import { Main_EditRowGridBase } from './main-edit-row-grid-base';
- 
+
+import ControlService from '@/widgets/control-service';
 
 /**
  * grid部件
@@ -18,5 +19,42 @@ import { Main_EditRowGridBase } from './main-edit-row-grid-base';
     }
 })
 @VueLifeCycleProcessing()
-export default class Main_EditRowGrid extends Main_EditRowGridBase { }
+export default class Main_EditRowGrid extends Main_EditRowGridBase {
+
+    /**
+     * 保存行，批量保存
+     */
+    public async save() {
+        if (!await this.validateAll()) {
+            this.$Notice.error({ title: (this.$t('app.commonWords.wrong') as string), desc: (this.$t('app.commonWords.rulesException') as string),duration: 3 });
+            return [];
+        }
+        let action = "saveBatch";
+        let _context = JSON.parse(JSON.stringify(this.context));
+        let result: Array<any> = [];
+        for(const item of this.items){
+            let _item: any = JSON.parse(JSON.stringify(item));
+            if (Object.is(item.rowDataState, 'create')){
+                _item.id = null;
+            }
+            let { data: Data,context: Context } = this.service.handleRequestData(action, _context, _item, true);
+            result.push(Data);
+        }
+        const post: Promise<any> = this.appEntityService.saveBatch(_context, result, true);
+        post.then((response:any) =>{
+            if (response && response.status === 200) {
+                this.$Notice.success({ 
+                    title: (this.$t('app.commonWords.saveSuccess') as string),
+                    duration: 3
+                });
+            }
+        }).catch((error: any) =>{
+            this.$Notice.error({
+                title: (this.$t('app.commonWords.sysException') as string),
+                duration: 3
+            });
+            console.error(error);
+        })
+    }
+}
 </script>
