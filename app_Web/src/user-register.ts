fa4c004a7f7744111f6824379453543f0e6358ed
JunZai <819209789@qq.com>
2020-07-15 17:06:45 +0800

    update

--- app_Web/src/user-register.ts
+++ app_Web/src/user-register.ts
@@ -3,6 +3,7 @@ import { UserDefaultComponent } from './user-default-register';
 import { iBizProjectCore } from './ibiz-project-core';
 import CombFormItem from './ibiz-project-core/components/comb-form-item/comb-form-item.vue'
 import GroupStepTable from './studio-core/components/group-step-table/group-step-table.vue'
+import RichTextEditor from './studio-core/components/rich-text-editor/rich-text-editor.vue'
 
 // 注册Vue插件
 export const UserComponent = {
@@ -11,5 +12,6 @@ export const UserComponent = {
         Vue.use(iBizProjectCore);
         v.component('comb-form-item',CombFormItem);
         v.component('group-step-table',GroupStepTable);
+        v.component('rich-text-editor',RichTextEditor);
     }
 };
