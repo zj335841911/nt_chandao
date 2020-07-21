3bfd0ee86118128018c98343fc5f5c4c720eb570
ibizdev <ibizdev@ibizsys.net>
2020-07-21 11:50:35 +0800

    JunZai 发布系统代码

--- app_Web/src/widgets/action/classify-by-type-list/classify-by-type-list-model.ts
+++ app_Web/src/widgets/action/classify-by-type-list/classify-by-type-list-model.ts
@@ -45,6 +45,21 @@ export default class ClassifyByTypeModel {
 			{
 				name: 'id',
 			},
+			{
+				name: 'srfkey',
+				prop: 'id',
+				dataType: 'ACID',
+			},
+			{
+				name: 'srfmajortext',
+				prop: 'comment',
+				dataType: 'LONGTEXT',
+			},
+			{
+				name: 'action',
+				prop: 'id',
+				dataType: 'FONTKEY',
+			},
       {
         name:'size',
         prop:'size'
