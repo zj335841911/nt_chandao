98bf4b8b2a531f93609f1046ea708b377da28f8f
ibizdev <ibizdev@ibizsys.net>
2020-07-23 19:27:11 +0800

    cbl1157151478 发布系统代码，todo修改

--- app_Web/src/pages/zentao/todo-grid-view/todo-grid-view-base.tsx
+++ app_Web/src/pages/zentao/todo-grid-view/todo-grid-view-base.tsx
@@ -339,16 +339,27 @@ export class TodoGridViewBase extends GridViewBase {
         const deResParameters: any[] = [];
         const parameters: any[] = [
             { pathName: 'todos', parameterName: 'todo' },
-            { pathName: 'editview', parameterName: 'editview' },
         ];
         const _this: any = this;
-        const openIndexViewTab = (data: any) => {
-            const _data: any = { w: (new Date().getTime()) };
-            Object.assign(_data, data);
-            const routePath = this.$viewTool.buildUpRoutePath(this.$route, tempContext, deResParameters, parameters, args, _data);
-            this.$router.push(routePath);
+        const openPopupModal = (view: any, data: any) => {
+            let container: Subject<any> = this.$appmodal.openModal(view, tempContext, data);
+            container.subscribe((result: any) => {
+                if (!result || !Object.is(result.ret, 'OK')) {
+                    return;
+                }
+                if (!xData || !(xData.refresh instanceof Function)) {
+                    return;
+                }
+                xData.refresh(result.datas);
+            });
         }
-        openIndexViewTab(data);
+        const view: any = {
+            viewname: 'todo-edit-view', 
+            height: 0, 
+            width: 0,  
+            title: this.$t('entities.todo.views.editview.title'),
+        };
+        openPopupModal(view, data);
     }
 
 
@@ -373,14 +384,27 @@ export class TodoGridViewBase extends GridViewBase {
         const deResParameters: any[] = [];
         const parameters: any[] = [
             { pathName: 'todos', parameterName: 'todo' },
-            { pathName: 'editview', parameterName: 'editview' },
         ];
         const _this: any = this;
-        const openIndexViewTab = (data: any) => {
-            const routePath = this.$viewTool.buildUpRoutePath(this.$route, tempContext, deResParameters, parameters, args, data);
-            this.$router.push(routePath);
+        const openPopupModal = (view: any, data: any) => {
+            let container: Subject<any> = this.$appmodal.openModal(view, tempContext, data);
+            container.subscribe((result: any) => {
+                if (!result || !Object.is(result.ret, 'OK')) {
+                    return;
+                }
+                if (!xData || !(xData.refresh instanceof Function)) {
+                    return;
+                }
+                xData.refresh(result.datas);
+            });
         }
-        openIndexViewTab(data);
+        const view: any = {
+            viewname: 'todo-edit-view', 
+            height: 0, 
+            width: 0,  
+            title: this.$t('entities.todo.views.editview.title'),
+        };
+        openPopupModal(view, data);
     }
 
 
