47e99b48b2bdb962ad58dc3e52635714c773966a
ibizdev <ibizdev@ibizsys.net>
2020-07-24 15:51:25 +0800

    huwei_0806 发布系统代码，快速重构

--- app_Web/src/studio-core/decorators/index.ts
+++
@@ -1,4 +0,0 @@
-export { ServiceInjector } from './service/ServiceInjector';
-export { ServiceProvide } from './service/ServiceProvide';
-export { SingletonMode } from './SingletonMode';
-export { VueLifeCycleProcessing } from './VueLifeCycleProcessing';
