f249901d0ce4efd28be7a0c799c2e8080c43666d
ibizdev <ibizdev@ibizsys.net>
2020-07-25 22:35:18 +0800

    nancy_iBiz 发布系统代码

--- app_Web/src/uiservice/story/story-ui-service-base.ts
+++ app_Web/src/uiservice/story/story-ui-service-base.ts
@@ -512,8 +512,8 @@ export default class StoryUIServiceBase extends UIService {
         let parentContext:any = {};
         let parentViewParam:any = {};
         const _this: any = actionContext;
-        Object.assign(context,{PROJECT:"%project%",SRFPARANTKEY:"%srfparantkey%"});
-        Object.assign(params,{project:"%project%",srfparantkey:"%srfparantkey%"});
+        Object.assign(context,{PROJECT:"%project%",PRODUCT:"%product%",SRFPARANTKEY:"%srfparantkey%"});
+        Object.assign(params,{product:"%product%",project:"%project%",srfparantkey:"%srfparantkey%"});
         const _args: any[] = Util.deepCopy(args);
         const actionTarget: string | null = 'NONE';
         if(_this.context){
