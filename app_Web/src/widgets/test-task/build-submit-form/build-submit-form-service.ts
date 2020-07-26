bf8bc7b2ce0c045ddebab99bd916906ab5952511
ibizdev <ibizdev@ibizsys.net>
2020-07-26 16:21:32 +0800

    huwei_0806 发布系统代码，测试版本表单项调整

--- app_Web/src/widgets/test-task/build-submit-form/build-submit-form-service.ts
+++ app_Web/src/widgets/test-task/build-submit-form/build-submit-form-service.ts
@@ -2,6 +2,7 @@ import { Http,Util,Errorlog } from '@/utils';
 import ControlService from '@/widgets/control-service';
 import TestTaskService from '@/service/test-task/test-task-service';
 import BuildSUBMITModel from './build-submit-form-model';
+import ProductService from '@/service/product/product-service';
 import ProjectService from '@/service/project/project-service';
 import BuildService from '@/service/build/build-service';
 
@@ -42,6 +43,14 @@ export default class BuildSUBMITService extends ControlService {
         super(opts);
         this.model = new BuildSUBMITModel();
     }
+
+    /**
+     * 产品服务对象
+     *
+     * @type {ProductService}
+     * @memberof BuildSUBMITService
+     */
+    public productService: ProductService = new ProductService();
 
     /**
      * 项目服务对象
@@ -98,6 +107,9 @@ export default class BuildSUBMITService extends ControlService {
      */
     @Errorlog
     public getItems(serviceName: string, interfaceName: string, context: any = {}, data: any, isloading?: boolean): Promise<any[]> {
+        if (Object.is(serviceName, 'ProductService') && Object.is(interfaceName, 'FetchDefault')) {
+            return this.doItems(this.productService.FetchDefault(JSON.parse(JSON.stringify(context)),data, isloading), 'id', 'product');
+        }
         if (Object.is(serviceName, 'ProjectService') && Object.is(interfaceName, 'FetchDefault')) {
             return this.doItems(this.projectService.FetchDefault(JSON.parse(JSON.stringify(context)),data, isloading), 'id', 'project');
         }
