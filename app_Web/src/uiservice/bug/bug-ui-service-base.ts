053e201f0abf4b0b4c43e8c4e70793a2cd47ccb3
ibizdev <ibizdev@ibizsys.net>
2020-07-28 11:10:43 +0800

    nancy_iBiz 发布系统代码

--- app_Web/src/uiservice/bug/bug-ui-service-base.ts
+++ app_Web/src/uiservice/bug/bug-ui-service-base.ts
@@ -158,8 +158,8 @@ export default class BugUIServiceBase extends UIService {
         let parentContext:any = {};
         let parentViewParam:any = {};
         const _this: any = actionContext;
-        Object.assign(context,{BUG:"0",PROJECT:"%project%",PRODUCT:"%product%",SRFPARENTKEY:"%srfparentkey%"});
-        Object.assign(params,{product:"%product%",srfparentkey:"%srfparentkey%",project:"%project%"});
+        Object.assign(context,{BUG:"0",BUILD:"%srfparentkey%",PROJECT:"%project%",PRODUCT:"%product%"});
+        Object.assign(params,{product:"%product%",project:"%project%",build:"%srfparentkey%"});
         const _args: any[] = Util.deepCopy(args);
         const actionTarget: string | null = 'NONE';
         if(_this.context){
