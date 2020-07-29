72757920722f35f0a3d372fa8c279251a16e7c6f
ibizdev <ibizdev@ibizsys.net>
2020-07-29 11:30:21 +0800

    nancy_iBiz 发布系统代码

--- app_Web/src/widgets/bug/main-build-sub-grid/main-build-sub-grid-base.tsx
+++ app_Web/src/widgets/bug/main-build-sub-grid/main-build-sub-grid-base.tsx
@@ -51,6 +51,204 @@ export class Main_BuildSubGridBase extends GridControlBase {
     protected appDeName: string = 'bug';
 
     /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_u5f3af70_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:BugUIService  = new BugUIService();
+        curUIService.Bug_ConfirmBug(datas,contextJO, paramJO,  $event, xData,this,"Bug");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_u424511e_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:BugUIService  = new BugUIService();
+        curUIService.Bug_ResolveBug(datas,contextJO, paramJO,  $event, xData,this,"Bug");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_u13bca5a_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:BugUIService  = new BugUIService();
+        curUIService.Bug_CloseBug(datas,contextJO, paramJO,  $event, xData,this,"Bug");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_udb5e3af_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        this.Copy(datas, contextJO,paramJO,  $event, xData,this,"Bug");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_u7995dea_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        this.Edit(datas, contextJO,paramJO,  $event, xData,this,"Bug");
+    }
+
+    /**
+     * 拷贝
+     *
+     * @param {any[]} args 当前数据
+     * @param {any} contextJO 行为附加上下文
+     * @param {*} [params] 附加参数
+     * @param {*} [$event] 事件源
+     * @param {*} [xData]  执行行为所需当前部件
+     * @param {*} [actionContext]  执行行为上下文
+     * @memberof BugBuildSubGridView_New_9212Base
+     */
+    public Copy(args: any[],contextJO?:any, params?: any, $event?: any, xData?: any,actionContext?:any,srfParentDeName?:string) {
+        if (args.length === 0) {
+            return;
+        }
+        const _this: any = this;
+        if (_this.newdata && _this.newdata instanceof Function) {
+            const data: any = {};
+            if (args.length > 0) {
+                Object.assign(data, { srfsourcekey: args[0].srfkey })
+                actionContext.$store.commit('addCopyData', { srfkey: args[0].srfkey, copyData: args[0] });
+            }
+            _this.newdata([{ ...data }],[{ ...data }],params, $event, xData);
+        } else if (xData && xData.copy instanceof Function) {
+            if (args.length > 0) {
+                actionContext.$store.commit('addCopyData', { srfkey: args[0].srfkey, copyData: args[0] });
+            }
+            xData.copy(args[0].srfkey);
+        } else {
+            _this.$Notice.error({ title: '错误', desc: 'opendata 视图处理逻辑不存在，请添加!' });
+        }
+    }
+
+    /**
+     * 编辑
+     *
+     * @param {any[]} args 当前数据
+     * @param {any} contextJO 行为附加上下文
+     * @param {*} [params] 附加参数
+     * @param {*} [$event] 事件源
+     * @param {*} [xData]  执行行为所需当前部件
+     * @param {*} [actionContext]  执行行为上下文
+     * @memberof BugBuildSubGridView_New_9212Base
+     */
+    public Edit(args: any[],contextJO?:any, params?: any, $event?: any, xData?: any,actionContext?:any,srfParentDeName?:string) {
+        if (args.length === 0) {
+            return;
+        }
+        const _this: any = this;
+        if (_this.opendata && _this.opendata instanceof Function) {
+            const data: any = { };
+            if (args.length > 0) {
+                Object.assign(data, { bug: args[0].bug })
+            }
+            _this.opendata([{ ...data }], params, $event, xData);
+        } else {
+            _this.$Notice.error({ title: '错误', desc: 'opendata 视图处理逻辑不存在，请添加!' });
+        }
+    }
+
+    /**
      * 界面UI服务对象
      *
      * @type {BugUIService}
@@ -65,6 +263,11 @@ export class Main_BuildSubGridBase extends GridControlBase {
      * @memberof Main_BuildSubBase
      */  
     public ActionModel: any = {
+        ConfirmBug: { name: 'ConfirmBug',disabled: false, visabled: true,noprivdisplaymode:1,dataaccaction: 'CONFIRM', target: 'SINGLEKEY'},
+        ResolveBug: { name: 'ResolveBug',disabled: false, visabled: true,noprivdisplaymode:1,dataaccaction: 'RESOLVE', target: 'SINGLEKEY'},
+        CloseBug: { name: 'CloseBug',disabled: false, visabled: true,noprivdisplaymode:1,dataaccaction: 'CLOSE', target: 'SINGLEKEY'},
+        Copy: { name: 'Copy',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: '', target: 'SINGLEKEY'},
+        Edit: { name: 'Edit',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: '', target: 'SINGLEKEY'}
     };
 
     /**
@@ -133,24 +336,40 @@ export class Main_BuildSubGridBase extends GridControlBase {
         },
         {
             name: 'openedby',
-            label: '由谁创建',
+            label: '创建',
             langtag: 'entities.bug.main_buildsub_grid.columns.openedby',
             show: true,
             util: 'PX',
             isEnableRowEdit: false,
         },
         {
-            name: 'openeddate',
-            label: '创建日期',
-            langtag: 'entities.bug.main_buildsub_grid.columns.openeddate',
+            name: 'assignedto',
+            label: '指派',
+            langtag: 'entities.bug.main_buildsub_grid.columns.assignedto',
             show: true,
             util: 'PX',
             isEnableRowEdit: false,
         },
         {
-            name: 'assignedto',
-            label: '指派给',
-            langtag: 'entities.bug.main_buildsub_grid.columns.assignedto',
+            name: 'resolvedby',
+            label: '解决者',
+            langtag: 'entities.bug.main_buildsub_grid.columns.resolvedby',
+            show: true,
+            util: 'PX',
+            isEnableRowEdit: false,
+        },
+        {
+            name: 'resolution',
+            label: '方案',
+            langtag: 'entities.bug.main_buildsub_grid.columns.resolution',
+            show: true,
+            util: 'PX',
+            isEnableRowEdit: false,
+        },
+        {
+            name: 'uagridcolumn1',
+            label: '操作列',
+            langtag: 'entities.bug.main_buildsub_grid.columns.uagridcolumn1',
             show: true,
             util: 'PX',
             isEnableRowEdit: false,
@@ -194,8 +413,10 @@ export class Main_BuildSubGridBase extends GridControlBase {
         'title':false,
         'status':false,
         'openedby':false,
-        'openeddate':false,
         'assignedto':false,
+        'resolvedby':false,
+        'resolution':false,
+        'uagridcolumn1':false,
     };
 
     /**
@@ -253,7 +474,50 @@ export class Main_BuildSubGridBase extends GridControlBase {
                 renderMode: 'string',
                 valueSeparator: ",",
             },
+            {
+                name: 'resolvedby',
+                srfkey: 'UserRealName',
+                codelistType : 'DYNAMIC',
+                textSeparator: ',',
+                renderMode: 'string',
+                valueSeparator: ",",
+            },
+            {
+                name: 'resolution',
+                srfkey: 'Bug__resolution',
+                codelistType : 'STATIC',
+                renderMode: 'other',
+                textSeparator: '、',
+                valueSeparator: ',',
+            },
         ]);
     }
 
+
+    /**
+     * 界面行为
+     *
+     * @param {*} row
+     * @param {*} tag
+     * @param {*} $event
+     * @memberof Main_BuildSubGridBase
+     */
+	public uiAction(row: any, tag: any, $event: any): void {
+        $event.stopPropagation();
+        if(Object.is('ConfirmBug', tag)) {
+            this.grid_uagridcolumn1_u5f3af70_click(row, tag, $event);
+        }
+        if(Object.is('ResolveBug', tag)) {
+            this.grid_uagridcolumn1_u424511e_click(row, tag, $event);
+        }
+        if(Object.is('CloseBug', tag)) {
+            this.grid_uagridcolumn1_u13bca5a_click(row, tag, $event);
+        }
+        if(Object.is('Copy', tag)) {
+            this.grid_uagridcolumn1_udb5e3af_click(row, tag, $event);
+        }
+        if(Object.is('Edit', tag)) {
+            this.grid_uagridcolumn1_u7995dea_click(row, tag, $event);
+        }
+    }
 }
