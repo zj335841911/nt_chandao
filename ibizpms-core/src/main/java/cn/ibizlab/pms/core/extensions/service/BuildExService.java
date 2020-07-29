7fbac7691f7df4f4728161eb9a650fa122b50b1d
nancy <nancyofficial@163.com>
2020-07-29 11:24:38 +0800

    v0.27.64 【版本】相关功能用户拓展代码提交

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/BuildExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/BuildExService.java
@@ -1,6 +1,8 @@
 package cn.ibizlab.pms.core.extensions.service;
 
 import cn.ibizlab.pms.core.zentao.service.impl.BuildServiceImpl;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.zentao.domain.Build;
 import org.springframework.stereotype.Service;
@@ -30,7 +32,27 @@ public class BuildExService extends BuildServiceImpl {
     @Override
     @Transactional
     public Build linkStory(Build et) {
-        return super.linkStory(et);
+        //storiesid存到哪里？
+        String zentaoSid = org.springframework.util.DigestUtils.md5DigestAsHex(cn.ibizlab.pms.core.util.zentao.service.IBZUAAZTUserService.getRequestToken().getBytes());
+        cn.ibizlab.pms.core.util.zentao.bean.ZTResult rst = new cn.ibizlab.pms.core.util.zentao.bean.ZTResult();
+        JSONObject jo = (JSONObject) JSONObject.toJSON(et);
+        if(et.get("srfactionparam") != null) {
+            ArrayList<Map> list = (ArrayList) et.get("srfactionparam");
+            JSONArray jsonArray = new JSONArray();
+            for(Map map : list) {
+                if (map.get("id") != null) {
+                    jsonArray.add(map.get("id"));
+                }
+            }
+            jo.put("stories",jsonArray);
+        }
+        boolean bRst = cn.ibizlab.pms.core.util.zentao.helper.ZTBuildHelper.linkStory(zentaoSid, (JSONObject) JSONObject.toJSON(et), rst);
+        if (bRst && rst.getEtId() != null) {
+            et = this.get(rst.getEtId());
+        }
+        et.set("ztrst", rst);
+        return et;
+
     }
 }
 
