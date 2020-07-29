e28daa6fcb81fe4be4484cba277a013411bef587
ibizdev <ibizdev@ibizsys.net>
2020-07-29 21:17:39 +0800

    huwei_0806 发布系统代码，调整路线图

--- app_Web/src/pages/zentao/product-life-road-map-slist-view9/product-life-road-map-slist-view9.vue
+++ app_Web/src/pages/zentao/product-life-road-map-slist-view9/product-life-road-map-slist-view9.vue
@@ -4,7 +4,7 @@ import { Component } from 'vue-property-decorator';
 import { VueLifeCycleProcessing } from '@/studio-core';
 import { ProductLifeRoadMapSListView9Base } from './product-life-road-map-slist-view9-base';
 import view_list2 from '@widgets/product-life/get-roadmap-s-list/get-roadmap-s-list.vue';
-import view_list from '@widgets/product-life/get-roadmap-year-list/get-roadmap-year-list.vue';
+import view_list from '@widgets/product-life/get-roadmap-year-s-list/get-roadmap-year-s-list.vue';
 
 /**
  * 路线图视图
---
+++ app_Web/src/widgets/product-life/get-roadmap-year-s-list/get-roadmap-year-s-list.vue
@@ -0,0 +1,22 @@
+<template src="./get-roadmap-year-s-list.html"/>
+<script lang='tsx'>
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core';
+import { GetRoadmapYearSListBase } from './get-roadmap-year-s-list-base';
+ 
+
+/**
+ * list部件
+ *
+ * @export
+ * @class GetRoadmapYearSList
+ * @extends {GetRoadmapYearSListBase}
+ */
+@Component({
+    components: {
+         
+    }
+})
+@VueLifeCycleProcessing()
+export default class GetRoadmapYearSList extends GetRoadmapYearSListBase { }
+</script>
