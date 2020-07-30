fc5435b40856d5e928c6ce43aad890ae68a6472f
胡维 <huwei@ibizsys.net>
2020-07-30 21:34:38 +0800

    V0.27.68 去掉build的扩展类中无用的引用

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/ProductStatsExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/ProductStatsExService.java
@@ -1,11 +1,15 @@
 package cn.ibizlab.pms.core.extensions.service;
 
 import cn.ibizlab.pms.core.ibiz.service.impl.ProductStatsServiceImpl;
+import cn.ibizlab.pms.util.security.AuthenticationUser;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibiz.domain.ProductStats;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
+
+import java.math.BigInteger;
 import java.util.*;
 
 /**
@@ -30,6 +34,27 @@ public class ProductStatsExService extends ProductStatsServiceImpl {
     @Transactional
     public ProductStats getTestStats(ProductStats et) {
         return super.getTestStats(et);
+    }
+
+    @Override
+    @Transactional
+    public ProductStats get(BigInteger key) {
+        ProductStats et = getById(key);
+        if(et==null){
+            et=new ProductStats();
+            et.setId(key);
+        }
+        else{
+            // 根据当前人信息获取指派给当前人员的bug数
+            Map<String,Object> params = new HashMap<>();
+            params.put("product", et.getId());
+            params.put("curuser", AuthenticationUser.getAuthenticationUser().getLoginname());
+            List<JSONObject> list = this.select("select count(1) as total from zt_bug t where t.deleted = '0' and t.product = #{et.product} and t.assignedTo = #{et.curuser}", params);
+            if(!list.isEmpty() && list.size() > 0) {
+                et.setAssigntomebugcnt(list.get(0).getInteger("total"));
+            }
+        }
+        return et;
     }
 }
 
