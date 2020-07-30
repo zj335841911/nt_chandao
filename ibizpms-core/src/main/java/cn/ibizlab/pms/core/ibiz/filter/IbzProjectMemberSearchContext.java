6da4121d87144d9d2140b1524d498031ec6cdffc
ibizdev <ibizdev@ibizsys.net>
2020-07-30 15:58:12 +0800

    JunZai 发布系统代码

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibiz/filter/IbzProjectMemberSearchContext.java
@@ -0,0 +1,43 @@
+package cn.ibizlab.pms.core.ibiz.filter;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.alibaba.fastjson.annotation.JSONField;
+
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+
+import cn.ibizlab.pms.util.filter.QueryWrapperContext;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import cn.ibizlab.pms.core.ibiz.domain.IbzProjectMember;
+/**
+ * 关系型数据实体[IbzProjectMember] 查询条件对象
+ */
+@Slf4j
+@Data
+public class IbzProjectMemberSearchContext extends QueryWrapperContext<IbzProjectMember> {
+
+
+    /**
+	 * 启用快速搜索
+	 */
+	public void setQuery(String query)
+	{
+		 this.query=query;
+		 if(!StringUtils.isEmpty(query)){
+		 }
+	}
+}
+
+
+
