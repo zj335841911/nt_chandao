4d5dc83824358285f669e2cb8520fa831074b774
Crimson <739979063@qq.com>
2020-08-04 02:00:10 +0800

    删除异常文件

--- ibizpms-provider/ibizpms-provider-webapi/src/main/java/cn/ibizlab/pms/webapi/mapping/TaskestimateMapping.java
+++ ibizpms-provider/ibizpms-provider-webapi/src/main/java/cn/ibizlab/pms/webapi/mapping/TaskestimateMapping.java
@@ -2,14 +2,14 @@ package cn.ibizlab.pms.webapi.mapping;
 
 import org.mapstruct.*;
 import cn.ibizlab.pms.core.zentao.domain.TaskEstimate;
-import cn.ibizlab.pms.webapi.dto.TaskestimateDTO;
+import cn.ibizlab.pms.webapi.dto.TaskEstimateDTO;
 import cn.ibizlab.pms.util.domain.MappingBase;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", uses = {},implementationName="WebApiTaskestimateMapping",
+@Mapper(componentModel = "spring", uses = {},implementationName="WebApiTaskEstimateMapping",
     nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
     nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
-public interface TaskestimateMapping extends MappingBase<TaskestimateDTO, TaskEstimate> {
+public interface TaskEstimateMapping extends MappingBase<TaskEstimateDTO, TaskEstimate> {
 
 
 }
