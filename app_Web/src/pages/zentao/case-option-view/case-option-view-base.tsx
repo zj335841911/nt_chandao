265d1e2922da712edca7a956268da23ff309c3e7
ibizdev <ibizdev@ibizsys.net>
2020-08-04 17:06:06 +0800

    huwei_0806 发布系统代码，功能测试调整

--- app_Web/src/pages/zentao/case-option-view/case-option-view-base.tsx
+++ app_Web/src/pages/zentao/case-option-view/case-option-view-base.tsx
@@ -66,6 +66,28 @@ export class CaseOptionViewBase extends OptionViewBase {
      */    
     protected counterServiceArray: Array<any> = [];
 
+	/**
+	 * 自定义视图导航上下文集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof CaseOptionViewBase
+	 */
+    protected customViewNavContexts: any = {
+        'VERSION': { isRawValue: false, value: 'version' }
+    };
+
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof CaseOptionViewBase
+	 */
+    protected customViewParams: any = {
+        'version': { isRawValue: false, value: 'version' }
+    };
+
     /**
      * 视图模型数据
      *
