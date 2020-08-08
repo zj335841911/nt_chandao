8d2dd95c4e13edf67f2007026904863f3dfd4084
ibizdev <ibizdev@ibizsys.net>
2020-08-08 16:30:12 +0800

    huwei_0806 发布系统代码，快速重构

--- ibizpms-util/src/main/java/cn/ibizlab/pms/util/mapper/IBZUSERMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.ibizlab.pms.util.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import cn.ibizlab.pms.util.domain.IBZUSER;
-
-public interface IBZUSERMapper extends BaseMapper<IBZUSER>{
-
-}
