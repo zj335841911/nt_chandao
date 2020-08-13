52e4847fe140482ce7f3ded100fb955d76560870
胡维 <huwei_0315@example.com>
2020-08-13 23:31:34 +0800

    V0.27.83 产品计划根据开始时间和周期计算结束时间

--- app_Web/src/service/product-plan/product-plan-service.ts
+++ app_Web/src/service/product-plan/product-plan-service.ts
@@ -21,5 +21,33 @@ export default class ProductPlanService extends ProductPlanServiceBase {
         super(opts);
     }
 
+    /**
+     * GetEnd接口方法
+     *
+     * @param {*} [context={}]
+     * @param {*} [data={}]
+     * @param {boolean} [isloading]
+     * @returns {Promise<any>}
+     * @memberof ProductPlanServiceBase
+     */
+    public async GetPlanEnd(context: any = {},data: any = {}, isloading?: boolean): Promise<any> {
+        let res: any = { status: 200, data: {} };
+        if(!(data && data.begin && data.delta)) {
+            return res;
+        }
+        let begin: Date = new Date(data.begin);
+        let period = parseInt(data.delta);
+
+        begin.setDate(begin.getDate() + period - 1);
+
+        let year = begin.getFullYear();
+        let month = begin.getMonth() + 1;
+        let day = begin.getDate();
+        Object.assign(res.data, {
+            end: `${year}-${month < 10 ? ('0' + month) : month}-${day < 10 ? ('0' + day) : day}`
+        });
+        return res;
+    }
+
 
 }
