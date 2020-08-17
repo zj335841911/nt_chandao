6d6d8a91781f4880ee526681d7a3a36335f5f855
KK <57750083+zhangkang1314@users.noreply.github.com>
2020-08-17 17:06:15 +0800

    脏文件

--- app_Mob/src/pages/ungroup/index/page-register.ts
+++
@@ -1,5 +0,0 @@
-export const PageComponents = {
-    install(Vue: any, opt: any) {
-                Vue.component('app-portal-view', () => import('@pages/ungroup/app-portal-view/app-portal-view.vue'));
-    }
-};
