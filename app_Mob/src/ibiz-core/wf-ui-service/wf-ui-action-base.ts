b51197de58a4bd998de6fe227aa1ab05282b07e7
ibizdev <ibizdev@ibizsys.net>
2020-08-17 22:45:46 +0800

    huwei_0806 发布系统代码

--- app_Mob/src/ibiz-core/wf-ui-service/wf-ui-action-base.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 工作流界面行为服务基类
- *
- * @export
- * @class WFUIActionBase
- */
-export class WFUIActionBase {
-
-}
--- app_Mob/src/mock/index.ts
+++
@@ -1,7 +0,0 @@
-// 预置 api 接口对象
-import './appdata/appdata';
-import './codelist/codelist';
-import './login/login';
-import './upload/upload';
-
-// 实体级接口对象
