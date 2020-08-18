a12bfa370ea8b981f1e905ae6d3a3692cbee62b6
ibizdev <ibizdev@ibizsys.net>
2020-08-18 22:55:32 +0800

    huwei_0806 发布系统代码

--- app_Mob/src/widgets/bug/mob-mobmdctrl/mob-mobmdctrl-base.vue
+++ app_Mob/src/widgets/bug/mob-mobmdctrl/mob-mobmdctrl-base.vue
@@ -397,7 +397,7 @@ export default class MobBase extends Vue implements ControlInterface {
     * @type {number}
     * @memberof Mob
     */
-    public pageSize: number = 1000;
+    public pageSize: number = 20;
 
     /**
     * 总页数
