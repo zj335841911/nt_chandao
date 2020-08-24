379dc3e19f7f12cc594c982323f95b893ead02ea
ibizdev <ibizdev@ibizsys.net>
2020-08-24 10:19:35 +0800

    KK951314 发布系统代码

--- app_Mob/src/pages/ibiz/test-task-mob-mdview/test-task-mob-mdview-base.vue
+++ app_Mob/src/pages/ibiz/test-task-mob-mdview/test-task-mob-mdview-base.vue
@@ -1,8 +1,718 @@
-!!!!模版产生代码错误:----
-Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
-----
+<template>
+<ion-page :className="{ 'view-container': true, 'default-mode-view': true, 'demobmdview': true, 'test-task-mob-mdview': true }">
+    
+    <ion-header>
+        <ion-toolbar>
+            <ion-searchbar style="height: 36px; padding-bottom: 0px;" :placeholder="$t('app.fastsearch')" debounce="500" @ionChange="quickValueChange($event)" show-cancel-button="focus" :cancel-button-text="$t('app.button.cancel')"></ion-searchbar>
+        </ion-toolbar>
+    </ion-header>
 
-----
-FTL stack trace ("~" means nesting-related):
-	- Failed at: #if ctrl.getPSControls?? && ctrl.getP...  [in template "TEMPLCODE_en_US" at line 789, column 5]
-----
+
+    <ion-content>
+                <view_mdctrl
+            :viewState="viewState"
+            viewName="TestTaskMobMDView"  
+            :viewparams="viewparams" 
+            :context="context" 
+            :showBusyIndicator="true" 
+            viewType="DEMOBMDVIEW"
+            controlStyle="LISTVIEW"
+            updateAction="Update"
+            removeAction="Remove"
+            loaddraftAction=""
+            loadAction="Get"
+            createAction="Create"
+            fetchAction="FetchDefault" 
+            :isMutli="!isSingleSelect"
+            :showCheack="showCheack"
+            @showCheackChange="showCheackChange"
+            :isTempMode="false"
+            name="mdctrl"  
+            ref='mdctrl' 
+            @selectionchange="mdctrl_selectionchange($event)"  
+            @beforeload="mdctrl_beforeload($event)"  
+            @rowclick="mdctrl_rowclick($event)"  
+            @load="mdctrl_load($event)"  
+            @closeview="closeView($event)">
+        </view_mdctrl>
+    </ion-content>
+    <ion-footer class="view-footer" style="z-index:9;">
+        
+    </ion-footer>
+</ion-page>
+</template>
+
+<script lang='ts'>
+import { Vue, Component, Prop, Provide, Emit, Watch } from 'vue-property-decorator';
+import { Subject } from 'rxjs';
+import GlobalUiService from '@/global-ui-service/global-ui-service';
+import TestTaskService from '@/app-core/service/test-task/test-task-service';
+
+import MobMDViewEngine from '@engine/view/mob-mdview-engine';
+
+
+@Component({
+    components: {
+    },
+})
+export default class TestTaskMobMDViewBase extends Vue {
+
+    /**
+     * 全局 ui 服务
+     *
+     * @type {GlobalUiService}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected globaluiservice: GlobalUiService = new GlobalUiService();
+
+    /**
+     * 实体服务对象
+     *
+     * @type {TestTaskService}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected appEntityService: TestTaskService = new TestTaskService();
+
+    /**
+     * 数据变化
+     *
+     * @param {*} val
+     * @returns {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Emit() 
+    protected viewDatasChange(val: any):any {
+        return val;
+    }
+
+    /**
+     * 视图上下文
+     *
+     * @type {string}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Prop() protected _context!: string;
+
+    /**
+     * 视图参数
+     *
+     * @type {string}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Prop() protected _viewparams!: string;
+
+    /**
+     * 视图默认使用
+     *
+     * @type {boolean}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Prop({ default: "routerView" }) protected viewDefaultUsage!: string;
+
+	/**
+	 * 视图标识
+	 *
+	 * @type {string}
+	 * @memberof TestTaskMobMDViewBase
+	 */
+	protected viewtag: string = 'e108e519b282ce8c3a588d9692b0863f';
+
+    /**
+     * 视图上下文
+     *
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected context: any = {};
+
+    /**
+     * 视图参数
+     *
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected viewparams: any = {};
+
+    /**
+     * 视图导航上下文
+     *
+     * @protected
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected navContext: any = {};
+
+    /**
+     * 视图导航参数
+     *
+     * @protected
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected navParam: any = {};
+
+    /**
+     * 视图模型数据
+     *
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected model: any = {
+        srfTitle: '测试版本移动端多数据视图',
+        srfCaption: 'testtask.views.mobmdview.caption',
+        srfSubCaption: '',
+        dataInfo: '',
+        iconcls: '',
+        icon: ''
+    }
+
+    /**
+     * 视图上下文变化
+     *
+     * @param {string} newVal
+     * @param {string} oldVal
+     * @memberof  TestTaskMobMDViewBase
+     */
+    @Watch('_context')
+    on_context(newVal: string, oldVal: string) {
+        const _this: any = this;
+        this.parseViewParam();
+        if (!Object.is(newVal, oldVal) && _this.engine) {
+            this.$nextTick(()=>{_this.engine.load();})
+        }
+        
+    }
+
+    /**
+     * 视图参数变化
+     *
+     * @param {string} newVal
+     * @param {string} oldVal
+     * @returns
+     * @memberof IBizChart
+     */
+    @Watch('_viewparams')
+    on_viewparams(newVal: string, oldVal: string) {
+        this.parseViewParam();
+    }
+
+    /**
+     * 容器模型
+     *
+     * @type {*}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected containerModel: any = {
+        view_mdctrl: { name: 'mdctrl', type: 'MOBMDCTRL' },
+        view_righttoolbar: { name: 'righttoolbar', type: 'TOOLBAR' },
+    };
+
+    /**
+     * 视图状态订阅对象
+     *
+     * @type {Subject<{action: string, data: any}>}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected viewState: Subject<ViewState> = new Subject();
+
+
+    /**
+     * 是否显示标题
+     *
+     * @type {string}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Prop({default:true}) protected showTitle?: boolean;
+
+
+
+   /**
+    * 工具栏 TestTaskMobMDView 模型
+    *
+    * @type {*}
+    * @memberof TestTaskMobMDView
+    */
+    public righttoolbarModels: any = {
+    };
+
+
+    /**
+     * 解析视图参数
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected parseViewParam(): void {
+        const { context, param } = this.$viewTool.formatNavigateViewParam(this, true);
+        this.context = { ...context };
+        this.viewparams = { ...param }
+    }
+
+    /**
+     * 是否显示返回按钮
+     *
+     * @readonly
+     * @type {boolean}
+     * @memberof TestTaskMobMDViewBase
+     */
+    get isShowBackButton(): boolean {
+        // 存在路由，非路由使用，嵌入
+        if (this.viewDefaultUsage === "indexView") {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 视图引擎
+     *
+     * @type {Engine}
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected engine: MobMDViewEngine = new MobMDViewEngine();
+
+    /**
+     * 引擎初始化
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected engineInit(): void {
+        this.engine.init({
+            view: this,
+            opendata: (args: any[], contextJO: any = {}, paramJO: any = {}, $event?: any, xData?: any, container?: any, srfParentDeName?: string) => {
+                this.opendata(args, contextJO, paramJO, $event, xData, container, srfParentDeName);
+            },
+            mdctrl: this.$refs.mdctrl,
+            newdata: (args: any[], contextJO: any = {}, paramJO: any = {}, $event?: any, xData?: any, container?: any, srfParentDeName?: string) => {
+                this.newdata(args, contextJO, paramJO, $event, xData, container, srfParentDeName);
+            },
+            keyPSDEField: 'testtask',
+            majorPSDEField: 'name',
+            isLoadDefault: true,
+        });
+    }
+
+    /**
+     * Vue声明周期
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected created() {
+        this.afterCreated();
+    }
+
+    /**
+     * Vue声明周期
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public activated() {
+        this.afterMounted();
+    }
+
+    /**
+     * 执行created后的逻辑
+     *
+     * @memberof TestTaskMobMDViewBase
+     */    
+    protected afterCreated(){
+        const secondtag = this.$util.createUUID();
+        this.$store.commit('viewaction/createdView', { viewtag: this.viewtag, secondtag: secondtag });
+        this.viewtag = secondtag;
+        this.parseViewParam();
+
+    }
+
+    /**
+     * 销毁之前
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected beforeDestroy() {
+        this.$store.commit('viewaction/removeView', this.viewtag);
+    }
+
+    /**
+     * Vue声明周期(组件初始化完毕)
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected mounted() {
+        this.afterMounted();
+    }
+
+    /**
+     * 执行mounted后的逻辑
+     * 
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected afterMounted(){
+        const _this: any = this;
+        _this.engineInit();
+        if (_this.loadModel && _this.loadModel instanceof Function) {
+            _this.loadModel();
+        }
+
+    }
+
+    /**
+     * 销毁视图回调
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected destroyed(){
+        this.afterDestroyed();
+    }
+
+    /**
+     * 执行destroyed后的逻辑
+     * 
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected afterDestroyed(){
+        if (this.viewDefaultUsage !== "indexView" && Object.keys(localStorage).length > 0) {
+            Object.keys(localStorage).forEach((item: string) => {
+                if (item.startsWith(this.context.srfsessionid)) {
+                    localStorage.removeItem(item);
+                }
+            });
+        }
+
+    }
+
+    /**
+     * mdctrl 部件 selectionchange 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected mdctrl_selectionchange($event: any, $event2?: any) {
+        this.engine.onCtrlEvent('mdctrl', 'selectionchange', $event);
+    }
+
+    /**
+     * mdctrl 部件 beforeload 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected mdctrl_beforeload($event: any, $event2?: any) {
+        this.engine.onCtrlEvent('mdctrl', 'beforeload', $event);
+    }
+
+    /**
+     * mdctrl 部件 rowclick 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected mdctrl_rowclick($event: any, $event2?: any) {
+        this.engine.onCtrlEvent('mdctrl', 'rowclick', $event);
+    }
+
+    /**
+     * mdctrl 部件 load 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected mdctrl_load($event: any, $event2?: any) {
+        this.engine.onCtrlEvent('mdctrl', 'load', $event);
+    }
+
+
+    /**
+     * 打开新建数据视图
+     *
+     * @param {any[]} args
+     * @param {*} [contextJO={}]
+     * @param {*} [paramJO={}]
+     * @param {*} [$event]
+     * @param {*} [xData]
+     * @param {*} [container]
+     * @param {string} [srfParentDeName]
+     * @returns {Promise<any>}
+     * @memberof TestTaskMobMDView
+     */
+    public async newdata(args: any[], contextJO: any = {}, paramJO: any = {}, $event?: any, xData?: any, container?: any, srfParentDeName?: string): Promise<any> {
+        const params: any = { ...paramJO };
+        let context = { ...this.context, ...contextJO };
+        if (args.length > 0) {
+            Object.assign(context, args[0]);
+        }
+        let response: any = null;
+        let panelNavParam = { } ;
+        let panelNavContext = { } ;
+        //导航参数处理
+        const { context: _context, param: _params } = this.$viewTool.formatNavigateParam( panelNavContext, panelNavParam, context, params, {});
+        let deResParameters: any[] = [];
+        if (context.product && true) {
+            deResParameters = [
+            { pathName: 'products', parameterName: 'product' },
+            ]
+        }
+        const parameters: any[] = [
+            { pathName: 'testtasks', parameterName: 'testtask' },
+            { pathName: 'mobeditview', parameterName: 'mobeditview' },
+        ];
+        const routeParam: any = this.globaluiservice.openService.formatRouteParam(_context, deResParameters, parameters, args, _params);
+        response = await this.globaluiservice.openService.openView(routeParam);
+        if (response) {
+            if (!response || !Object.is(response.ret, 'OK')) {
+                return;
+            }
+            if (!xData || !(xData.refresh instanceof Function)) {
+                return;
+            }
+            xData.refresh(response.datas);
+        }
+    }
+
+
+    /**
+     * 打开编辑数据视图
+     *
+     * @param {any[]} args
+     * @param {*} [contextJO={}]
+     * @param {*} [paramJO={}]
+     * @param {*} [$event]
+     * @param {*} [xData]
+     * @param {*} [container]
+     * @param {string} [srfParentDeName]
+     * @returns {Promise<any>}
+     * @memberof TestTaskMobMDView
+     */
+    public async opendata(args: any[], contextJO: any = {}, paramJO: any = {}, $event?: any, xData?: any, container?: any, srfParentDeName?: string): Promise<any> {
+        const params: any = { ...paramJO };
+        let context = { ...this.context, ...contextJO };
+        if (args.length > 0) {
+            Object.assign(context, args[0]);
+        }
+        let response: any = null;
+        let panelNavParam = { } ;
+        let panelNavContext = { } ;
+        //导航参数处理
+        const { context: _context, param: _params } = this.$viewTool.formatNavigateParam( panelNavContext, panelNavParam, context, params, {});
+        let deResParameters: any[] = [];
+        if (context.product && true) {
+            deResParameters = [
+            { pathName: 'products', parameterName: 'product' },
+            ]
+        }
+        const parameters: any[] = [
+            { pathName: 'testtasks', parameterName: 'testtask' },
+            { pathName: 'mobeditview', parameterName: 'mobeditview' },
+        ];
+        const routeParam: any = this.globaluiservice.openService.formatRouteParam(_context, deResParameters, parameters, args, _params);
+        response = await this.globaluiservice.openService.openView(routeParam);
+        if (response) {
+            if (!response || !Object.is(response.ret, 'OK')) {
+                return;
+            }
+            if (!xData || !(xData.refresh instanceof Function)) {
+                return;
+            }
+            xData.refresh(response.datas);
+        }
+    }
+
+
+    /**
+     * 关闭视图
+     *
+     * @param {any[]} args
+     * @memberof TestTaskMobMDViewBase
+     */
+    protected async closeView(args: any[]): Promise<any> {
+        if (this.viewDefaultUsage === "routerView" ) {
+            this.$store.commit("deletePage", this.$route.fullPath);
+            this.$router.go(-1);
+        } else {
+            this.$emit("close", { status: "success", action: "close", data: args instanceof MouseEvent ? null : args });
+        }
+        
+    }
+
+    /**
+     * 刷新数据
+     *
+     * @readonly
+     * @type {(number | null)}
+     * @memberof TestTaskMobMDViewBase
+     */
+    get refreshdata(): number | null {
+        return this.$store.getters['viewaction/getRefreshData'](this.viewtag);
+    }
+
+    /**
+     * 监控数据变化
+     *
+     * @param {*} newVal
+     * @param {*} oldVal
+     * @returns
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Watch('refreshdata')
+    onRefreshData(newVal: any, oldVal: any) {
+        if (newVal === null || newVal === undefined) {
+            return;
+        }
+        if (newVal === 0) {
+            return;
+        }
+        const _this: any = this;
+        if (_this.onRefreshView && _this.onRefreshView instanceof Function) {
+            _this.onRefreshView();
+        }
+    }
+
+
+    /**
+     * 搜索值
+     *
+     * @type {string}
+     * @memberof TestTaskMobMDViewBase
+     */
+    public query: string = '';
+
+    /**
+     * 快速搜索值变化
+     *
+     * @param {*} event
+     * @returns
+     * @memberof TestTaskMobMDViewBase
+     */
+    public async quickValueChange(event: any) {
+        let { detail } = event;
+        if (!detail) {
+            return;
+        }
+        let { value } = detail;
+        this.query = value;
+
+        const mdctrl: any = this.$refs.mdctrl;
+        if (mdctrl) {
+            let response = await mdctrl.quickSearch(this.query);
+            if (response) {
+            }
+        }
+    }
+
+   /**
+     * 是否单选
+     *
+     * @type {boolean}
+     * @memberof TestTaskMobMDViewBase
+     */
+    @Prop({ default: true }) protected isSingleSelect!: boolean;
+
+
+    /**
+     * 分类值
+     *
+     * @type {boolean}
+     * @memberof TestTaskMobMDViewBase
+     */
+    public categoryValue :any = {};
+
+    /**
+     * 排序值
+     *
+     * @type {boolean}
+     * @memberof TestTaskMobMDViewBase
+     */
+    public sortValue :any = {};
+
+    /**
+     * 刷新视图
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public onRefreshView() {
+        let mdctrl: any = this.$refs.mdctrl;
+        if (mdctrl) {
+            mdctrl.refresh();
+        }
+    }
+
+    /**
+     * 打开搜索表单
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public openSearchform() {
+      let search :any = this.$refs.searchformtesttaskmobmdview;
+      if(search){
+          search.open();
+      }
+    }
+
+    /**
+     * 关闭搜索表单
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public closeSearchform(){
+      let search :any = this.$refs.searchformtesttaskmobmdview;
+      if(search){
+          search.close();
+      }
+    }
+
+    /**
+     * 多选状态改变事件
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public showCheackChange(value:any){
+        this.showCheack = value;
+    }
+
+    /**
+     * 多选状态
+     *
+     * @memberof TestTaskMobMDViewBase
+     */
+    public showCheack = false;
+
+    /**
+     * 取消选择状态
+     * @memberof TestTaskMobMDViewBase
+     */
+    public cancelSelect() {
+        this.showCheackChange(false);
+    }
+
+    /**
+     * 视图加载（排序|分类）
+     * @memberof TestTaskMobMDViewBase
+     */
+    public onViewLoad() {
+        let value = Object.assign(this.categoryValue,this.sortValue);
+        this.engine.onViewEvent('mdctrl','viewload',value);
+    }
+
+    /**
+     * 分类搜索
+     *
+     * @param {*} value
+     * @memberof MOBENTITYHDLBBase
+     */
+    public onCategory(value:any){
+        this.categoryValue = value;
+        this.onViewLoad();
+    }
+
+
+
+
+
+}
+</script>
+
+<style lang='less'>
+@import './test-task-mob-mdview.less';
+</style>
