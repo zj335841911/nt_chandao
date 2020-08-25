736864372d7a04fd425a64a71a0bb96d23f4bba3
ibizdev <ibizdev@ibizsys.net>
2020-08-25 18:28:42 +0800

    KK951314 发布系统代码

--- app_Mob/src/widgets/action/mob-mobmdctrl/mob-mobmdctrl-base.vue
+++ app_Mob/src/widgets/action/mob-mobmdctrl/mob-mobmdctrl-base.vue
@@ -2,7 +2,7 @@
     <div  class="app-mob-mdctrl ">
         <div class="app-mob-mdctrl-mdctrl">
           <van-pull-refresh class="app-mob-mdctrl-refresh" v-model="isLoading" success-text="刷新成功"  @refresh="refresh" :disabled="!isEnableRefresh">
-                    <app-history-list :items="items"></app-history-list>
+                    <app-history-list :items="items"  :itemNameDetail="{time:{{ 'name':'date','isCodeList':false,} },method:{{ 'name':'actions','isCodeList':false,} },info:{{ 'name':'actionmanner','isCodeList':true,} }}"></app-history-list>
 
           </van-pull-refresh>
         </div>
