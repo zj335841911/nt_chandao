ef8addc6e23f8740ee35a5a4fe70f44998f0afa1
ibizdev <ibizdev@ibizsys.net>
2020-08-28 11:21:30 +0800

    JunZai 发布系统代码

--- app_Web/src/widgets/product/test-sidebar-list/test-sidebar-list-base.tsx
+++ app_Web/src/widgets/product/test-sidebar-list/test-sidebar-list-base.tsx
@@ -164,7 +164,19 @@ export class TestSidebarListBase extends ListControlBase {
      */
     public minorSortPSDEF: string = 'id';
 
-
+    /**
+     * 项行为触发
+     *
+     * @param {*} $event
+     * @param {*} $event2
+     * @memberof TestSidebarListBase
+     */
+    public itemActionClick($event: any, $event2: any) {
+        this.handleClick($event2);
+        if(this.list_quicktoolbar_click && this.list_quicktoolbar_click instanceof Function) {
+            this.list_quicktoolbar_click($event, $event2);
+        }
+    }
 
 
 }
