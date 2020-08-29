356002953a190930e756e4551ab5c7a100f3111e
ibizdev <ibizdev@ibizsys.net>
2020-08-29 14:03:17 +0800

    KK951314 发布系统代码

--- app_Mob/src/ibiz-core/service/indexeddb-service.ts
+++ app_Mob/src/ibiz-core/service/indexeddb-service.ts
@@ -27,7 +27,7 @@ export class IndexedDBService extends IndexedDBServiceBase {
         if (IndexedDBService.instance) {
             return IndexedDBService.instance;
         }
-        super('Aug 28, 2020');
+        super('Aug 29, 2020');
     }
 
     /**
