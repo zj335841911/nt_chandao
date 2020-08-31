86cf45a76ffb54c93ddc3600501e3bb288f973dc
ibizdev <ibizdev@ibizsys.net>
2020-08-31 20:53:57 +0800

    KK951314 发布系统代码

--- app_Mob/src/locale/lanres/test-task/test-task_en_US.ts
+++ app_Mob/src/locale/lanres/test-task/test-task_en_US.ts
@@ -1,2 +1,8 @@
-!!!!模版产生代码错误:Syntax error in template "TEMPLCODE_en_US" in line 10, column 110:
-#if is an existing directive, but the tag is malformed.  (See FreeMarker Manual / Directive Reference.)
+!!!!模版产生代码错误:----
+Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
+----
+
+----
+FTL stack trace ("~" means nesting-related):
+	- Failed at: @getLogicNameLanguage psdeField, lang...  [in template "TEMPLCODE_en_US" at line 10, column 62]
+----
