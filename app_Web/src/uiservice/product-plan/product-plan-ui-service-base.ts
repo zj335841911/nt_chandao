93e84cd4ff778ae276ba49a8c33784e0fd898808
ibizdev <ibizdev@ibizsys.net>
2020-08-31 20:01:34 +0800

    huwei_0806 发布系统代码

--- app_Web/src/uiservice/product-plan/product-plan-ui-service-base.ts
+++ app_Web/src/uiservice/product-plan/product-plan-ui-service-base.ts
@@ -425,17 +425,28 @@ export default class ProductPlanUIServiceBase extends UIService {
         let deResParameters: any[] = [];
         const parameters: any[] = [
             { pathName: 'projects', parameterName: 'project' },
-            { pathName: 'planaddeditview', parameterName: 'planaddeditview' },
         ];
-        const openIndexViewTab = (data: any) => {
-            const routePath = actionContext.$viewTool.buildUpRoutePath(actionContext.$route, context, deResParameters, parameters, _args, data);
-            actionContext.$router.push(routePath);
-            if (xData && xData.refresh && xData.refresh instanceof Function) {
-                xData.refresh(args);
+            const openDrawer = (view: any, data: any) => {
+                let container: Subject<any> = actionContext.$appdrawer.openDrawer(view, context,data);
+                container.subscribe((result: any) => {
+                    if (!result || !Object.is(result.ret, 'OK')) {
+                        return;
+                    }
+                    const _this: any = actionContext;
+                    if (xData && xData.refresh && xData.refresh instanceof Function) {
+                        xData.refresh(args);
+                    }
+                    return result.datas;
+                });
             }
-            return null;
-        }
-        openIndexViewTab(data);
+            const view: any = {
+                viewname: 'project-plan-add-edit-view', 
+                height: 0, 
+                width: 0,  
+                title: actionContext.$t('entities.project.views.planaddeditview.title'),
+                placement: 'DRAWER_LEFT',
+            };
+            openDrawer(view, data);
     }
 
     /**
