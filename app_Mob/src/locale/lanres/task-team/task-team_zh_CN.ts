2f2428a71c487d406b756175036e894c0aba499d
ibizdev <ibizdev@ibizsys.net>
2020-08-31 20:00:19 +0800

    KK951314 发布系统代码

--- app_Mob/src/locale/lanres/task-team/task-team_zh_CN.ts
+++ app_Mob/src/locale/lanres/task-team/task-team_zh_CN.ts
@@ -4,5 +4,5 @@ Tip: If the failing expression is known to be legally refer to something that's 
 
 ----
 FTL stack trace ("~" means nesting-related):
-	- Failed at: @getLogicNameLanguage field.getPSDEFi...  [in template "TEMPLCODE_en_US" at line 7, column 56]
+	- Failed at: @getLogicNameLanguage field.getPSDEFi...  [in template "TEMPLCODE_en_US" at line 8, column 56]
 ----
