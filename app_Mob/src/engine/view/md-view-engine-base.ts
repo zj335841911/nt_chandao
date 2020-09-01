d8b35a61c004468162d4641ddb675577e7b0c9bb
ibizdev <ibizdev@ibizsys.net>
2020-09-01 14:41:56 +0800

    KK951314 发布系统代码

--- app_Mob/src/engine/view/md-view-engine-base.ts
+++ app_Mob/src/engine/view/md-view-engine-base.ts
@@ -361,6 +361,7 @@ export default class MDViewEngineBase extends ViewEngine {
         }
         const state = args.length > 0 && !Object.is(args[0].srfkey, '') ? false : true;
         this.calcToolbarItemState(state);
+        this.calcToolbarItemAuthState(null);
     }
 
     /**
@@ -374,6 +375,7 @@ export default class MDViewEngineBase extends ViewEngine {
             this.view.$emit('viewload', args);
         }
         this.calcToolbarItemState(true);
+        this.calcToolbarItemAuthState(null);
     }
 
     /**
