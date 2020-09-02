b383b9c7ceaf2b478126824003c01a4805d20819
ibizdev <ibizdev@ibizsys.net>
2020-09-02 09:55:45 +0800

    KK951314 发布系统代码

--- app_Mob/src/widgets/bug/mob-new-from-form/mob-new-from-form-base.vue
+++ app_Mob/src/widgets/bug/mob-new-from-form/mob-new-from-form-base.vue
@@ -1703,6 +1703,7 @@ export default class MobNewFROMBase extends Vue implements ControlInterface {
     private resetFormData({ name, newVal, oldVal }: { name: string, newVal: any, oldVal: any }): void {
         if (Object.is(name, 'productname')) {
             this.onFormItemValueChange({ name: 'branch', value: null });
+            this.onFormItemValueChange({ name: 'productname', value: null });
         }
         if (Object.is(name, 'branch')) {
             this.onFormItemValueChange({ name: 'modulename', value: null });
