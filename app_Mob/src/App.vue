5746e0aebcc67de66d550d8af9eed922616b6e0d
ibizdev <ibizdev@ibizsys.net>
2020-09-03 13:09:46 +0800

    KK951314 发布系统代码

--- app_Mob/src/App.vue
+++ app_Mob/src/App.vue
@@ -1,2 +1,55 @@
-!!!!模版产生代码错误:Syntax error in template "TEMPLCODE_en_US" in line 57, column 8:
-#list is an existing directive, but the tag is malformed.  (See FreeMarker Manual / Directive Reference.)
+<template>
+	<ion-app id="app" :class="themeClass" class="ios ion-page hydrated">
+		        <keep-alive>
+            <router-view v-if="$route.meta.keepAlive"></router-view>
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"></router-view>
+	</ion-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class App extends Vue { 
+
+	/**
+     * 当前主题
+     *
+     * @readonly
+     * @memberof App
+     */
+    get themeClass() {
+        if (this.$router.app.$store.state.selectTheme) {
+            return this.$router.app.$store.state.selectTheme;
+        } else if (localStorage.getItem('theme-class')) {
+            return localStorage.getItem('theme-class');
+        } else {
+            return 'app-dark-blue-theme';
+        }
+	}
+
+    /**
+     * 设置第三方应用
+     *
+     * @readonly
+     * @memberof App
+     */
+    public setThirdPartyPath(){
+        let name = this.$viewTool.getThirdPartyName();
+        if(name){
+            this.$router.app.$store.commit('setThirdPartyName',name);
+        }
+    }
+
+	/**
+     * 生命周期
+     *
+     * @readonly
+     * @memberof App
+     */
+    public created(){
+        this.setThirdPartyPath();
+    }
+}
+</script>
