5f8badabea407c9607a6018a3573e2934e449335
Neuromancer255 <wanghn255@foxmail.com>
2020-09-03 16:25:29 +0800

    PMS自定义富文本编辑器

--- app_Mob/src/user-register.ts
+++ app_Mob/src/user-register.ts
@@ -2,5 +2,7 @@ export const UserComponent = {
     install(v: any, opt: any) {
         // 历史记录插件
         v.component('app-history-list',() => import('@/components/app-history-list/app-history-list.vue'));
+        // 富文本编辑器（show）
+        v.component('app-mob-rich-text-editor-pms',() => import('@/components/app-mob-rich-text-editor-pms/app-mob-rich-text-editor-pms.vue'));
     }
 };
