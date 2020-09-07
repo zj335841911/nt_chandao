b45a218f8664e4c9b5bbabc0ce212b3b2ad4481e
ibizdev <ibizdev@ibizsys.net>
2020-09-07 23:47:45 +0800

    huwei_0806 发布系统代码

--- app_Mob/src/ibiz-core/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export { Util } from './util/util';
-export { Verify } from './verify/verify';
-export { XMLWriter } from './xml-writer/xml-writer';
-export { Http } from './http/http';
-export { Loading } from './loading/loading';
-export { HttpResponse } from './http/http-response';
