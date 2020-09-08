fc0bb4d40675b14ba5a81339e46b7537e635edd0
ibizdev <ibizdev@ibizsys.net>
2020-09-08 15:01:53 +0800

    JunZai 发布系统代码

--- app_Web/src/studio-core/components/view-quick-group-tab/view-quick-group-tab.tsx
+++ app_Web/src/studio-core/components/view-quick-group-tab/view-quick-group-tab.tsx
@@ -1,4 +1,4 @@
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import './view-quick-group-tab.less';
 
 /**
@@ -18,14 +18,75 @@ export class ViewQuickGroupTab extends Vue {
      * @memberof ViewQuickGroupTab
      */
     @Prop({ default: () => [] })
-    public items!: any[];
+    items!: any[];
+    @Watch('items', { immediate: true })
+    itemsWatch() {
+        if (this.items?.length > 0) {
+            this.formatItems(this.items);
+        }
+    }
+
+    /**
+     * tab展示模式
+     *
+     * @type {('average' | 'left' | 'right')}
+     * @memberof ViewQuickGroupTab
+     */
+    @Prop({ default: 'left' })
+    tabMode!: 'average' | 'left' | 'right';
+
+    /**
+     * 计数器服务名
+     *
+     * @type {string}
+     * @memberof AppQuickGroup
+     */
+    @Prop()
+    public counterService?: any;
+
+    /**
+     * 当前选中项
+     *
+     * @type {*}
+     * @memberof ViewQuickGroupTab
+     */
+    selectItem: any = null;
+
+    /**
+     * 格式化数据
+     *
+     * @param {any[]} items
+     * @memberof ViewQuickGroupTab
+     */
+    formatItems(items: any[]): void {
+        items.forEach((item: any) => {
+            if (item.default) {
+                this.select(item);
+            }
+        });
+    }
+
+    /**
+     * 设置选中项
+     *
+     * @param {*} item
+     * @memberof ViewQuickGroupTab
+     */
+    select(item: any): void {
+        this.selectItem = item;
+        this.$emit('valuechange', item);
+    }
 
     render() {
         return <div class='view-quick-group-tab'>
-            <div class='tab-list'>
+            <div class={{ 'tab-list': true, [this.tabMode]: true }}>
                 {this.items.map((item: any) => {
-                    return <div class='`tab-item`'>
-                        {item.text}
+                    const count = this.counterService?.counterData[item.codename];
+                    return <div class={{ 'tab-item': true, 'activated': this.selectItem.codename === item.codename }} on-click={() => this.select(item)}>
+                        <span class='item-label'>
+                            {item.text}
+                            {count != null ? <span class='item-count'>({count})</span> : null}
+                        </span>
                     </div>;
                 })}
             </div>
