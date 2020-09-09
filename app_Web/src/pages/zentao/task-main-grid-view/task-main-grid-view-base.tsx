5c2ff5217cc6bd661b9d0d8bbfc9d7506f9cdb05
ibizdev <ibizdev@ibizsys.net>
2020-09-09 14:23:06 +0800

    huwei_0806 发布系统代码

--- app_Web/src/pages/zentao/task-main-grid-view/task-main-grid-view-base.tsx
+++ app_Web/src/pages/zentao/task-main-grid-view/task-main-grid-view-base.tsx
@@ -61,6 +61,28 @@ export class TaskMainGridViewBase extends GridViewBase {
      */
     public appUIService: TaskUIService = new TaskUIService(this.$store);
 
+	/**
+	 * 自定义视图导航上下文集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof TaskMainGridViewBase
+	 */
+    protected customViewNavContexts: any = {
+        'PROJECT': { isRawValue: false, value: 'project' }
+    };
+
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof TaskMainGridViewBase
+	 */
+    protected customViewParams: any = {
+        'project': { isRawValue: false, value: 'project' }
+    };
+
     /**
      * 视图模型数据
      *
