7b3d4c050068225df7ccbe16eeb8ce1b8c04a64d
ibizdev <ibizdev@ibizsys.net>
2020-09-16 16:21:46 +0800

    KK951314 发布系统代码

--- app_Mob/src/components/app-mob-select-changeTheme/app-mob-select-changeTheme.vue
+++ app_Mob/src/components/app-mob-select-changeTheme/app-mob-select-changeTheme.vue
@@ -59,6 +59,15 @@ export default class AppSelect extends Vue {
         {value:"app-blue-theme",text:"dark",background:"#705697",color:"#fff"},
         {value:"app-dark-blue-theme",text:"light",background:"#5475ab",color:"#fff"},
         {value:"app-default-theme",text:"fault",background:"#3880ff",color:"#fff"},
+        {value:"app-class-black-theme",text:"极致黑",background:"#282829",color:"#FFF"},
+        {value:"app-class-blue-theme",text:"经典蓝",background:"#1E538B",color:"#FFF"},
+        {value:"app-light-green-theme",text:"浅葱绿",background:"#05D2C2",color:"#FFF"},
+        {value:"app-minimalist-white-theme",text:"极致白",background:"#FEFEFE",color:"#000"},
+        {value:"app-peach-pink-theme",text:"桃桃粉",background:"#FD84A3",color:"#FFF"},
+        {value:"app-plain-grey-theme",text:"素雅灰",background:"#F4F6F9",color:"#000"},
+        {value:"app-star-purple-theme",text:"星辰紫",background:"#6937D9",color:"#FFF"},
+        {value:"app-summer-yellow-theme",text:"盛夏黄",background:"#FEE45A",color:"#000"},
+        {value:"app-vital-red-theme",text:"元气红",background:"#FE657A",color:"#FFF"},
     ];
 
     /**
