29af28e79647d5a2a09702e5c37f57776a03a52c
ibizdev <ibizdev@ibizsys.net>
2020-09-19 19:45:51 +0800

    KK951314 发布系统代码

--- app_Mob/src/pages/ibiz/user-mob-mpickup-view/user-mob-mpickup-view-base.vue
+++ app_Mob/src/pages/ibiz/user-mob-mpickup-view/user-mob-mpickup-view-base.vue
@@ -2,6 +2,15 @@
 <ion-page :className="{ 'view-container': true, 'default-mode-view': true, 'demobmpickupview': true, 'user-mob-mpickup-view': true }">
     
     <ion-header>
+        <ion-toolbar v-show="titleStatus" class="ionoc-view-header">
+            <ion-buttons slot="start">
+                <ion-button v-show="isShowBackButton" @click="closeView">
+                    <ion-icon name="chevron-back"></ion-icon>
+                    {{$t('app.button.back')}}
+                </ion-button>
+            </ion-buttons>
+            <ion-title class="view-title"><label class="title-label"><ion-icon v-if="model.icon" :name="model.icon"></ion-icon> <img v-else-if="model.iconcls" :src="model.iconcls" alt=""> {{$t(model.srfCaption)}}</label></ion-title>
+        </ion-toolbar>
 
     
               <ion-toolbar>
