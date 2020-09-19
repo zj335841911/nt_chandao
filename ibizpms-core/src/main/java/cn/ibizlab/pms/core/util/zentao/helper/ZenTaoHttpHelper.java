59fe8e510bdabbc5fa73964e3046d738157b0a21
胡维 <huwei_0315@example.com>
2020-09-19 19:33:19 +0800

    v0.29.109 禅道接口返回值调整

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/zentao/helper/ZenTaoHttpHelper.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/zentao/helper/ZenTaoHttpHelper.java
@@ -116,12 +116,11 @@ public class ZenTaoHttpHelper {
         if (HttpStatus.FOUND.equals(responseEntity.getStatusCode()) && (body == null || body.isEmpty())) {
             body = responseEntity.toString();
         }
-
         log.debug(ZenTaoMessage.MSG_INFO_0002, body);
         if (body == null || body.isEmpty()) {
             return null;
         }
-        if (body.startsWith("<html>") || body.startsWith("<302")) {
+        if (body.startsWith("<html>") || body.startsWith("<302") || (responseEntity.getStatusCodeValue() == 200 && body.startsWith("<br />"))) {
             jo.put("html", body);
         } else {
             jo = JSONObject.parseObject(body);
