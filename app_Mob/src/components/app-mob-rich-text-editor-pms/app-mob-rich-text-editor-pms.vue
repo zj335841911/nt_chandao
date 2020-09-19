dc9af1858d112ac4bca93d9b627da4d6b1fddcb1
zhujiamin <57311383+LUCIFER-ZHU@users.noreply.github.com>
2020-09-19 18:43:29 +0800

    富文本新增图片预览功能

--- app_Mob/src/components/app-mob-rich-text-editor-pms/app-mob-rich-text-editor-pms.vue
+++ app_Mob/src/components/app-mob-rich-text-editor-pms/app-mob-rich-text-editor-pms.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="app-mob-rich-text-editor" @click="open">
-    <div class="rich-text-editor-info" v-html="showVal"></div>
+  <div class="app-mob-rich-text-editor" >
+    <div class="rich-text-editor-info" v-html="showVal" ref="content" @click="open"></div>
     <ion-icon class="app-mob-rich-text-editor-icon" v-if="!showVal" name="options-outline" @click.stop="open"></ion-icon>
+    <van-popup closeable v-model="picturePreview.status" @click-overlay="closePreview" get-container="#app" @opened="openedPreview"><img class="preview" v-show="picturePreview.status" :src="picturePreview.src"/></van-popup>
   </div>
 </template>
 <script lang = 'ts'>
@@ -46,6 +47,23 @@ export default class AppRichTextEditor extends Vue {
     onValueChange(){
       const url:string = this.downloadUrl.indexOf('../') === 0 ? this.downloadUrl.substring(3) : this.downloadUrl;
       this.showVal = this.value ? this.value.replace(/\{(\d+)\.(bmp|jpg|jpeg|png|tif|gif|pcx|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|raw|WMF|webp)\}/g, `${url}/$1`) : "";
+    }
+
+    /**
+     * 是否图片预览
+     *
+     * @memberof AppRichTextEditor
+     */
+    public picturePreview:any = {};
+
+    /**
+     * 监听图片预览
+     *
+     * @memberof AppRichTextEditor
+     */
+    @Watch('picturePreview')
+    onPicturePreviewChange(){
+        this.$forceUpdate();
     }
 
     /**
@@ -165,7 +183,6 @@ export default class AppRichTextEditor extends Vue {
         }
     }
 
-
     /**
      * 数据处理
      *
@@ -190,6 +207,22 @@ export default class AppRichTextEditor extends Vue {
         this.uploadUrl = _uploadUrl;
     }
 
+    /**
+     * 生命周期
+     *
+     * @private
+     * @memberof AppMobFileUpload
+     */
+    public created(){
+        if (localStorage.getItem('picture-preview')) {
+            let pic:any = localStorage.getItem('picture-preview');   
+            this.picturePreview = JSON.parse(pic);
+            this.$forceUpdate();
+        } else {
+            this.picturePreview = {status:false};
+            this.$forceUpdate();
+        }
+    }
 
     /**
      * 生命周期
@@ -199,9 +232,47 @@ export default class AppRichTextEditor extends Vue {
      */
     public mounted(){
       this.dataProcess();
+      this.getPictureDom();
     }
 
+    /**
+     * 获取图片DOM进行操作
+     *
+     * @memberof AppMobFileUpload
+     */
+    public getPictureDom(){
+        // 获取到所有图片dom节点
+        let contents:any = this.$refs.content;  
+        let imgs:any = contents.getElementsByTagName("img");
+        setTimeout(() => {
+          Array.from(imgs).forEach((img:any)=>{
+            img.addEventListener('click',(event:any)=>{
+              localStorage.setItem('picture-preview',`{"src":"${img.src}","status":true}`);
+              this.picturePreview = {"src":img.src,"status":true}
+              event.stopPropagation();
+            },false)
+          })
+        }, 500);
+    }
 
+    /**
+     * 关闭图片预览弹出层
+     *
+     * @memberof AppMobFileUpload
+     */
+    public closePreview(){
+      localStorage.setItem('picture-preview',`{"src":"","status":false}`);
+      this.$forceUpdate();
+    }
+
+    /**
+     * 打开图片预览弹出层后
+     *
+     * @memberof AppMobFileUpload
+     */
+    public openedPreview(){
+      console.log('打开弹出层且动画结束后触发');
+    }
 }
 </script>
 <style lang="less">
