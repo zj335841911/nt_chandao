07065816297c15596edd3df4288aa26eef4defd5
胡维 <huwei_0315@example.com>
2020-09-22 19:47:46 +0800

    v0.29.113 新建报表更新键值

--- app_Web/src/service/test-report/test-report-service.ts
+++ app_Web/src/service/test-report/test-report-service.ts
@@ -20,6 +20,29 @@ export default class TestReportService extends TestReportServiceBase {
     constructor(opts: any = {}) {
         super(opts);
     }
+    /**
+     * GetInfoTestTask接口方法
+     *
+     * @param {*} [context={}]
+     * @param {*} [data={}]
+     * @param {boolean} [isloading]
+     * @returns {Promise<any>}
+     * @memberof TestReportServiceBase
+     */
+    public async GetInfoTestTask(context: any = {},data: any = {}, isloading?: boolean): Promise<any> {
+        if(context.product && context.testreport){
+            let masterData:any = {};
+            context.testreport = 0;
+            data.id = 0;
+            Object.assign(data,masterData);
+            let res:any = await Http.getInstance().put(`/products/${context.product}/testreports/${context.testreport}/getinfotesttask`,data,isloading);
 
+            return res;
+        }
+        context.testreport = 0;
+        data.id = 0;
+        let res:any = Http.getInstance().put(`/testreports/${context.testreport}/getinfotesttask`,data,isloading);
+        return res;
+    }
 
 }
