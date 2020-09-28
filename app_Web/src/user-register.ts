3953ef05b6cd5f8f051394176eaf416338abc7f8
Neuromancer255 <wanghn255@foxmail.com>
2020-09-28 22:59:33 +0800

    导入自定义图表插件（表格图例）

--- app_Web/src/user-register.ts
+++ app_Web/src/user-register.ts
@@ -2,10 +2,16 @@ import Vue from 'vue';
 import { UserDefaultComponent } from './user-default-register';
 import { iBizProjectCore } from './ibiz-project-core';
 
+// 图表插件（表格图例）
+import ChartFormLegend from './components/chart-form-legend/chart-form-legend.vue'
+
+
 // 注册Vue插件
 export const UserComponent = {
     install(v: any, opt: any) {
         Vue.use(UserDefaultComponent);
         Vue.use(iBizProjectCore);
+        // 图表插件（表格图例）
+        v.component('chart-form-legend',ChartFormLegend);
     }
 };
