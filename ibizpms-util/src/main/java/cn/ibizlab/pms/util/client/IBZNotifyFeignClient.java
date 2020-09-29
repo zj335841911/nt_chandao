c803310f38aa26fb51d149dcbd1ea4cb30093c98
Crimson <739979063@qq.com>
2020-09-29 16:05:54 +0800

    V0.32.118 与notify的消息推送地址修改

--- ibizpms-util/src/main/java/cn/ibizlab/pms/util/client/IBZNotifyFeignClient.java
+++ ibizpms-util/src/main/java/cn/ibizlab/pms/util/client/IBZNotifyFeignClient.java
@@ -9,28 +9,19 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(value = "${ibiz.ref.service.notify:ibznotify-api}",fallback = IBZNotifyFallback.class)
 public interface IBZNotifyFeignClient
 {
-	@RequestMapping(method = RequestMethod.POST,value = "/SendMsg")
+	@RequestMapping(method = RequestMethod.POST,value = "/notify/sendmsg")
 	Boolean SendMsg(@RequestBody JSONObject msg);
 
-	@RequestMapping(method = RequestMethod.POST,value = "/createMsgTemplate")
+	@RequestMapping(method = RequestMethod.POST,value = "/notify/createmsgtempl")
 	Boolean createMsgTemplate(@RequestBody JSONObject template);
 
 
-	@RequestMapping(method = RequestMethod.POST,value = "/dingtalk/sendlinkmsg")
+	@RequestMapping(method = RequestMethod.POST,value = "/notify/dingtalk/sendlinkmsg")
 	Long sendDingTalkLinkMsg(@RequestBody JSONObject msg);
-	/**
-	 * 创建钉钉待办
-	 * @param msg
-	 * @return
-	 */
-//	@RequestMapping(method = RequestMethod.POST,value = "/dingtalk/createworkrecord")
+
 	@RequestMapping(method = RequestMethod.POST,value = "/notify/dingtalk/createworkrecord")
 	String createDingTalkWorkRecord(@RequestBody JSONObject msg);
-	/**
-	 * 完成钉钉待办
-	 * @param msgId
-	 * @return
-	 */
+
 	@RequestMapping(method = RequestMethod.POST,value = "/notify/dingtalk/finishworkrecord/{msgid}")
 	Boolean finishDingTalkWorkRecord(@PathVariable("msgid") String msgId);
 }
