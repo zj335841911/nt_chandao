f4b87aa4b93a30f1a6c7bb8e6df4ed26ea8e427f
ibizdev <ibizdev@ibizsys.net>
2020-09-29 13:49:03 +0800

    huajiyu 发布系统代码

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibiz/service/IEMPLOYEELOADService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibiz/service/IEMPLOYEELOADService.java
@@ -38,7 +38,6 @@ public interface IEMPLOYEELOADService extends IService<EMPLOYEELOAD>{
     void saveBatch(List<EMPLOYEELOAD> list) ;
     Page<EMPLOYEELOAD> searchDefault(EMPLOYEELOADSearchContext context) ;
     Page<EMPLOYEELOAD> searchGETWOERKLOAD(EMPLOYEELOADSearchContext context) ;
-    Page<EMPLOYEELOAD> searchGETWORKLOAD_UNASSIGN(EMPLOYEELOADSearchContext context) ;
     List<EMPLOYEELOAD> selectByProject(Long id) ;
     void removeByProject(Long id) ;
     /**
