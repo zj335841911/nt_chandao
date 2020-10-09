fceac99506f62744d25f8a3800d2c957b24f344f
ibizdev <ibizdev@ibizsys.net>
2020-10-09 17:52:32 +0800

    huwei_0806 发布系统代码

---
+++ app_Web/src/widgets/user-year-work-stats/month-bug-chart/month-bug-chart.vue
@@ -0,0 +1,22 @@
+<template src="./month-bug-chart.html"/>
+<script lang='tsx'>
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core';
+import { MonthBugChartBase } from './month-bug-chart-base';
+ 
+
+/**
+ * dashboard_sysportlet5_chart部件
+ *
+ * @export
+ * @class MonthBugChart
+ * @extends {MonthBugChartBase}
+ */
+@Component({
+    components: {
+         
+    }
+})
+@VueLifeCycleProcessing()
+export default class MonthBugChart extends MonthBugChartBase { }
+</script>
