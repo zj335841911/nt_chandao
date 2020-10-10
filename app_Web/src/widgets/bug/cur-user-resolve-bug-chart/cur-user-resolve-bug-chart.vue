5ab3c6795de21532064f533dd4daee6af6b8f199
Neuromancer255 <wanghn255@foxmail.com>
2020-10-10 16:42:48 +0800

    用户年度工作内容统计图表样式调整

--- app_Web/src/widgets/bug/cur-user-resolve-bug-chart/cur-user-resolve-bug-chart.vue
+++ app_Web/src/widgets/bug/cur-user-resolve-bug-chart/cur-user-resolve-bug-chart.vue
@@ -18,5 +18,70 @@ import { CurUserResolveBugChartBase } from './cur-user-resolve-bug-chart-base';
     }
 })
 @VueLifeCycleProcessing()
-export default class CurUserResolveBugChart extends CurUserResolveBugChartBase { }
+export default class CurUserResolveBugChart extends CurUserResolveBugChartBase { 
+    /**
+     * 初始化图表所需参数
+     *
+     * @type {}
+     * @memberof ChartBase
+     */   
+    public chartOption:any = {
+        title:{
+            show:true ,
+            text:'解决的Bug数',
+            subtext:'',
+            x:'center',
+            y:'center',
+            textStyle: {
+                fontSize: 20
+            },
+            subtextStyle: {
+                fontSize:15
+            }
+        },
+        legend:{
+            show:true
+        },
+        tooltip:{
+            show:true
+        },
+        dataset:[],
+        series:[
+                     {
+            id:'resolvebug',
+            name:'',
+            type:'pie',
+            datasetIndex:0,
+            startAngle:180,
+            seriesLayoutBy:"column",
+            encode:{
+                itemName:"severity",
+                value:"srfcount"
+            }}
+        ]
+    };
+
+    /**
+     * 获取图表数据
+     * 
+     * @returns {*} 
+     * @memberof Dashboard_sysportlet6_chartBase
+     */
+    public load(opt?:any) {
+        let _this = this;
+        const arg: any = { ...opt };
+        const parentdata: any = {};
+        this.$emit('beforeload', parentdata);
+        Object.assign(arg, parentdata);
+        Object.assign(arg,{viewparams:this.viewparams,page:0,size:1000});
+        this.service.search(this.fetchAction,JSON.parse(JSON.stringify(this.context)),arg,this.showBusyIndicator).then((res) => {
+            if (res) {
+               this.chartOption.title.subtext = `${res.data.length} 个`;
+               this.transformToBasicChartSetData(res.data,(codelist:any) =>{_this.drawCharts(codelist)});
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+}
 </script>
--- app_Web/src/widgets/project/involved-project-chart/involved-project-chart.vue
+++ app_Web/src/widgets/project/involved-project-chart/involved-project-chart.vue
@@ -18,5 +18,71 @@ import { InvolvedProjectChartBase } from './involved-project-chart-base';
     }
 })
 @VueLifeCycleProcessing()
-export default class InvolvedProjectChart extends InvolvedProjectChartBase { }
+export default class InvolvedProjectChart extends InvolvedProjectChartBase { 
+    
+    /**
+     * 初始化图表所需参数
+     *
+     * @type {}
+     * @memberof Dashboard_sysportlet6_chartBase
+     */   
+    public chartOption:any = {
+        title:{
+            show:true ,
+            text:'参与项目',
+            subtext:'',
+            x:'center',
+            y:'center',
+            textStyle: {
+                fontSize: 20
+            },
+            subtextStyle: {
+                fontSize:15
+            }
+        },
+        legend:{
+            show:true
+        },
+        tooltip:{
+            show:true
+        },
+        dataset:[],
+        series:[
+                     {
+            id:'involved',
+            name:'',
+            type:'pie',
+            datasetIndex:0,
+            startAngle:180,
+            seriesLayoutBy:"column",
+            encode:{
+                itemName:"status",
+                value:"srfcount"
+            }}
+        ]
+    };
+
+    /**
+     * 获取图表数据
+     * 
+     * @returns {*} 
+     * @memberof Dashboard_sysportlet6_chartBase
+     */
+    public load(opt?:any) {
+        let _this = this;
+        const arg: any = { ...opt };
+        const parentdata: any = {};
+        this.$emit('beforeload', parentdata);
+        Object.assign(arg, parentdata);
+        Object.assign(arg,{viewparams:this.viewparams,page:0,size:1000});
+        this.service.search(this.fetchAction,JSON.parse(JSON.stringify(this.context)),arg,this.showBusyIndicator).then((res) => {
+            if (res) {
+               this.chartOption.title.subtext = `${res.data.length} 个`;
+               this.transformToBasicChartSetData(res.data,(codelist:any) =>{_this.drawCharts(codelist)});
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+}
 </script>
