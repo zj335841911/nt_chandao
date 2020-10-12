23ca8a22818feb831d45793e97f3d07106763408
ibizdev <ibizdev@ibizsys.net>
2020-10-12 16:04:52 +0800

    KK951314 发布系统代码

---
+++ app_Mob/src/config/appConfig.ts
@@ -0,0 +1,55 @@
+export const appConfig = {
+  //默认主题
+  defaultTheme: "app-dark-blue-theme",
+  //主题集合
+  themes: [
+    {
+      value: "app-blue-theme",
+      text: "魅力紫",
+      background: "#705697",
+      color: "#fff",
+    },
+    {
+      value: "app-dark-blue-theme",
+      text: "经典蓝",
+      background: "#5475ab",
+      color: "#fff",
+    },
+    {
+      value: "app-class-black-theme",
+      text: "极致黑",
+      background: "#282829",
+      color: "#FFF",
+    },
+    {
+      value: "app-light-green-theme",
+      text: "浅葱绿",
+      background: "#05D2C2",
+      color: "#FFF",
+    },
+    {
+      value: "app-peach-pink-theme",
+      text: "桃桃粉",
+      background: "#FD84A3",
+      color: "#FFF",
+    },
+    {
+      value: "app-star-purple-theme",
+      text: "星辰紫",
+      background: "#6937D9",
+      color: "#FFF",
+    },
+    {
+      value: "app-summer-yellow-theme",
+      text: "盛夏黄",
+      background: "#FEE45A",
+      color: "#000",
+    },
+    {
+      value: "app-vital-red-theme",
+      text: "元气红",
+      background: "#FE657A",
+      color: "#FFF",
+    },
+  ],
+};
