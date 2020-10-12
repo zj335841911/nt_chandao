6da1fa390bfac7a701651ec485d511ba4af34661
myth <1157151478@qq.com>
2020-10-12 20:08:47 +0800

    10.12花季余修改年度总结获取基本信息

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/UserYearWorkStatsExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/UserYearWorkStatsExService.java
@@ -1,8 +1,14 @@
 package cn.ibizlab.pms.core.extensions.service;
 
 import cn.ibizlab.pms.core.ibiz.service.impl.UserYearWorkStatsServiceImpl;
+import cn.ibizlab.pms.core.ibiz.service.logic.IUserYearWorkStatsGetDevInfomationLogic;
+import cn.ibizlab.pms.core.ibiz.service.logic.IUserYearWorkStatsGetPOInfomationLogic;
+import cn.ibizlab.pms.core.ibiz.service.logic.IUserYearWorkStatsGetQAInformationLogic;
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibiz.domain.UserYearWorkStats;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -15,6 +21,17 @@ import java.util.*;
 @Primary
 @Service("UserYearWorkStatsExService")
 public class UserYearWorkStatsExService extends UserYearWorkStatsServiceImpl {
+
+
+    @Autowired
+    @Lazy
+    IUserYearWorkStatsGetQAInformationLogic qaInformationLogic;
+    @Autowired
+    @Lazy
+    IUserYearWorkStatsGetDevInfomationLogic devInfomationLogic;
+    @Autowired
+    @Lazy
+    IUserYearWorkStatsGetPOInfomationLogic poInfomationLogic;
 
     @Override
     protected Class currentModelClass() {
@@ -29,8 +46,15 @@ public class UserYearWorkStatsExService extends UserYearWorkStatsServiceImpl {
     @Override
     @Transactional
     public UserYearWorkStats getUserYearAction(UserYearWorkStats et) {
-        if(et.getCuryear() == null) {
-            et = this.get(et.getId());
+        et = this.get(et.getId());
+        if (et.getJudgerole().equals("dev")){
+            devInfomationLogic.execute(et);
+        }
+        else if (et.getJudgerole().equals("qa")){
+            qaInformationLogic.execute(et);
+        }
+        else {
+            poInfomationLogic.execute(et);
         }
         return super.getUserYearAction(et);
     }
@@ -43,6 +67,15 @@ public class UserYearWorkStatsExService extends UserYearWorkStatsServiceImpl {
     @Transactional
     public UserYearWorkStats updateTitleByYear(UserYearWorkStats et) {
         et.setTitle(String.format("%1$s年工作内容统计一览表 —— %2$s",et.getCuryear(),et.getRealname()));
+        if (et.getJudgerole().equals("dev")){
+            devInfomationLogic.execute(et);
+        }
+        else if (et.getJudgerole().equals("qa")){
+            qaInformationLogic.execute(et);
+        }
+        else {
+            poInfomationLogic.execute(et);
+        }
         return super.updateTitleByYear(et);
     }
 }
