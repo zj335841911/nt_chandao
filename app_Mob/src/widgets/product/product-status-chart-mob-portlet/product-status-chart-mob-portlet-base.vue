2ba4e0e91a910ef15323d13cd0a980fb140a14ae
ibizdev <ibizdev@ibizsys.net>
2020-10-20 17:04:52 +0800

    KK951314 发布系统代码

--- app_Mob/src/widgets/product/product-status-chart-mob-portlet/product-status-chart-mob-portlet-base.vue
+++ app_Mob/src/widgets/product/product-status-chart-mob-portlet/product-status-chart-mob-portlet-base.vue
@@ -21,9 +21,7 @@
     @closeview="closeView($event)">
 </view_dashboard_sysportlet8_chart>
         </ion-list>
-        <ion-select ref="select" v-show="false"  @ionChange="actionBarClick" interface="action-sheet" :cancel-text="$t('app.button.cancel')">
-            <ion-select-option  v-for="option of actionBarModelData" :key="option.viewlogicname"  :value="option.viewlogicname">{{option.actionName}}</ion-select-option>
-        </ion-select>
+        <van-action-sheet v-model="selectStatus" get-container="#app" :actions="actionBarModelData" cancel-text="取消" close-on-click-action @select="actionBarClick" @cancel="onCancel" />
     </ion-row>
 </template>
 
@@ -189,9 +187,25 @@ export default class ProductStatusChartMobBase extends Vue implements ControlInt
      * 定制标题
      *
      * @type {string}
-     * @memberof MOBMyFavoriteStory
+     * @memberof ProductStatusChartMob
      */
     @Prop() protected customizeTitle?: string;
+
+    
+    /**
+     * 选择器状态
+     *
+     * @type {boolean}
+     * @memberof ProductStatusChartMob
+     */
+    public selectStatus:boolean = false
+
+    /**
+     * 选择器状态取消事件
+     */
+    public onCancel() {
+        this.selectStatus = false;
+    }
 
     /**
      * 操作栏模型数据
@@ -291,9 +305,9 @@ export default class ProductStatusChartMobBase extends Vue implements ControlInt
     /**
      * 内置门户行为组
      *
-     * @memberof MyTaskMob
+     * @memberof ProductStatusChartMob
      */
-    public builtinItemS = [{actionName:'重命名',viewlogicname:'rename'},{actionName:"删除",viewlogicname:"delete"}]
+    public builtinItemS = [{name:'重命名',viewlogicname:'rename'},{name:"删除",viewlogicname:"delete"}]
 
     /**
      * 门户点击行为菜单
@@ -301,12 +315,7 @@ export default class ProductStatusChartMobBase extends Vue implements ControlInt
      * @memberof ProductStatusChartMob
      */
     public open() {
-        let select :any= this.$refs['select'];
-        if(select){
-            setTimeout(() => {
-                select.open();
-            }, 1);
-        }
+        this.selectStatus = true;
     }
 
     get edit_title_btn(){
@@ -318,20 +327,16 @@ export default class ProductStatusChartMobBase extends Vue implements ControlInt
      *
      * @memberof ProductStatusChartMob
      */
-    public actionBarClick(value:any) {
-        if(value.detail.value){
-            if(value.detail.value == 'rename' ){
+    public actionBarClick(data:any) {
+        if(data.viewlogicname){
+            if(data.viewlogicname == 'rename' ){
                 this.isEditTitle = true;
-            }else if(value.detail.value == 'delete' ){
+            }else if(data.viewlogicname == 'delete' ){
                 this.$emit("enableCustomizedEvent",'delete',this.item)
             }
             else{
-                this.handleItemClick(value.detail.value);
+                this.handleItemClick(data.viewlogicname);
             }
-        }
-        let select :any = this.$refs['select'];
-        if (select) {
-            select.value = null;
         }
     }
 
