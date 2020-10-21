28b3cc679200123354c0e544226936860ef2e518
ibizdev <ibizdev@ibizsys.net>
2020-10-21 23:44:41 +0800

    huwei_0806 发布系统代码

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/service/impl/UserTplServiceImpl.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/service/impl/UserTplServiceImpl.java
@@ -52,14 +52,7 @@ public class UserTplServiceImpl extends ServiceImpl<UserTplMapper, UserTpl> impl
         @Override
     @Transactional
     public boolean create(UserTpl et) {
-        String zentaoSid = org.springframework.util.DigestUtils.md5DigestAsHex(cn.ibizlab.pms.core.util.zentao.helper.TokenHelper.getRequestToken().getBytes()); 
-        cn.ibizlab.pms.core.util.zentao.bean.ZTResult rst = new cn.ibizlab.pms.core.util.zentao.bean.ZTResult();
-        boolean bRst = cn.ibizlab.pms.core.util.zentao.helper.ZTUserTplHelper.create(zentaoSid, cn.ibizlab.pms.core.util.zentao.helper.TransHelper.ET2JO(et, "create"), rst);
-        if (bRst && rst.getEtId() != null) {
-            et = this.get(rst.getEtId());
-        }
-        et.set("ztrst", rst);
-        return bRst;
+  			return cn.ibizlab.pms.util.security.SpringContextHolder.getBean(cn.ibizlab.pms.core.util.ibizzentao.helper.UserTplHelper.class).create(et);
     }
 
     @Override
@@ -83,12 +76,7 @@ public class UserTplServiceImpl extends ServiceImpl<UserTplMapper, UserTpl> impl
         @Override
     @Transactional
     public boolean remove(Long key) {
-        String zentaoSid = org.springframework.util.DigestUtils.md5DigestAsHex(cn.ibizlab.pms.core.util.zentao.helper.TokenHelper.getRequestToken().getBytes());
-        cn.ibizlab.pms.core.util.zentao.bean.ZTResult rst = new cn.ibizlab.pms.core.util.zentao.bean.ZTResult();
-        UserTpl et = this.get(key);
-        boolean bRst = cn.ibizlab.pms.core.util.zentao.helper.ZTUserTplHelper.delete(zentaoSid, (JSONObject) JSONObject.toJSON(et), rst);
-        et.set("ztrst", rst);
-        return bRst;
+  			return cn.ibizlab.pms.util.security.SpringContextHolder.getBean(cn.ibizlab.pms.core.util.ibizzentao.helper.UserTplHelper.class).delete(key);
     }
 
     @Override
