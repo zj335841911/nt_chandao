27bdd2ff6fbab3f50cbc52feb7d975dc8b44cbec
胡维 <huwei_0315@example.com>
2020-10-22 21:04:11 +0800

    删除禅道登录接口

--- ibizpms-util/src/main/java/cn/ibizlab/pms/util/client/IPMSFeignClient.java
+++ ibizpms-util/src/main/java/cn/ibizlab/pms/util/client/IPMSFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "${ibiz.ref.service.pms:pms-webapihw}", fallback = IPMSFeignFallback.class)
+@FeignClient(value = "${ibiz.ref.service.pms:pms-webapi}", fallback = IPMSFeignFallback.class)
 public interface IPMSFeignClient {
 
     /**
