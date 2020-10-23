36bbcf733cbbf9d72f774c301ee92b5207138a2e
ibizdev <ibizdev@ibizsys.net>
2020-10-23 15:16:30 +0800

    wulonghu 发布系统代码

--- app_Mob/src/widgets/task/mob-main-edit-form/mob-main-edit-form-base.vue
+++ app_Mob/src/widgets/task/mob-main-edit-form/mob-main-edit-form-base.vue
@@ -455,7 +455,25 @@
     :disabled="detailsModel.assignedto.disabled"
     :error="detailsModel.assignedto.error" 
     :isEmptyCaption="false">
-        
+        <app-mob-select-drop-down 
+    name='assignedto' 
+    valueitem='' 
+    style="" 
+    editortype="dropdown" 
+    :formState="formState"
+    :data="data"
+    :context="context"
+    :navigateContext ='{ "project": "%project%", "multiple": "0" } '
+    :navigateParam ='{ "project": "%project%", "multiple": "0" } '
+    :viewparams="viewparams"
+    :itemParam='{ }' 
+    :disabled="detailsModel.assignedto.disabled"
+    :service="service"
+    :acParams="{ }"
+    :value="data.assignedto" 
+    @formitemvaluechange="onFormItemValueChange"
+    @change="($event)=>this.data.assignedto = $event">
+</app-mob-select-drop-down>
 </app-form-item>
 
 
