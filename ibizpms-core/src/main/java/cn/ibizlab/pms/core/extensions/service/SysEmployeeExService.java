24dcce62769b51c4c878e168a381b067a4d8e822
胡维 <huwei_0315@example.com>
2020-10-23 13:36:52 +0800

    用户代码表逻辑调整

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/SysEmployeeExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/SysEmployeeExService.java
@@ -29,15 +29,27 @@ public class SysEmployeeExService extends SysEmployeeServiceImpl {
 
     @Override
     public Page<SysEmployee> searchDefault(SysEmployeeSearchContext context) {
+        log.info("SysEmployeeExService：searchDefault");
+        Map<String,Object> params = context.getParams();
 
+        if(params.get("srfparentdename") != null) {
+            log.info("SysEmployeeExService：searchDefault-" + params.get("srfparentdename").toString());
+            if("Bug".equals(params.get("srfparentdename"))) {
+                this.searchBugUser(context);
+            }else if("Task".equals(params.get("srfparentdename"))) {
+                this.searchTaskTeam(context);
+            }
+        }
         return super.searchDefault(context);
     }
 
     @Override
     public Page<SysEmployee> searchBugUser(SysEmployeeSearchContext context) {
         Map<String,Object> params = context.getParams();
+        log.info("SysEmployeeExService：searchBugUser");
         if(params.get("project") != null && !"0".equals(params.get("project"))) {
             // 项目团队
+            log.info("SysEmployeeExService：searchBugUser-" + params.get("project").toString());
             context.setN_username_in(getAccounts("project", params.get("project")));
         }
         Page<SysEmployee> page = super.searchDefault(context);
@@ -46,6 +58,7 @@ public class SysEmployeeExService extends SysEmployeeServiceImpl {
 
     @Override
     public Page<SysEmployee> searchProjectTeamM(SysEmployeeSearchContext context) {
+        log.info("SysEmployeeExService：searchProjectTeamM");
         Map<String,Object> params = context.getParams();
         if(params.get("srfparentkey") != null && "0".equals(params.get("srfparentkey"))) {
             // 项目团队
@@ -56,16 +69,19 @@ public class SysEmployeeExService extends SysEmployeeServiceImpl {
 
     @Override
     public Page<SysEmployee> searchProjectTeamUser(SysEmployeeSearchContext context) {
+        log.info("SysEmployeeExService：searchProjectTeamUser");
         Map<String,Object> params = context.getParams();
         if(params.get("project") != null && "0".equals(params.get("project"))) {
             // 项目团队
             context.setN_username_in(getAccounts("project", params.get("project")));
+
         }
         return super.searchDefault(context);
     }
 
     @Override
     public Page<SysEmployee> searchProjectTeamUser_Task(SysEmployeeSearchContext context) {
+        log.info("SysEmployeeExService：searchProjectTeamUser_Task");
         Map<String,Object> params = context.getParams();
         if(params.get("srfparentkey") != null && "0".equals(params.get("srfparentkey"))) {
             // 项目团队
@@ -76,12 +92,15 @@ public class SysEmployeeExService extends SysEmployeeServiceImpl {
 
     @Override
     public Page<SysEmployee> searchTaskTeam(SysEmployeeSearchContext context) {
+        log.info("SysEmployeeExService：searchTaskTeam");
         Map<String,Object> params = context.getParams();
-        if(params.get("multiple") != null && "0".equals(params.get("multiple"))) {
+        if((params.get("multiple") == null && params.get("project") != null) || (params.get("multiple") != null && "0".equals(params.get("multiple")))) {
             // 项目团队
+            log.info("SysEmployeeExService：SysEmployeeExService-" + params.get("project").toString());
             context.setN_username_in(getAccounts("project", params.get("project")));
         }else {
             // 任务团队
+            log.info("SysEmployeeExService：SysEmployeeExService-" + params.get("id").toString());
             context.setN_username_in(getAccounts("task", params.get("id")));
         }
         return super.searchDefault(context);
@@ -93,7 +112,7 @@ public class SysEmployeeExService extends SysEmployeeServiceImpl {
 
         for(Team team : list) {
             if(!"".equals(accounts)) {
-                accounts += ",";
+                accounts += ";";
             }
             accounts += team.getAccount();
         }
