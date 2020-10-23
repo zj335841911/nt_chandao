a4f8c1a02e8ec78b2f1a84853172850f646d9f26
ibizdev <ibizdev@ibizsys.net>
2020-10-23 14:13:58 +0800

    huwei_0806 发布系统代码

---
+++ app_Web/src/widgets/task/type-gantt-gantt/type-gantt-gantt.vue
@@ -0,0 +1,24 @@
+<template src="./type-gantt-gantt.html"/>
+<script lang='tsx'>
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core';
+import { TypeGanttGanttBase } from './type-gantt-gantt-base';
+import GanttElastic from "ibiz-gantt-elastic/src/GanttElastic.vue";
+ 
+
+/**
+ * gantt部件
+ *
+ * @export
+ * @class TypeGanttGantt
+ * @extends {TypeGanttGanttBase}
+ */
+@Component({
+    components: {
+        GanttElastic,
+ 
+    }
+})
+@VueLifeCycleProcessing()
+export default class TypeGanttGantt extends TypeGanttGanttBase { }
+</script>
