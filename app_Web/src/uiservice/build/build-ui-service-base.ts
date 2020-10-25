81236a415d07b47e09bf979d89757a842cd56e09
ibizdev <ibizdev@ibizsys.net>
2020-10-25 13:39:54 +0800

    huwei_0806 发布系统代码

--- app_Web/src/uiservice/build/build-ui-service-base.ts
+++ app_Web/src/uiservice/build/build-ui-service-base.ts
@@ -186,8 +186,8 @@ export default class BuildUIServiceBase extends UIService {
         let parentContext:any = {};
         let parentViewParam:any = {};
         const _this: any = actionContext;
-        Object.assign(context,{PROJECT:"%project%"});
-        Object.assign(params,{project:"%project%"});
+        Object.assign(context,{BUILD:"%id%",PROJECT:"%project%",PRODUCT:"%product%"});
+        Object.assign(params,{product:"%product%",project:"%project%",build:"%id%"});
         const _args: any[] = Util.deepCopy(args);
         const actionTarget: string | null = 'SINGLEKEY';
         Object.assign(context, { build: '%build%' });
