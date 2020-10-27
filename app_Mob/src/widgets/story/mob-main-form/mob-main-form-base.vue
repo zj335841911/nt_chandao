1201e46c8b180761d14aeeffb956c92226261f6d
ibizdev <ibizdev@ibizsys.net>
2020-10-27 16:10:37 +0800

    KK951314 发布系统代码

--- app_Mob/src/widgets/story/mob-main-form/mob-main-form-base.vue
+++ app_Mob/src/widgets/story/mob-main-form/mob-main-form-base.vue
@@ -559,7 +559,7 @@
     :context="context" 
     :viewparams="viewparams" 
     :navigateContext ='{ } ' 
-    :navigateParam ='{ } ' 
+    :navigateParam ='{ "n_extra_eq": "%version%", "objecttype": "story" } ' 
     :ignorefieldvaluechange="ignorefieldvaluechange" 
     :data="JSON.stringify(this.data)"  
     @drdatasaved="drdatasaved($event)"/>
