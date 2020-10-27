26207cc3407275d2cf16df21196a6407246604b9
KK <57750083+zhangkang1314@users.noreply.github.com>
2020-10-27 17:13:28 +0800

    历史组件真实姓名

--- app_Mob/src/ibiz-core/components/app-history-list/app-history-list.vue
+++ app_Mob/src/ibiz-core/components/app-history-list/app-history-list.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
-    <div class="onecontent" ref="onecontent">   
+    <div  class="onecontent" ref="onecontent">   
       <div v-for="item in items" :key="item.id" class="oneitem" ref="oneitem">
             <div class="header"><span>{{item.date}}</span> <span>{{item.method}}</span></div>
             <div v-if="item.item.length > 0" class="footer">
               <div v-for="(detail,index) in item.item" :key="index">
                 <span>{{$t('by')}} </span>
-                <strong>{{item.actor}}</strong> 
+                <strong v-if="isGetUser">{{item.actorText}}</strong> 
                 {{item.method}} 
                 <span v-if="item.actions !== 'closed'">
                   <span v-if="item.actions !=='suspended'">
@@ -30,7 +30,7 @@
             </div>
             <div v-else class="footer">
               <span>{{$t('by')}} </span>
-                <strong>{{item.actor}}</strong> 
+                <strong v-if="isGetUser">{{item.actorText}}</strong> 
                 {{item.method}} 
                 <span v-if="item.actions !== 'closed'">
                   <span v-if="item.actions !=='suspended'">
@@ -63,7 +63,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop,Watch } from 'vue-property-decorator';
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import { CodeListService } from "@/ibiz-core";
 
 
@@ -104,7 +104,7 @@ export default class APPHistoryList extends Vue {
      */
     @Prop() public items?: Array<any>;
 
-    public listItems:Array<any> = [];
+    public listItems: Array<any> = [];
 
     /**
      * 传入数据itemNameDetail
@@ -112,7 +112,7 @@ export default class APPHistoryList extends Vue {
      * @type {any}
      * @memberof APPHistoryList
      */
-    @Prop() public itemNameDetail?:any;
+    @Prop() public itemNameDetail?: any;
 
     /**
      * 监听itemNameDetail
@@ -121,8 +121,8 @@ export default class APPHistoryList extends Vue {
      * @memberof APPHistoryList
      */
     @Watch('itemNameDetail')
-    itemNameDetailChange(){
-      this.handler();
+    itemNameDetailChange() {
+        this.handler();
     }
 
     /**
@@ -131,15 +131,15 @@ export default class APPHistoryList extends Vue {
      * @returns {void}
      * @memberof APPHistoryList
      */
-    @Watch('items',{immediate: true, deep: true})
-    itemsChange(){
-      if (this.items && this.items.length !== 0) {
-        this.text = '查看更多记录';
-      } else {
-        this.text = '暂无更多记录';
-      }
-      this.handler();
-      this.setHeight();
+    @Watch('items', { immediate: true, deep: true })
+    itemsChange() {
+        if (this.items && this.items.length !== 0) {
+            this.text = '查看更多记录';
+        } else {
+            this.text = '暂无更多记录';
+        }
+        this.handler();
+        this.setHeight();
     }
 
     /**
@@ -148,12 +148,12 @@ export default class APPHistoryList extends Vue {
      * @returns {void}
      * @memberof APPHistoryList
      */
-    public getCodeList(tag:string,type:string,value:any){
-      if (type == 'STATIC') {
-        let infos:any = this.$store.getters.getCodeListItems(tag);
-        let info:any = infos.find((v:any)=>v.value == value);
-        return info;
-      }
+    public getCodeList(tag: string, type: string, value: any) {
+        if (type == 'STATIC') {
+            let infos: any = this.$store.getters.getCodeListItems(tag);
+            let info: any = infos.find((v: any) => v.value == value);
+            return info;
+        }
     }
 
     /**
@@ -163,36 +163,47 @@ export default class APPHistoryList extends Vue {
      * @memberof APPHistoryList
      */
     public handler() {
-      if (this.items) {
-        this.items.forEach((v:any) => {
-          let file:string = "";
-          let method:string = "";
-          if (v.actions) {
-            let info:any = this.getCodeList(this.codeListStandard.actions.tag,'STATIC',v.actions);
-              v.method = info.text;
-              method = info.text;
-              if (v.actions === 'closed') {
-                v.item.length = 1;
-              }
-          }
-          if (v.item.length > 0) {
-            v.item.forEach((i:any) => {
-              i.file = (this.$t(v.objecttype+'.fields.'+i.field.toLowerCase()) as string);
-              v.old = i.old;
-              v.new = i.ibiznew;
-            });
-          }
-        })
-      }
-    }     
+        if (this.items) {
+            this.items.forEach((v: any) => {
+                let file: string = "";
+                let method: string = "";
+                if (v.actions) {
+                    let info: any = this.getCodeList(this.codeListStandard.actions.tag, 'STATIC', v.actions);
+                    v.method = info.text;
+                    method = info.text;
+                    if (v.actions === 'closed') {
+                        v.item.length = 1;
+                    }
+                }
+                if (v.item.length > 0) {
+                    v.item.forEach((i: any) => {
+                        i.file = (this.$t(v.objecttype + '.fields.' + i.field.toLowerCase()) as string);
+                        v.old = i.old;
+                        v.new = i.ibiznew;
+                    });
+                }
+            })
+            this.setUerReName();
+        }
+    }
 
+    /**
+     * 设置用户真实姓名
+     */
+    public setUerReName() {
+        if (this.items) {
+            for (let index = 0; index < this.items.length; index++) {
+                this.items[index].actorText = this.getUserReName(this.items[index].actor);
+            }
+        }
+    }
     /**
      * 按钮文本
      *
      * @type {String}
      * @memberof APPHistoryList
      */
-    public text:string = '查看更多记录';
+    public text: string = '查看更多记录';
 
     /**
      * 文本切换
@@ -200,22 +211,56 @@ export default class APPHistoryList extends Vue {
      * @type {Boolean}
      * @memberof APPHistoryList
      */
-    public isShow:boolean = false;
+    public isShow: boolean = false;
+
+    /**
+     * 是否获取用户数据
+     *
+     * @returns {void}
+     * @memberof APPHistoryList
+     */
+    public isGetUser = false;
+
+    /**
+     * 用户数据
+     *
+     * @returns {void}
+     * @memberof APPHistoryList
+     */
+    public userData: any[] = [];
+    @Watch('userData')
+    onUserDataChange(newData: any, oldData: any) {
+        if (newData.length > 0) {
+            this.isGetUser = true;
+        }
+        this.handler();
+    }
+
+    /**
+     * 获取用户真实姓名
+     *
+     * @returns {void}
+     * @memberof APPHistoryList
+     */
+    public getUserReName(id: string): string {
+        let index = this.userData.findIndex((item) => { return item.value == id })
+        return index > -1 ? this.userData[index].label : "";
+    }
 
     /**
      * 代码表规范
      *
      * @memberof APPHistoryList
      */
-    public codeListStandard :any= {
-      "actions":{
-        type:"static",
-        tag:"Action__type"
-      },
-      "old":{
-        type:"static",
-        tag:"Product__status"
-      }
+    public codeListStandard: any = {
+        "actions": {
+            type: "static",
+            tag: "Action__type"
+        },
+        "old": {
+            type: "static",
+            tag: "Product__status"
+        }
     }
 
     /**
@@ -224,7 +269,7 @@ export default class APPHistoryList extends Vue {
      * @type {Number}
      * @memberof APPHistoryList
      */
-    public num:number = 3;
+    public num: number = 3;
 
     /**
      * 初始高度
@@ -232,7 +277,7 @@ export default class APPHistoryList extends Vue {
      * @type {Number}
      * @memberof APPHistoryList
      */
-    public startHeig:number = 0;
+    public startHeig: number = 0;
 
     /**
      * 终止高度
@@ -240,7 +285,7 @@ export default class APPHistoryList extends Vue {
      * @type {Number}
      * @memberof APPHistoryList
      */
-    public endHeig:number = 0;
+    public endHeig: number = 0;
 
     /**
      * 点击展开
@@ -248,25 +293,24 @@ export default class APPHistoryList extends Vue {
      * @returns {void}
      * @memberof APPHistoryList
      */
-    public loadMore():void {
-          this.isShow = !this.isShow;
-          this.text = this.isShow?  '收起':'查看更多记录';
-          let ele:any =  document.querySelector('.onecontent');
-          let ite:any =  document.querySelectorAll('.oneitem');
-          this.startHeig = 0;
-          this.endHeig = 0;
-            for (let i = 0; i < this.num; i++) {
-              if (ite[i] != undefined) {
+    public loadMore(): void {
+        this.isShow = !this.isShow;
+        this.text = this.isShow ? '收起' : '查看更多记录';
+        let ele: any = document.querySelector('.onecontent');
+        let ite: any = document.querySelectorAll('.oneitem');
+        this.startHeig = 0;
+        this.endHeig = 0;
+        for (let i = 0; i < this.num; i++) {
+            if (ite[i] != undefined) {
                 this.startHeig += ite[i].offsetHeight;
-              }
             }
-            for (let i = 0; i <= ite.length; i++) {
-              if (ite[i] != undefined) {              
+        }
+        for (let i = 0; i <= ite.length; i++) {
+            if (ite[i] != undefined) {
                 this.endHeig += ite[i].offsetHeight;
-              }
-            }  
-          ele.style.height = this.isShow?this.endHeig+'px':  + this.startHeig+'px';
-
+            }
+        }
+        ele.style.height = this.isShow ? this.endHeig + 'px' : + this.startHeig + 'px';
     }
 
     /**
@@ -275,8 +319,19 @@ export default class APPHistoryList extends Vue {
      * @returns {void}
      * @memberof APPHistoryList
      */
-    public created(){
-      this.handler();
+    public created() {
+        this.handler();
+        this.getUserData();
+    }
+
+    /**
+     * 获取用户数据
+     *
+     * @returns {void}
+     * @memberof APPHistoryList
+     */
+    public async getUserData() {
+        this.userData = await this.codeListService.getItems("UserRealName");
     }
 
     /**
@@ -286,26 +341,26 @@ export default class APPHistoryList extends Vue {
      * @memberof APPHistoryList
      */
     public setHeight() {
-      let ele:any =  document.querySelector('.onecontent');
-      let ite:any =  this.$refs.oneitem;
-      if (ite !== undefined) {
-        for(let i:any = 0; i < this.num; i++){
-          if (ite[i] !== undefined) {
-            this.startHeig += ite[i].offsetHeight;
-          }
+        let ele: any = document.querySelector('.onecontent');
+        let ite: any = this.$refs.oneitem;
+        if (ite !== undefined) {
+            for (let i: any = 0; i < this.num; i++) {
+                if (ite[i] !== undefined) {
+                    this.startHeig += ite[i].offsetHeight;
+                }
+            }
         }
-      }
-      if (ele && ele.style) {
-        ele.style.height = this.startHeig + 'px';
-      }
-      const userAgent:string = navigator.userAgent;
-      let isIOS = (userAgent: string) => /iphone/i.test(userAgent) || /ipod/i.test(userAgent) || /iPad/i.test(userAgent);
-      if (isIOS(userAgent)) {
-        let loadMore:any = this.$refs.loadMore;
-        if (loadMore) {
-          loadMore.style.marginBottom = "10px";
+        if (ele && ele.style) {
+            ele.style.height = this.startHeig + 'px';
         }
-      }
+        const userAgent: string = navigator.userAgent;
+        let isIOS = (userAgent: string) => /iphone/i.test(userAgent) || /ipod/i.test(userAgent) || /iPad/i.test(userAgent);
+        if (isIOS(userAgent)) {
+            let loadMore: any = this.$refs.loadMore;
+            if (loadMore) {
+                loadMore.style.marginBottom = "10px";
+            }
+        }
     }
 
     /**
@@ -314,9 +369,8 @@ export default class APPHistoryList extends Vue {
      * @returns {void}
      * @memberof APPHistoryList
      */
-    public mounted(){
-      this.setHeight();
-      // console.log("this.items",this.items)
+    public mounted() {
+        this.setHeight();
     }
 
 }
