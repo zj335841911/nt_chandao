ac88b4ba992436737c3255c4033a9a501d317cd6
KK <57750083+zhangkang1314@users.noreply.github.com>
2020-10-27 15:17:23 +0800

    pms附件列表插件

--- app_Mob/src/user-register.ts
+++ app_Mob/src/user-register.ts
@@ -6,5 +6,7 @@ export const UserComponent = {
         v.component('app-mob-rich-text-editor-pms',() => import('@/ibiz-core/components/app-mob-rich-text-editor-pms/app-mob-rich-text-editor-pms.vue'));
         // 富文本编辑器（edit）
         v.component('app-rich-text-pms',() => import('@/ibiz-core/components/app-rich-text-pms/app-rich-text-pms.vue'));
+        // 文件列表插件
+        v.component('app-pms-upload-list',() => import('@/ibiz-core/components/app-pms-upload-list/app-pms-upload-list.vue'));
     }
 };
