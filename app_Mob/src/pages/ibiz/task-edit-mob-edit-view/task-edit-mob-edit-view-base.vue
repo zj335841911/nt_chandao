b55f459534883b0c66ebef0d985f9a8c9b493b83
ibizdev <ibizdev@ibizsys.net>
2020-10-28 14:44:19 +0800

    wulonghu 发布系统代码，后台体系[后台服务体系]，前端应用[iBiz软件生产管理（Mob）]

--- app_Mob/src/pages/ibiz/task-edit-mob-edit-view/task-edit-mob-edit-view-base.vue
+++ app_Mob/src/pages/ibiz/task-edit-mob-edit-view/task-edit-mob-edit-view-base.vue
@@ -178,7 +178,7 @@ export default class TaskEditMobEditViewBase extends Vue {
      * @type {*}
      * @memberof TaskEditMobEditViewBase
      */
-    protected navContext: any = { 'objecttype': 'task', 'srfparentkey': '%task%' };
+    protected navContext: any = {};
 
     /**
      * 视图导航参数
@@ -187,7 +187,7 @@ export default class TaskEditMobEditViewBase extends Vue {
      * @type {*}
      * @memberof TaskEditMobEditViewBase
      */
-    protected navParam: any = { 'srfparentkey': '%task%', 'objecttype': 'task' };
+    protected navParam: any = { 'srfparentkey': '%task%', 'objecttype': 'task', 'objectid': '%id%', 'version': '1' };
 
     /**
      * 视图模型数据
