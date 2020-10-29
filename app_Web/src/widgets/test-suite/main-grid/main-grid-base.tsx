f01f9f6aadfbec258e918a334bd68a03d427c377
ibizdev <ibizdev@ibizsys.net>
2020-10-29 14:31:32 +0800

    tony001 发布系统代码，后台体系[后台服务体系]，前端应用[iBiz软件生产管理]

--- app_Web/src/widgets/test-suite/main-grid/main-grid-base.tsx
+++ app_Web/src/widgets/test-suite/main-grid/main-grid-base.tsx
@@ -6,5 +6,5 @@ Tip: If the failing expression is known to be legally refer to something that's 
 
 ----
 FTL stack trace ("~" means nesting-related):
-	- Failed at: #if ((hasDEExport && column.getPSCode...  [in template "TEMPLCODE_en_US" at line 568, column 5]
+	- Failed at: #if column.getPSCodeList?? && column...  [in template "TEMPLCODE_en_US" at line 569, column 6]
 ----
