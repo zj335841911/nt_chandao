e1b9107d0dc0982dcda137bdcff20770d0d47f06
ibizdev <ibizdev@ibizsys.net>
2020-10-29 14:34:56 +0800

    tony001 发布系统代码，后台体系[后台服务体系]，前端应用[iBiz软件生产管理]

--- app_Web/src/widgets/story/main-build-sub-grid/main-build-sub-grid-base.tsx
+++ app_Web/src/widgets/story/main-build-sub-grid/main-build-sub-grid-base.tsx
@@ -1,10 +1,462 @@
-!!!!模版产生代码错误:----
-Tip: It's the step after the last dot that caused this error, not those before it.
-----
-Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
-----
+import { Prop, Provide, Emit, Model } from 'vue-property-decorator';
+import { Subject, Subscription } from 'rxjs';
+import { UIActionTool,Util,ViewTool } from '@/utils';
+import { Watch, GridControlBase } from '@/studio-core';
+import StoryService from '@/service/story/story-service';
+import Main_BuildSubService from './main-build-sub-grid-service';
+import StoryUIService from '@/uiservice/story/story-ui-service';
+import { FormItemModel } from '@/model/form-detail';
 
-----
-FTL stack trace ("~" means nesting-related):
-	- Failed at: #if column.getPSCodeList?? && column...  [in template "TEMPLCODE_en_US" at line 569, column 6]
-----
+
+/**
+ * grid部件基类
+ *
+ * @export
+ * @class GridControlBase
+ * @extends {Main_BuildSubGridBase}
+ */
+export class Main_BuildSubGridBase extends GridControlBase {
+
+    /**
+     * 获取部件类型
+     *
+     * @protected
+     * @type {string}
+     * @memberof Main_BuildSubGridBase
+     */
+    protected controlType: string = 'GRID';
+
+    /**
+     * 建构部件服务对象
+     *
+     * @type {Main_BuildSubService}
+     * @memberof Main_BuildSubGridBase
+     */
+    public service: Main_BuildSubService = new Main_BuildSubService({ $store: this.$store });
+
+    /**
+     * 实体服务对象
+     *
+     * @type {StoryService}
+     * @memberof Main_BuildSubGridBase
+     */
+    public appEntityService: StoryService = new StoryService({ $store: this.$store });
+
+    /**
+     * 应用实体名称
+     *
+     * @protected
+     * @type {string}
+     * @memberof Main_BuildSubGridBase
+     */
+    protected appDeName: string = 'story';
+
+    /**
+     * 应用实体中文名称
+     *
+     * @protected
+     * @type {string}
+     * @memberof Main_BuildSubGridBase
+     */
+    protected appDeLogicName: string = '需求';
+
+    /**
+     * 界面UI服务对象
+     *
+     * @type {StoryUIService}
+     * @memberof Main_BuildSubBase
+     */  
+    public appUIService:StoryUIService = new StoryUIService(this.$store);
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public grid_uagridcolumn1_u4de0e38_click(params: any = {}, tag?: any, $event?: any) {
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:StoryUIService  = new StoryUIService();
+        curUIService.Story_buildUnlinkStory(datas,contextJO, paramJO,  $event, xData,this,"Story");
+    }
+
+
+    /**
+     * 界面行为模型
+     *
+     * @type {*}
+     * @memberof Main_BuildSubBase
+     */  
+    public ActionModel: any = {
+        buildUnlinkStory: { name: 'buildUnlinkStory',disabled: false, visabled: true,noprivdisplaymode:1,dataaccaction: 'SRFUR__STORY_UNLP_BUT', actiontarget: 'SINGLEKEY'}
+    };
+
+    /**
+     * 主信息表格列
+     *
+     * @type {string}
+     * @memberof Main_BuildSubBase
+     */  
+    public majorInfoColName:string = "title";
+
+    /**
+     * 本地缓存标识
+     *
+     * @protected
+     * @type {string}
+     * @memberof Main_BuildSubBase
+     */
+    protected localStorageTag: string = 'zt_story_main_buildsub_grid';
+
+    /**
+     * 排序方向
+     *
+     * @type {string}
+     * @memberof Main_BuildSubGridBase
+     */
+    public minorSortDir: string = 'DESC';
+
+    /**
+     * 排序字段
+     *
+     * @type {string}
+     * @memberof Main_BuildSubGridBase
+     */
+    public minorSortPSDEF: string = 'id';
+
+    /**
+     * 所有列成员
+     *
+     * @type {any[]}
+     * @memberof Main_BuildSubGridBase
+     */
+    public allColumns: any[] = [
+        {
+            name: 'id',
+            label: 'ID',
+            langtag: 'entities.story.main_buildsub_grid.columns.id',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'pri',
+            label: 'P',
+            langtag: 'entities.story.main_buildsub_grid.columns.pri',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'title',
+            label: '需求名称',
+            langtag: 'entities.story.main_buildsub_grid.columns.title',
+            show: true,
+            unit: 'STAR',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'modulename',
+            label: '所属模块名称',
+            langtag: 'entities.story.main_buildsub_grid.columns.modulename',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'openedby',
+            label: '创建',
+            langtag: 'entities.story.main_buildsub_grid.columns.openedby',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'estimate',
+            label: '预计',
+            langtag: 'entities.story.main_buildsub_grid.columns.estimate',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'status',
+            label: '状态',
+            langtag: 'entities.story.main_buildsub_grid.columns.status',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'stage',
+            label: '阶段',
+            langtag: 'entities.story.main_buildsub_grid.columns.stage',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+        {
+            name: 'uagridcolumn1',
+            label: '操作',
+            langtag: 'entities.story.main_buildsub_grid.columns.uagridcolumn1',
+            show: true,
+            unit: 'PX',
+            isEnableRowEdit: false,
+            enableCond: 3 ,
+        },
+    ]
+
+    /**
+     * 获取表格行模型
+     *
+     * @type {*}
+     * @memberof Main_BuildSubGridBase
+     */
+    public getGridRowModel(){
+        return {
+          srfkey: new FormItemModel(),
+        }
+    }
+
+    /**
+     * 属性值规则
+     *
+     * @type {*}
+     * @memberof Main_BuildSubGridBase
+     */
+    public rules: any = {
+        srfkey: [
+            { required: false, validator: (rule:any, value:any, callback:any) => { return (rule.required && (value === null || value === undefined || value === "")) ? false : true;}, message: '编号 值不能为空', trigger: 'change' },
+            { required: false, validator: (rule:any, value:any, callback:any) => { return (rule.required && (value === null || value === undefined || value === "")) ? false : true;}, message: '编号 值不能为空', trigger: 'blur' },
+        ],
+    }
+
+    /**
+     * 获取对应列class
+     *
+     * @type {*}
+     * @memberof Main_BuildSubBase
+     */
+    public hasRowEdit: any = {
+        'id':false,
+        'pri':false,
+        'title':false,
+        'modulename':false,
+        'openedby':false,
+        'estimate':false,
+        'status':false,
+        'stage':false,
+        'uagridcolumn1':false,
+    };
+
+    /**
+     * 获取对应列class
+     *
+     * @param {*} $args row 行数据，column 列数据，rowIndex 行索引，列索引
+     * @returns {void}
+     * @memberof Main_BuildSubBase
+     */
+    public getCellClassName(args: {row: any, column: any, rowIndex: number, columnIndex: number}): any {
+        return ( this.hasRowEdit[args.column.property] && this.actualIsOpenEdit ) ? "edit-cell" : "info-cell";
+    }
+
+
+    /**
+     * 是否为实体导出对象
+     *
+     * @protected
+     * @type {boolean}
+     * @memberof Main_BuildSubGridBase
+     */
+    protected isDeExport: boolean = true;
+
+    /**
+     * 所有导出列成员
+     *
+     * @type {any[]}
+     * @memberof Main_BuildSubGridBase
+     */
+    public allExportColumns: any[] = [
+        {
+            name: 'id',
+            label: 'ID',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.id',
+            show: true,
+        },
+        {
+            name: 'pri',
+            label: 'P',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.pri',
+            show: true,
+        },
+        {
+            name: 'title',
+            label: '需求名称',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.title',
+            show: true,
+        },
+        {
+            name: 'plan',
+            label: '计划',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.plan',
+            show: true,
+        },
+        {
+            name: 'openedby',
+            label: '创建',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.openedby',
+            show: true,
+        },
+        {
+            name: 'assignedto',
+            label: '指派',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.assignedto',
+            show: true,
+        },
+        {
+            name: 'estimate',
+            label: '预计',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.estimate',
+            show: true,
+        },
+        {
+            name: 'status',
+            label: '状态',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.status',
+            show: true,
+        },
+        {
+            name: 'stage',
+            label: '阶段',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.stage',
+            show: true,
+        },
+        {
+            name: 'modulename',
+            label: '所属模块名称',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.modulename',
+            show: true,
+        },
+        {
+            name: 'module',
+            label: '所属模块',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.module',
+            show: true,
+        },
+        {
+            name: 'isfavorites',
+            label: '是否收藏',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.isfavorites',
+            show: true,
+        },
+        {
+            name: 'ischild',
+            label: '是否可以细分',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.ischild',
+            show: true,
+        },
+        {
+            name: 'product',
+            label: '所属产品',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.product',
+            show: true,
+        },
+        {
+            name: 'prodoctname',
+            label: '产品名称',
+            langtag: 'entities.story.main_buildsub_grid.exportColumns.prodoctname',
+            show: true,
+        },
+    ]
+
+    /**
+     * 导出数据格式化
+     *
+     * @param {*} filterVal
+     * @param {*} jsonData
+     * @param {any[]} [codelistColumns=[]]
+     * @returns {Promise<any>}
+     * @memberof Main_BuildSubGridBase
+     */
+    public async formatExcelData(filterVal: any, jsonData: any, codelistColumns?: any[]): Promise<any> {
+        return super.formatExcelData(filterVal, jsonData, [
+        ]);
+    }
+
+
+    /**
+     * 界面行为
+     *
+     * @param {*} row
+     * @param {*} tag
+     * @param {*} $event
+     * @memberof Main_BuildSubGridBase
+     */
+	public uiAction(row: any, tag: any, $event: any): void {
+        $event.stopPropagation();
+        if(Object.is('buildUnlinkStory', tag)) {
+            this.grid_uagridcolumn1_u4de0e38_click(row, tag, $event);
+        }
+    }
+
+    /**
+     * 更新默认值
+     * @param {*}  row 行数据
+     * @memberof Main_BuildSubBase
+     */
+    public updateDefault(row: any){                    
+    }
+
+    /**
+     * 计算数据对象类型的默认值
+     * @param {string}  action 行为
+     * @param {string}  param 默认值参数
+     * @param {*}  data 当前行数据
+     * @memberof Main_BuildSubBase
+     */
+    public computeDefaultValueWithParam(action:string,param:string,data:any){
+        if(Object.is(action,"UPDATE")){
+            const nativeData:any = this.service.getCopynativeData();
+            if(nativeData && (nativeData instanceof Array) && nativeData.length >0){
+                let targetData:any = nativeData.find((item:any) =>{
+                    return item.id === data.srfkey;
+                })
+                if(targetData){
+                    return targetData[param]?targetData[param]:null;
+                }else{
+                    return null;
+                }
+            }else{
+                return null;
+            }
+        }else{
+           return this.service.getRemoteCopyData()[param]?this.service.getRemoteCopyData()[param]:null;
+        }
+    }
+
+
+}
