1783891a16d7cb18990ae57134c35a36700967fc
Shine-zwj <1917095344@qq.com>
2020-10-29 10:10:34 +0800

    产品，项目，测试列表刷新

--- app_Web/src/widgets/product/test-sidebar-list/test-sidebar-list.vue
+++ app_Web/src/widgets/product/test-sidebar-list/test-sidebar-list.vue
@@ -18,5 +18,20 @@ import { TestSidebarListBase } from './test-sidebar-list-base';
     }
 })
 @VueLifeCycleProcessing()
-export default class TestSidebarList extends TestSidebarListBase { }
+export default class TestSidebarList extends TestSidebarListBase { 
+    /**
+     * 刷新
+     *
+     * @param {*} [args]
+     * @memberof TestSidebarList
+     */
+    public refresh(args?: any) {
+        if(args){
+            this.load(args);
+        }else{
+            this.isAddBehind = true;
+            this.load(args);
+        }
+    }
+}
 </script>
