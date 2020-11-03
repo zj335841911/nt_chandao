b3cc5876a74a69c7ec37b6b4ec6a51cfba040943
ibizdev <ibizdev@ibizsys.net>
2020-11-03 09:51:49 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/job/TodoServiceJobHandler.java
@@ -0,0 +1,16 @@
+package cn.ibizlab.pms.core.util.job;
+
+import com.baomidou.jobs.api.JobsResponse;
+import com.baomidou.jobs.exception.JobsException;
+import com.baomidou.jobs.handler.IJobsHandler;
+import org.springframework.stereotype.Component;
+
+@Component("TodoServiceJobHandler")
+public class TodoServiceJobHandler implements  IJobsHandler {
+
+    @Override
+    public JobsResponse execute(String tenantId, String param) throws JobsException {
+        System.out.println("执行 DemoJobHandler tenantId=" + tenantId + ",param=" + param);
+        return JobsResponse.ok();
+    }
+}
