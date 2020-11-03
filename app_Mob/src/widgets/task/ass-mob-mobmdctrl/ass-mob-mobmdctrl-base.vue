2655bb8f4445cd6c2b62f40157934221a5bf4414
ibizdev <ibizdev@ibizsys.net>
2020-11-03 16:36:43 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/task/ass-mob-mobmdctrl/ass-mob-mobmdctrl-base.vue
+++ app_Mob/src/widgets/task/ass-mob-mobmdctrl/ass-mob-mobmdctrl-base.vue
@@ -356,7 +356,7 @@ export default class AssMobBase extends Vue implements ControlInterface {
         const _this: any = this;
         let contextJO: any = {};
         let paramJO: any = {};
-        
+        Object.assign(paramJO, {});
         xData = this;
         if (_this.getDatas && _this.getDatas instanceof Function) {
             datas = [..._this.getDatas()];
