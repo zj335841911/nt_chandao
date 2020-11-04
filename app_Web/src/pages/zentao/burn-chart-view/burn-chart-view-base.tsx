7276635e4cd1cbb159d72b858f3d5a7bda71406e
ibizdev <ibizdev@ibizsys.net>
2020-11-04 18:08:21 +0800

    tony001 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/zentao/burn-chart-view/burn-chart-view-base.tsx
+++ app_Web/src/pages/zentao/burn-chart-view/burn-chart-view-base.tsx
@@ -215,5 +215,37 @@ export class BurnChartViewBase extends ChartViewBase {
 
 
 
+    /**
+     * 是否启用快速分组
+     *
+     * @type {boolean}
+     * @memberof BurnChartViewBase
+     */
+    public isEnableQuickGroup: boolean = true;
+
+    /**
+     * 加载快速分组模型
+     *
+     * @protected
+     * @memberof BurnChartViewBase
+     */
+    protected loadQuickGroupModel(): void {
+        const quickGroupCodeList: any = { tag: 'BurnQuickpacket', codelistType: 'STATIC' };
+        if(quickGroupCodeList.tag && Object.is(quickGroupCodeList.codelistType, "STATIC")) {
+            const codelist = this.$store.getters.getCodeList(quickGroupCodeList.tag);
+            if (codelist) {
+                this.quickGroupModel = [...this.handleDynamicData(JSON.parse(JSON.stringify(codelist.items)))];
+            } else {
+                console.log(`----${quickGroupCodeList.tag}----代码表不存在`);
+            }
+        } else if(quickGroupCodeList.tag && Object.is(quickGroupCodeList.codelistType, "DYNAMIC")) {
+            this.codeListService.getItems(quickGroupCodeList.tag, {}, {}).then((res: any) => {
+                this.quickGroupModel = res;
+            }).catch((error:any) => {
+                console.log(`----${quickGroupCodeList.tag}----代码表不存在`);
+            });
+        }
+    }
+
 
 }
