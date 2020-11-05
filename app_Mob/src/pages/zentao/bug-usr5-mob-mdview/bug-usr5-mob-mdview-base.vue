80c8569c56c23e03416df604970117254ddacaf3
ibizdev <ibizdev@ibizsys.net>
2020-11-05 13:55:55 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/pages/zentao/bug-usr5-mob-mdview/bug-usr5-mob-mdview-base.vue
+++ app_Mob/src/pages/zentao/bug-usr5-mob-mdview/bug-usr5-mob-mdview-base.vue
@@ -190,7 +190,7 @@ export default class BugUsr5MobMDViewBase extends Vue {
      * @type {*}
      * @memberof BugUsr5MobMDViewBase
      */
-    protected navContext: any = {};
+    protected navContext: any = { 'project': '%project%', 'objecttype': 'build', 'srfparentkey': '%build%' };
 
     /**
      * 视图导航参数
@@ -199,7 +199,7 @@ export default class BugUsr5MobMDViewBase extends Vue {
      * @type {*}
      * @memberof BugUsr5MobMDViewBase
      */
-    protected navParam: any = {};
+    protected navParam: any = { 'srfparentkey': '%build%', 'project': '%project%', 'objecttype': 'build' };
 
     /**
      * 视图模型数据
