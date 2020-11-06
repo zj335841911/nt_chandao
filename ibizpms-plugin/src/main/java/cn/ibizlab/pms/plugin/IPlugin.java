551826019830ba4e81d8702e1183bcbea7b21b4e
ibizdev <ibizdev@ibizsys.net>
2020-11-06 16:06:18 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- ibizpms-plugin/src/main/java/cn/ibizlab/pms/plugin/IPlugin.java
+++ ibizpms-plugin/src/main/java/cn/ibizlab/pms/plugin/IPlugin.java
@@ -1,8 +1,10 @@
-package cn.ibizlab.pms.plugin;
+!!!!模版产生代码错误:----
+Tip: Maybe using obj.something instead of obj.getSomething will yield the desired value.
+----
+Tip: For sequences/collections (lists and such) use "?seq_contains" instead.
+----
 
-/**
- * 插件
- */
-public interface IPlugin {
-
-}
+----
+FTL stack trace ("~" means nesting-related):
+	- Failed at: #if pub.getUserTag?contains(";PLUGIN;")  [in template "CODETEMPL_en_US" at line 1, column 1]
+----
