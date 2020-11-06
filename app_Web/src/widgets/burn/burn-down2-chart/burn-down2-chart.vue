46c76ef15365432d0ffecbadcc33bb582a926808
Shine-zwj <1917095344@qq.com>
2020-11-06 16:57:48 +0800

    燃尽图间隔

--- app_Web/src/widgets/burn/burn-down2-chart/burn-down2-chart.vue
+++ app_Web/src/widgets/burn/burn-down2-chart/burn-down2-chart.vue
@@ -18,5 +18,41 @@ import { BurnDown2ChartBase } from './burn-down2-chart-base';
     }
 })
 @VueLifeCycleProcessing()
-export default class BurnDown2Chart extends BurnDown2ChartBase { }
+export default class BurnDown2Chart extends BurnDown2ChartBase {
+
+    /**
+     * 获取图表数据
+     * 
+     * @returns {*} 
+     * @memberof ChartBase
+     */
+    public load(opt?:any) {
+        let _this = this;
+        const arg: any = { ...opt };
+        const parentdata: any = {};
+        this.$emit('beforeload', parentdata);
+        Object.assign(arg, parentdata);
+        let tempViewParams:any = parentdata.viewparams?parentdata.viewparams:{};
+        Object.assign(tempViewParams,JSON.parse(JSON.stringify(this.viewparams)));
+        Object.assign(arg,{viewparams:tempViewParams});
+        Object.assign(arg,{page:0,size:1000});
+        this.service.search(this.fetchAction,JSON.parse(JSON.stringify(this.context)),arg,this.showBusyIndicator).then((res) => {
+            if (res) {
+                if(parentdata && parentdata.isweekend){
+                    this.chartOption.xAxis.forEach((xAix: any)=>{
+                        xAix.axisLabel = {interval: parseInt(parentdata.isweekend)}
+                    })
+                }else{
+                    this.chartOption.xAxis.forEach((xAix: any)=>{
+                        xAix.axisLabel = {interval: 0}
+                    })
+                }
+                this.transformToBasicChartSetData(res.data,(codelist:any) =>{_this.drawCharts(codelist)});
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+    
+}
 </script>
