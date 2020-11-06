7c10b7df6f307b5a1543c9e28f1c61086874253c
ibizdev <ibizdev@ibizsys.net>
2020-11-06 15:51:50 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/story/mob-duo-shu-ju-choice-story-mobmdctrl/mob-duo-shu-ju-choice-story-mobmdctrl.vue
+++ app_Mob/src/widgets/story/mob-duo-shu-ju-choice-story-mobmdctrl/mob-duo-shu-ju-choice-story-mobmdctrl.vue
@@ -2,11 +2,9 @@
 import { Component } from 'vue-property-decorator';
 import MOB_DuoShuJuChoice_StoryBase from './mob-duo-shu-ju-choice-story-mobmdctrl-base.vue';
 
-import layout_mdctrl_itempanel from '@widgets/story/mob-panel/mob-panel.vue';
  
 @Component({
     components: {
-        layout_mdctrl_itempanel,
          
     }
 })
