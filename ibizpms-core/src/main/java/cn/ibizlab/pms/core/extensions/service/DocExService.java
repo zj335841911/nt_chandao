797845e4d9ed9e033012e278405213f1472b1dd7
胡维 <huwei_0315@example.com>
2020-11-07 16:37:07 +0800

    自定义附件下载行为

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/DocExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/DocExService.java
@@ -1,8 +1,12 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.zentao.domain.DocContent;
+import cn.ibizlab.pms.core.zentao.service.IDocContentService;
 import cn.ibizlab.pms.core.zentao.service.impl.DocServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.zentao.domain.Doc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
@@ -15,6 +19,9 @@ import java.util.*;
 @Primary
 @Service("DocExService")
 public class DocExService extends DocServiceImpl {
+
+    @Autowired
+    IDocContentService docContentService;
 
     @Override
     protected Class currentModelClass() {
@@ -29,7 +36,25 @@ public class DocExService extends DocServiceImpl {
     @Override
     @Transactional
     public Doc byVersionUpdateContext(Doc et) {
+        List<DocContent> list = docContentService.list(new QueryWrapper<DocContent>().eq("doc", et.getId()).eq("version", et.getVersion()));
+        if(list.size() > 0) {
+            et.setContent(list.get(0).getContent());
+        }
         return super.byVersionUpdateContext(et);
+    }
+
+    @Override
+    @Transactional
+    public Doc get(Long key) {
+        Doc et = getById(key);
+        if(et==null){
+            et=new Doc();
+            et.setId(key);
+        }
+        else{
+            byVersionUpdateContext(et);
+        }
+        return et;
     }
 }
 
