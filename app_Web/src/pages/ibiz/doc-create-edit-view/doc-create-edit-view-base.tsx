09256efb39f6e7d3d3e7a7c2c3c975f38efc5b85
ibizdev <ibizdev@ibizsys.net>
2020-11-09 17:05:19 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/ibiz/doc-create-edit-view/doc-create-edit-view-base.tsx
+++ app_Web/src/pages/ibiz/doc-create-edit-view/doc-create-edit-view-base.tsx
@@ -66,28 +66,6 @@ export class DocCreateEditViewBase extends EditViewBase {
      */
     public appUIService: DocUIService = new DocUIService(this.$store);
 
-	/**
-	 * 自定义视图导航上下文集合
-	 *
-     * @protected
-	 * @type {*}
-	 * @memberof DocCreateEditViewBase
-	 */
-    protected customViewNavContexts: any = {
-        'DOCLIB': { isRawValue: false, value: 'lib' }
-    };
-
-	/**
-	 * 自定义视图导航参数集合
-	 *
-     * @protected
-	 * @type {*}
-	 * @memberof DocCreateEditViewBase
-	 */
-    protected customViewParams: any = {
-        'doclib': { isRawValue: false, value: 'lib' }
-    };
-
     /**
      * 是否显示信息栏
      *
