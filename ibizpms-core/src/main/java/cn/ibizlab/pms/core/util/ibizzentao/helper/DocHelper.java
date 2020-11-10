f1504cd3f95cdcbda024ba64d826d806c68aa31b
wulonghu <wulonghu@wulonghu>
2020-11-10 16:22:39 +0800

    编辑文档功能的实现

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/ibizzentao/helper/DocHelper.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/ibizzentao/helper/DocHelper.java
@@ -5,11 +5,17 @@ import cn.ibizlab.pms.core.zentao.domain.DocContent;
 import cn.ibizlab.pms.core.zentao.domain.DocLib;
 import cn.ibizlab.pms.core.zentao.mapper.DocMapper;
 import cn.ibizlab.pms.util.dict.StaticDict;
+import cn.ibizlab.pms.util.security.AuthenticationUser;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Component
 public class DocHelper extends ZTBaseHelper<DocMapper, Doc> {
@@ -45,6 +51,9 @@ public class DocHelper extends ZTBaseHelper<DocMapper, Doc> {
         et.setVersion(1);
         et.setGroups("");
         et.setUsers("");
+        if (StaticDict.Doc__acl.PRIVATE.getValue().equals(et.getAcl())) {
+            et.setUsers(et.getAddedby());
+        }
 
         DocContent docContent = new DocContent();
         if (StaticDict.Doc__type.TEXT.getValue().equals(et.getType())) {
@@ -80,14 +89,83 @@ public class DocHelper extends ZTBaseHelper<DocMapper, Doc> {
         docContent.setDoc(et.getId());
         docContentHelper.create(docContent);
 
-        fileHelper.updateObjectID(et.getId(), StaticDict.File__object_type.DOC.getValue(), files);
+        fileHelper.updateObjectID(et.getId(), StaticDict.File__object_type.DOC.getValue(), files, String.valueOf(et.getVersion()));
         actionHelper.create(StaticDict.Action__object_type.DOC.getValue(),et.getId(),StaticDict.Action__type.OPENED.getValue(), "","",null,true);
 
         return bOk;
     }
 
     @Override
+    @Transactional
     public boolean edit(Doc et) {
+        Doc oldDoc = this.get(et.getId());
+
+        if (StaticDict.Doc__acl.PRIVATE.getValue().equals(et.getAcl())) {
+            et.setUsers(oldDoc.getAddedby());
+        }
+
+        DocContent oldDocContent = docContentHelper.getOne(new QueryWrapper<DocContent>().eq("doc", et.getId()).eq("version", oldDoc.getVersion()));
+        if (oldDocContent != null) {
+            oldDoc.setTitle(oldDocContent.getTitle());
+            oldDoc.setContent(oldDocContent.getContent());
+        }
+
+        DocLib docLib = et.getZtDoclib();
+        if (docLib == null) {
+            docLib = docLibHelper.get(et.getLib());
+        }
+
+        et.setProduct(docLib.getProduct());
+        et.setProject(docLib.getProject());
+        et.setEditedby(AuthenticationUser.getAuthenticationUser().getUsername());
+        et.setEditeddate(new Timestamp(new Date().getTime()));
+
+        if (StaticDict.Doc__type.URL.getValue().equals(et.getType())) {
+            et.setContent(et.getUrl());
+        }
+
+        boolean changed = false;
+        String files = et.getFiles();
+        if (files != null) {
+            changed = true;
+        }
+        if ((oldDoc.getTitle() != null && !oldDoc.getTitle().equals(et.getTitle())) ||
+                (oldDoc.getTitle() == null && et.getTitle() != null)) {
+            changed = true;
+        }
+        if ((oldDoc.getContent() != null && !oldDoc.getContent().equals(et.getContent())) ||
+                (oldDoc.getContent() == null && et.getContent() != null)) {
+            changed = true;
+        }
+        if (changed) {
+            et.setVersion(oldDoc.getVersion() + 1);
+            DocContent docContent = new DocContent();
+            docContent.setDoc(et.getId());
+            docContent.setTitle(et.getTitle());
+            docContent.setContent(et.getContent() == null ? "" : et.getContent());
+            docContent.setVersion(et.getVersion());
+            docContent.setType(StaticDict.Doccontent__type.HTML.getValue());
+            docContent.setDigest(oldDocContent.getDigest());
+            StringBuilder filesId = new StringBuilder(oldDocContent.getFiles());
+            if (files != null) {
+                JSONArray jsonArray = JSONArray.parseArray(files);
+                for (int i = 0; i < jsonArray.size(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    cn.ibizlab.pms.core.zentao.domain.File file = new cn.ibizlab.pms.core.zentao.domain.File();
+                    if (filesId.length() > 0) {
+                        filesId.append(",");
+                    }
+                    filesId.append(jsonObject.getLongValue("id"));
+                }
+            }
+            docContent.setFiles(filesId.toString());
+            docContentHelper.create(docContent);
+        }
+
+        this.updateById(et);
+
+        fileHelper.updateObjectID(et.getId(), StaticDict.File__object_type.DOC.getValue(), files, String.valueOf(et.getVersion()));
+        actionHelper.create(StaticDict.Action__object_type.DOC.getValue(),et.getId(),StaticDict.Action__type.EDITED.getValue(), "","",null,true);
         return super.edit(et);
     }
 }
