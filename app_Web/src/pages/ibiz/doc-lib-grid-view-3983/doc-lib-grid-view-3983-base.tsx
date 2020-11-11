36635256fbfff63a70e5222b86cb3ae9b8fa9285
ibizdev <ibizdev@ibizsys.net>
2020-11-11 14:40:45 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/ibiz/doc-lib-grid-view-3983/doc-lib-grid-view-3983-base.tsx
+++ app_Web/src/pages/ibiz/doc-lib-grid-view-3983/doc-lib-grid-view-3983-base.tsx
@@ -77,7 +77,7 @@ export class DocLibGridView_3983Base extends GridViewBase {
 	 * @memberof DocLibGridView_3983Base
 	 */
     protected customViewNavContexts: any = {
-        'PROJECT': { isRawValue: false, value: 'srfparentkey' }
+        'PROJECT': { isRawValue: false, value: 'project' }
     };
 
 	/**
@@ -88,8 +88,7 @@ export class DocLibGridView_3983Base extends GridViewBase {
 	 * @memberof DocLibGridView_3983Base
 	 */
     protected customViewParams: any = {
-        'project': { isRawValue: false, value: 'srfparentkey' },
-        'objecttype': { isRawValue: true, value: 'project' }
+        'project': { isRawValue: false, value: 'project' }
     };
 
     /**
