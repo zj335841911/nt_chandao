02981278c9bb77f12dbb30b54b3d77e6914eaa1f
ibizdev <ibizdev@ibizsys.net>
2020-11-11 15:14:10 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/doc/more-recent-update-grid/more-recent-update-grid-base.tsx
+++ app_Web/src/widgets/doc/more-recent-update-grid/more-recent-update-grid-base.tsx
@@ -161,7 +161,7 @@ export class MoreRecentUpdateGridBase extends GridControlBase {
      */  
     public ActionModel: any = {
         Look: { name: 'Look',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: '', actiontarget: 'SINGLEKEY'},
-        Edit: { name: 'Edit',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: 'FILE', actiontarget: 'SINGLEKEY'},
+        Edit: { name: 'Edit',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: 'DOC', actiontarget: 'SINGLEKEY'},
         Delete: { name: 'Delete',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: 'FILE', actiontarget: 'SINGLEKEY'}
     };
 
