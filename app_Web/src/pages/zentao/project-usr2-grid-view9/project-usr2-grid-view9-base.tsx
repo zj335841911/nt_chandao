7f28ae496fc63b62505556ec159dc0e1d251698b
ibizdev <ibizdev@ibizsys.net>
2020-11-11 16:44:48 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/zentao/project-usr2-grid-view9/project-usr2-grid-view9-base.tsx
+++ app_Web/src/pages/zentao/project-usr2-grid-view9/project-usr2-grid-view9-base.tsx
@@ -286,11 +286,15 @@ export class ProjectUsr2GridView9Base extends GridView9Base {
      */
     public opendata(args: any[],fullargs?:any[],params?: any, $event?: any, xData?: any) {
         const localContext: any = null;
-        const localViewParam: any =null;
+        const localViewParam: any ={project:"%project%"};
         const data: any = {};
         let tempContext = JSON.parse(JSON.stringify(this.context));
         if(args.length >0){
             Object.assign(tempContext,args[0]);
+        }
+        if(localViewParam && Object.keys(localViewParam).length >0){
+            let _param:any = this.$util.computedNavData(args[0],this.context,this.viewparams,localViewParam);
+            Object.assign(data,_param);
         }
         const deResParameters: any[] = [];
         const parameters: any[] = [
