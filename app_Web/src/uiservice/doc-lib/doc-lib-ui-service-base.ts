0d33d401602793dad57beac53ef9125615abc178
ibizdev <ibizdev@ibizsys.net>
2020-11-13 09:37:57 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/uiservice/doc-lib/doc-lib-ui-service-base.ts
+++ app_Web/src/uiservice/doc-lib/doc-lib-ui-service-base.ts
@@ -778,6 +778,7 @@ export default class DocLibUIServiceBase extends UIService {
         let parentContext:any = {};
         let parentViewParam:any = {};
         const _this: any = actionContext;
+        Object.assign(params,{srfnavcnt.doclibtype:"%doclibtype%"});
         const _args: any[] = Util.deepCopy(args);
         const actionTarget: string | null = 'SINGLEKEY';
         Object.assign(context, { doclib: '%doclib%' });
