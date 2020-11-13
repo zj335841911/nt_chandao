982ddc57703769e0eca63009b5f606d983a47cb5
胡维 <huwei_0315@example.com>
2020-11-13 10:25:13 +0800

    接口自定义实体行为需要手动调用feign

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/IBIZProMessageExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/IBIZProMessageExService.java
@@ -1,14 +1,18 @@
 package cn.ibizlab.pms.core.extensions.service;
 
+import cn.ibizlab.pms.core.ibizplugin.client.IBIZPRO_MESSAGEFeignClient;
 import cn.ibizlab.pms.core.ibizplugin.service.impl.IBIZProMessageServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibizplugin.domain.IBIZProMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
 import java.util.*;
 
 /**
+ *
+ * @author huhai
  * 实体[消息] 自定义服务对象
  */
 @Slf4j
@@ -17,13 +21,15 @@ import java.util.*;
 public class IBIZProMessageExService extends IBIZProMessageServiceImpl {
 
 
+    @Autowired
+    IBIZPRO_MESSAGEFeignClient ibizproMessageFeignClient;
     /**
      * 自定义行为[MarkDone]用户扩展
      * @param et
      * @return
      */
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public IBIZProMessage markDone(IBIZProMessage et) {
         return super.markDone(et);
     }
@@ -33,7 +39,7 @@ public class IBIZProMessageExService extends IBIZProMessageServiceImpl {
      * @return
      */
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public IBIZProMessage markRead(IBIZProMessage et) {
         return super.markRead(et);
     }
@@ -43,9 +49,10 @@ public class IBIZProMessageExService extends IBIZProMessageServiceImpl {
      * @return
      */
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public IBIZProMessage send(IBIZProMessage et) {
-        return super.send(et);
+
+        return ibizproMessageFeignClient.send(et.getIbizproMessageid(), et);
     }
 }
 
