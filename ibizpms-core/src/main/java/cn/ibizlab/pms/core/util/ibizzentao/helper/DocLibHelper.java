128e22f4c61162baa66dca3efddfeab4741e0d1e
胡维 <huwei_0315@example.com>
2020-11-13 10:01:33 +0800

    自定义代码优化

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/ibizzentao/helper/DocLibHelper.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/ibizzentao/helper/DocLibHelper.java
@@ -15,45 +15,52 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * @author huajiyu
+ */
 @Component
 public class DocLibHelper extends ZTBaseHelper<DocLibMapper, DocLib> {
 
+    @Autowired
+    ProductHelper productHelper;
+    @Autowired
+    ActionHelper actionHelper;
 
-        @Autowired
-        ProductHelper productHelper;
-        @Autowired
-        ActionHelper actionHelper;
-
-
-
-        @Override
-        @Transactional
-        public boolean create(DocLib et){
-            if (et.getType().equals("product")) et.setProject(0L);
-            if (et.getType().equals("project")) et.setProduct(0L);
-            if (et.getAcl().equals("private")) et.setUsers(AuthenticationUser.getAuthenticationUser().getUsername());
-            boolean flag =  super.create(et);
-            actionHelper.create(StaticDict.Action__object_type.DOCLIB.getValue(),et.getId(),StaticDict.Action__type.CREATED.getValue(),"","",null,true);
-            return flag;
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public boolean create(DocLib et){
+        if (StaticDict.Doclib__type.PRODUCT.getValue().equals(et.getType())) {
+            et.setProject(0L);
         }
-
-        @Transactional
-        public boolean edit(DocLib et){
-            long libId =  et.getId();
-            DocLib old = new DocLib();
-            CachedBeanCopier.copy(this.get(et.getId()),old);
-            if (et.getAcl().equals("private")){
-                String libCreatedBy = actionHelper.getOne(new QueryWrapper<Action>().eq("objectType","doclib").eq("objectID",libId).eq("action","created")).getActor();
-                et.setUsers(libCreatedBy != null ? libCreatedBy : AuthenticationUser.getAuthenticationUser().getUsername());
-            }
-            boolean flag =  this.internalUpdate(et);
-            List<History> changes = ChangeUtil.diff(old,et);
-            if (changes.size() > 0) {
-                Action action = actionHelper.create(StaticDict.Action__object_type.DOCLIB.getValue(),et.getId(),StaticDict.Action__type.EDITED.getValue(),"","",null,true);
-                actionHelper.logHistory(action.getId(),changes);
-            }
-            return flag;
+        if (StaticDict.Doclib__type.PROJECT.getValue().equals(et.getType())) {
+            et.setProduct(0L);
         }
+        if (StaticDict.Doclib__acl.CUSTOM.getValue().equals(et.getAcl())) {
+            et.setUsers(AuthenticationUser.getAuthenticationUser().getUsername());
+        }
+        boolean flag =  super.create(et);
+        actionHelper.create(StaticDict.Action__object_type.DOCLIB.getValue(),et.getId(),StaticDict.Action__type.CREATED.getValue(),"","",null,true);
+        return flag;
+    }
 
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public boolean edit(DocLib et){
+        long libId =  et.getId();
+        DocLib old = new DocLib();
+        CachedBeanCopier.copy(this.get(et.getId()),old);
+        if (StaticDict.Doclib__acl.CUSTOM.getValue().equals(et.getAcl())){
+            String libCreatedBy = actionHelper.getOne(new QueryWrapper<Action>().eq("objectType",StaticDict.Action__object_type.DOCLIB.getValue()).eq("objectID",libId).eq("action",StaticDict.Action__type.CREATED.getValue())).getActor();
+            et.setUsers(libCreatedBy != null ? libCreatedBy : AuthenticationUser.getAuthenticationUser().getUsername());
+        }
+        boolean flag =  this.internalUpdate(et);
+        List<History> changes = ChangeUtil.diff(old,et);
+        if (changes.size() > 0) {
+            Action action = actionHelper.create(StaticDict.Action__object_type.DOCLIB.getValue(),et.getId(),StaticDict.Action__type.EDITED.getValue(),"","",null,true);
+            actionHelper.logHistory(action.getId(),changes);
+        }
+        return flag;
+    }
 
 }
