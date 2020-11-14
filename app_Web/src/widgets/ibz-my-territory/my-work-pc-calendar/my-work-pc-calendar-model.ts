b467f6b575e05b126faf77ecd8586781bc515a9c
ibizdev <ibizdev@ibizsys.net>
2020-11-14 16:37:33 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/ibz-my-territory/my-work-pc-calendar/my-work-pc-calendar-model.ts
+++ app_Web/src/widgets/ibz-my-territory/my-work-pc-calendar/my-work-pc-calendar-model.ts
@@ -119,7 +119,7 @@ export default class MyWorkPCModel {
                   },
                   {
                     name:'end',
-                    prop:'begin'
+                    prop:'end'
                   },
               ];
               break;
