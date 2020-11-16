e8eed5506515b856ecf3ca3f0efc6389c35ff5f7
胡维 <huwei_0315@example.com>
2020-11-16 14:47:14 +0800

    迁移UpdateTime方法内容到EmpLoyeeload-service.ts

--- app_Web/src/service/emp-loyeeload/emp-loyeeload-service.ts
+++ app_Web/src/service/emp-loyeeload/emp-loyeeload-service.ts
@@ -22,5 +22,41 @@ export default class EmpLoyeeloadService extends EmpLoyeeloadServiceBase {
         super(opts);
     }
 
+    /**
+     * UpdateTime接口方法
+     *
+     * @param {*} [context={}]
+     * @param {*} [data={}]
+     * @param {boolean} [isloading]
+     * @returns {Promise<any>}
+     * @memberof EmployEeloadServiceBase
+     */
+    public async UpdateTime(context: any = {},data: any = {}, isloading?: boolean): Promise<any> {
+        let res: any = { status: 200, data: {} };
+
+        if(!(data && data.begin && data.end)) {
+            return res;
+        }
+        let begin: Date = new Date(data.begin);
+        let end: Date = new Date(data.end);
+        let period = Math.floor((end.getTime() - begin.getTime())/(1000 * 60 * 60 *24)) + 1;
+        let days: number = 0;
+        let curWeek: number = begin.getDay();
+        begin.setDate(begin.getDate() + period);
+        for(; period > 0; period--, curWeek++) {
+            curWeek = curWeek > 6 ? (curWeek - 7) : curWeek;
+            if(curWeek > 0 && curWeek < 6) {
+                days++;
+            }
+        }
+
+        Object.assign(res.data, {
+
+            workday: days
+        });
+
+        return res;
+    }
+
 
 }
