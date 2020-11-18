68e4bfa3059b367bf78cd8bd65f7d2311e9f39c9
Shine-zwj <1917095344@qq.com>
2020-11-18 18:20:16 +0800

    表格保存值校验错误提示

--- app_Web/src/widgets/ibztask-estimate/main-grid/main-grid.vue
+++ app_Web/src/widgets/ibztask-estimate/main-grid/main-grid.vue
@@ -1,69 +1,80 @@
 <template src="./main-grid.html" />
 <script lang="tsx">
-    import { Component } from 'vue-property-decorator';
-    import { VueLifeCycleProcessing } from '@/studio-core';
-    import { MainGridBase } from './main-grid-base';
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core';
+import { MainGridBase } from './main-grid-base';
 
 
+/**
+ * grid部件
+ *
+ * @export
+ * @class MainGrid
+ * @extends {MainGridBase}
+ */
+@Component({
+    components: {
+
+    },
+})
+@VueLifeCycleProcessing()
+export default class MainGrid extends MainGridBase {
     /**
-     * grid部件
+     * 保存
      *
-     * @export
-     * @class MainGrid
-     * @extends {MainGridBase}
+     * @param {any[]} args
+     * @param {*} [params]
+     * @param {*} [$event]
+     * @param {*} [xData]
+     * @returns
+     * @memberof Main
      */
-    @Component({
-        components: {
-
-        },
-    })
-    @VueLifeCycleProcessing()
-    export default class MainGrid extends MainGridBase {
-        /**
-         * 保存
-         *
-         * @param {any[]} args
-         * @param {*} [params]
-         * @param {*} [$event]
-         * @param {*} [xData]
-         * @returns
-         * @memberof Main
-         */
-        public async save(args: any[], params?: any, $event?: any, xData?: any) {
-            if(!this.items.some((temp:any)=>{return temp.left})){
-                this.$Notice.error({ title: '错误', desc: '请输入预计剩余！' });
-                return
+    public async save(args: any[], params?: any, $event?: any, xData?: any) {
+        if (!(await this.validateAll())) {
+            if (this.errorMessages && this.errorMessages.length > 0) {
+                this.$Notice.error({ title: this.$t('app.commonWords.wrong') as string, desc: this.errorMessages[0] });
+            } else {
+                this.$Notice.error({
+                    title: this.$t('app.commonWords.wrong') as string,
+                    desc: this.$t('app.commonWords.rulesException') as string,
+                });
             }
-            for (const item of this.items) {
-                item.srfmajortext = item.id;
-            }
-            let successItems: any = [];
-            for (const item of this.items) {
-                let result: Promise<any>;
-                const _appEntityService: any = this.appEntityService;
-                let curAction:string = "";
-                const _context: any = JSON.parse(JSON.stringify(this.context));
-                let { data: Data,context: Context } = this.service.handleRequestData(this.createAction, _context, item, true);
-                if (Object.is(item.rowDataState, 'create')) {
-                    Data.id = null;
-                    Data.task = null;
-                    curAction = this.createAction;
-                }
-                if(Object.is(item.rowDataState, 'update')){
-                    curAction = this.updateAction;
-                }
-                if(!curAction) continue;
-                if (_appEntityService[curAction] && _appEntityService[curAction] instanceof Function) {
-                    result =  _appEntityService[curAction](Context,Data, this.showBusyIndicator);
-                }else{
-                    result =  _appEntityService.Create(Context,Data, this.showBusyIndicator);
-                }
-                result.then((response) => {
-                    this.service.handleResponse(curAction, response);
-                    successItems.push(JSON.parse(JSON.stringify(response.data)));
-                })
-            }
-            this.$emit('save', successItems);
+            return [];
         }
+        if(!this.items.some((temp:any)=>{return temp.left})){
+            this.$Notice.error({ title: '错误', desc: '请输入预计剩余！' });
+            return
+        }
+        for (const item of this.items) {
+            item.srfmajortext = item.id;
+        }
+        let successItems: any = [];
+        for (const item of this.items) {
+            let result: Promise<any>;
+            const _appEntityService: any = this.appEntityService;
+            let curAction:string = "";
+            const _context: any = JSON.parse(JSON.stringify(this.context));
+            let { data: Data,context: Context } = this.service.handleRequestData(this.createAction, _context, item, true);
+            if (Object.is(item.rowDataState, 'create')) {
+                Data.id = null;
+                Data.task = null;
+                curAction = this.createAction;
+            }
+            if(Object.is(item.rowDataState, 'update')){
+                curAction = this.updateAction;
+            }
+            if(!curAction) continue;
+            if (_appEntityService[curAction] && _appEntityService[curAction] instanceof Function) {
+                result =  _appEntityService[curAction](Context,Data, this.showBusyIndicator);
+            }else{
+                result =  _appEntityService.Create(Context,Data, this.showBusyIndicator);
+            }
+            result.then((response) => {
+                this.service.handleResponse(curAction, response);
+                successItems.push(JSON.parse(JSON.stringify(response.data)));
+            })
+        }
+        this.$emit('save', successItems);
     }
+}
 </script>
