48b192e9f1a7c800aeef4753e532d49c662f5209
ibizdev <ibizdev@ibizsys.net>
2020-11-20 13:27:18 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/doc-lib/doc-lib-tree-product-treeview/doc-lib-tree-product-treeview-service.ts
+++ app_Web/src/widgets/doc-lib/doc-lib-tree-product-treeview/doc-lib-tree-product-treeview-service.ts
@@ -310,8 +310,9 @@ export default class DocLibTreeProductService extends ControlService {
         filter = this.handleResNavParams(context,filter,rsNavParams,rsParams);
         return new Promise((resolve:any,reject:any) =>{
             let searchFilter: any = {};
+
             if (Object.is(filter.strNodeType, this.TREENODE_MODULE)) {
-                Object.assign(searchFilter, { n_parent_eq: filter.nodeid });
+                Object.assign(searchFilter, { n_module_eq: filter.nodeid });
             }
 
             Object.assign(searchFilter, { total: false });
