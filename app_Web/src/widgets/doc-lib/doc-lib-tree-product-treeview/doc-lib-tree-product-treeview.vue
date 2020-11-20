78cd8c7af4944c98647145c09ec169fc7eae6a1a
ibizdev <ibizdev@ibizsys.net>
2020-11-20 15:57:17 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/doc-lib/doc-lib-tree-product-treeview/doc-lib-tree-product-treeview.vue
+++ app_Web/src/widgets/doc-lib/doc-lib-tree-product-treeview/doc-lib-tree-product-treeview.vue
@@ -20,6 +20,3 @@ import { DocLibTreeProductTreeBase } from './doc-lib-tree-product-treeview-base'
 @VueLifeCycleProcessing()
 export default class DocLibTreeProductTree extends DocLibTreeProductTreeBase {}
 </script>
-<style lang="less">
-@import './doc-lib-tree-product-treeview.less';
-</style>
