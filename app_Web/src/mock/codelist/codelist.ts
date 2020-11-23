e90f4b3644288c93657b1e4c9e1b6f69838ee4b4
ibizdev <ibizdev@ibizsys.net>
2020-11-23 22:18:38 +0800

    huwei_0806 部署微服务应用 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/mock/codelist/codelist.ts
+++ app_Web/src/mock/codelist/codelist.ts
@@ -1954,7 +1954,6 @@ mock.onGet('./assets/json/data-dictionary.json').reply((config: any) => {
                 id: "1",
                 label: "1",
                 text: "1",
-                "iconcls":"fa fa-square-o",
                 "data":"",
                 "codename":"Item_1",
                 value: 1,
