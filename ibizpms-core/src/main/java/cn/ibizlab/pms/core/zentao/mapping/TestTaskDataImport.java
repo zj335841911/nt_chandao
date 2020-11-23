f5f37d7091096a34ffbc3b8e595ab80360c6ea7b
ibizdev <ibizdev@ibizsys.net>
2020-11-23 19:25:45 +0800

    tony001 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
@@ -8,15 +8,6 @@ import java.util.List;
 public interface TestTaskDataImport {
     @Mappings({
         @Mapping(target = "id", source = "id"),
-        @Mapping(target = "report", source = "report"),
-        @Mapping(target = "product", source = "product"),
-        @Mapping(target = "auto", source = "auto"),
-        @Mapping(target = "end", source = "end"),
-        @Mapping(target = "projecttname", source = "projecttname"),
-        @Mapping(target = "pri", source = "pri"),
-        @Mapping(target = "status", source = "status"),
-        @Mapping(target = "build", source = "build"),
-        @Mapping(target = "name", source = "name"),
         @Mapping(target = "buildname", source = "buildname"),
         @Mapping(target = "project", source = "project"),
         @Mapping(target = "substatus", source = "substatus"),
@@ -25,8 +16,17 @@ public interface TestTaskDataImport {
         @Mapping(target = "deleted", source = "deleted"),
         @Mapping(target = "mailto", source = "mailto"),
         @Mapping(target = "owner", source = "owner"),
-        @Mapping(target = "comment", source = "comment"),
         @Mapping(target = "productname", source = "productname"),
+        @Mapping(target = "report", source = "report"),
+        @Mapping(target = "comment", source = "comment"),
+        @Mapping(target = "end", source = "end"),
+        @Mapping(target = "product", source = "product"),
+        @Mapping(target = "pri", source = "pri"),
+        @Mapping(target = "auto", source = "auto"),
+        @Mapping(target = "status", source = "status"),
+        @Mapping(target = "projecttname", source = "projecttname"),
+        @Mapping(target = "build", source = "build"),
+        @Mapping(target = "name", source = "name"),
     })
     @BeanMapping(ignoreByDefault = true)
     TestTask toDomain(TestTask entity);
