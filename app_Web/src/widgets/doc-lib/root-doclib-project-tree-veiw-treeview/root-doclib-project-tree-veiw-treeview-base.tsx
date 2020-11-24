fca6792b58906a6386557d42f776d9a9f914151e
ibizdev <ibizdev@ibizsys.net>
2020-11-24 17:30:16 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/doc-lib/root-doclib-project-tree-veiw-treeview/root-doclib-project-tree-veiw-treeview-base.tsx
+++ app_Web/src/widgets/doc-lib/root-doclib-project-tree-veiw-treeview/root-doclib-project-tree-veiw-treeview-base.tsx
@@ -67,6 +67,25 @@ export class RootDoclibProjectTreeVeiwTreeBase extends MainControlBase {
     public appUIService: DocLibUIService = new DocLibUIService(this.$store);
 
     /**
+     * doclibchildmodule_cm 部件 click 事件
+     *
+     * @param {*} [args={}]
+     * @param {*} $event
+     * @memberof RootDoclibProjectTreeVeiwTreeBase
+     */
+    public doclibchildmodule_cm_click($event: any, $event2?: any) {
+        if (Object.is($event.tag, 'deuiaction1')) {
+            this.doclibchildmodule_cm_deuiaction1_click(null, 'doclibchildmodule_cm', $event2);
+        }
+        if (Object.is($event.tag, 'deuiaction2')) {
+            this.doclibchildmodule_cm_deuiaction2_click(null, 'doclibchildmodule_cm', $event2);
+        }
+        if (Object.is($event.tag, 'deuiaction3')) {
+            this.doclibchildmodule_cm_deuiaction3_click(null, 'doclibchildmodule_cm', $event2);
+        }
+    }
+
+    /**
      * rootdoc_cm 部件 click 事件
      *
      * @param {*} [args={}]
@@ -152,6 +171,93 @@ export class RootDoclibProjectTreeVeiwTreeBase extends MainControlBase {
      * @memberof 
      */
     public rootdoc_cm_deuiaction3_click(params: any = {}, tag?: any, $event?: any) {
+        // 参数
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:DocUIService  = new DocUIService();
+        curUIService.Doc_UnCollect(datas,contextJO, paramJO,  $event, xData,this,"DocLib");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public doclibchildmodule_cm_deuiaction1_click(params: any = {}, tag?: any, $event?: any) {
+        // 参数
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:DocUIService  = new DocUIService();
+        curUIService.Doc_Edit(datas,contextJO, paramJO,  $event, xData,this,"DocLib");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public doclibchildmodule_cm_deuiaction2_click(params: any = {}, tag?: any, $event?: any) {
+        // 参数
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this;
+        if (_this.getDatas && _this.getDatas instanceof Function) {
+            datas = [..._this.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        const curUIService:DocUIService  = new DocUIService();
+        curUIService.Doc_Collect(datas,contextJO, paramJO,  $event, xData,this,"DocLib");
+    }
+
+    /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public doclibchildmodule_cm_deuiaction3_click(params: any = {}, tag?: any, $event?: any) {
         // 参数
         // 取数
         let datas: any[] = [];
@@ -288,6 +394,9 @@ export class RootDoclibProjectTreeVeiwTreeBase extends MainControlBase {
      * @memberof RootDoclibProjectTreeVeiwBase
      */
      public actionModel: any = {
+        DoclibChildModule_deuiaction1: {name:'deuiaction1',nodeOwner:'DoclibChildModule',type: 'DEUIACTION', tag: 'Edit', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'DOC', visible: true, disabled: false},
+        DoclibChildModule_deuiaction2: {name:'deuiaction2',nodeOwner:'DoclibChildModule',type: 'DEUIACTION', tag: 'Collect', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'SRFUR__DOC_FAVOUR_BUT', visible: true, disabled: false},
+        DoclibChildModule_deuiaction3: {name:'deuiaction3',nodeOwner:'DoclibChildModule',type: 'DEUIACTION', tag: 'UnCollect', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'SRFUR__DOC_NFAVOUR_BUT', visible: true, disabled: false},
         rootdoc_deuiaction1: {name:'deuiaction1',nodeOwner:'rootdoc',type: 'DEUIACTION', tag: 'Edit', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'DOC', visible: true, disabled: false},
         rootdoc_deuiaction2: {name:'deuiaction2',nodeOwner:'rootdoc',type: 'DEUIACTION', tag: 'Collect', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'SRFUR__DOC_FAVOUR_BUT', visible: true, disabled: false},
         rootdoc_deuiaction3: {name:'deuiaction3',nodeOwner:'rootdoc',type: 'DEUIACTION', tag: 'UnCollect', actiontarget: 'SINGLEKEY', noprivdisplaymode:2, dataaccaction:'SRFUR__DOC_NFAVOUR_BUT', visible: true, disabled: false},
@@ -556,6 +665,9 @@ export class RootDoclibProjectTreeVeiwTreeBase extends MainControlBase {
     public onAction(item: any,tag: string) {
         let _this:any = this;
         this.currentselectedNode = JSON.parse(JSON.stringify(item));
+        if (_this.doclibchildmodule_cm_click && _this.doclibchildmodule_cm_click instanceof Function) {
+            _this.doclibchildmodule_cm_click({ tag: tag });
+        }           
         if (_this.rootdoc_cm_click && _this.rootdoc_cm_click instanceof Function) {
             _this.rootdoc_cm_click({ tag: tag });
         }           
