4aea13c81acdeb34a0f9cb7eb10fb7d6cd0fd52c
ibizdev <ibizdev@ibizsys.net>
2020-11-24 13:47:08 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/uiservice/doc-lib-module/doc-lib-module-ui-service-base.ts
+++ app_Web/src/uiservice/doc-lib-module/doc-lib-module-ui-service-base.ts
@@ -265,14 +265,24 @@ export default class DocLibModuleUIServiceBase extends UIService {
         let deResParameters: any[] = [];
         const parameters: any[] = [
             { pathName: 'doclibmodules', parameterName: 'doclibmodule' },
-            { pathName: 'editviewmain', parameterName: 'editviewmain' },
         ];
-        const openIndexViewTab = (data: any) => {
-            const routePath = actionContext.$viewTool.buildUpRoutePath(actionContext.$route, context, deResParameters, parameters, _args, data);
-            actionContext.$router.push(routePath);
-            return null;
-        }
-        openIndexViewTab(data);
+            const openPopupModal = (view: any, data: any) => {
+                let container: Subject<any> = actionContext.$appmodal.openModal(view, context, data);
+                container.subscribe((result: any) => {
+                    if (!result || !Object.is(result.ret, 'OK')) {
+                        return;
+                    }
+                    const _this: any = actionContext;
+                    return result.datas;
+                });
+            }
+            const view: any = {
+                viewname: 'doc-lib-module-edit-view-main', 
+                height: 600, 
+                width: 800,  
+                title: actionContext.$t('entities.doclibmodule.views.editviewmain.title'),
+            };
+            openPopupModal(view, data);
     }
 
     /**
