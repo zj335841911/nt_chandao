577a9b5088502ae5bb5a53b8b925ea0ff86eb3c1
ibizdev <ibizdev@ibizsys.net>
2020-11-25 11:03:09 +0800

    Mosher 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/studio-core/view/KanBanViewBase.tsx
+++ app_Web/src/studio-core/view/KanBanViewBase.tsx
@@ -7,4 +7,18 @@ import { MDViewBase } from './MDViewBase';
  * @class KanBanViewBase
  * @extends {MDViewBase}
  */
-export class KanBanViewBase extends MDViewBase {}
+export class KanBanViewBase extends MDViewBase {
+
+    /**
+     * 快速搜索
+     *
+     * @protected
+     * @memberof KanBanViewBase
+     */
+    protected onSearch(): void {
+        const kanban: any = this.$refs.kanban;
+        if (kanban) {
+            kanban.load(this.context, true);
+        }
+    }
+}
