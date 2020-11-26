176c14cda52a9cd7fbc9575d2c1bc46d4091095d
ibizdev <ibizdev@ibizsys.net>
2020-11-26 10:21:59 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/bug/main-grid/main-grid-model.ts
+++ app_Web/src/widgets/bug/main-grid/main-grid-model.ts
@@ -231,6 +231,11 @@ export default class MainModel {
           dataType: 'PICKUP',
         },
         {
+          name: 'color',
+          prop: 'color',
+          dataType: 'SSCODELIST',
+        },
+        {
           name: 'entry',
           prop: 'entry',
           dataType: 'PICKUP',
