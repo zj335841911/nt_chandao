d1b0d300d297ce2344ec8a83830d351f4264ca7d
ibizdev <ibizdev@ibizsys.net>
2020-11-28 19:08:48 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/project/project-status-bar-mob-chart/project-status-bar-mob-chart-base.vue
+++ app_Mob/src/widgets/project/project-status-bar-mob-chart/project-status-bar-mob-chart-base.vue
@@ -1,4 +1,4 @@
 !!!!模版产生代码错误:----
 FTL stack trace ("~" means nesting-related):
-	- Failed at: #if ctrl.getPSSysPFPlugin().getCode3?...  [in template "TEMPLCODE_en_US" at line 55, column 9]
+	- Failed at: #if ctrl.getPSSysPFPlugin().getCode3()??  [in template "TEMPLCODE_en_US" at line 55, column 9]
 ----
