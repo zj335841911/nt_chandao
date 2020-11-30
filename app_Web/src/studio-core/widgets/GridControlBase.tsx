64d27ad9b5a14727e0c8b42f9a5d75cd67417afb
ibizdev <ibizdev@ibizsys.net>
2020-11-30 16:43:42 +0800

    tony001 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/studio-core/widgets/GridControlBase.tsx
+++ app_Web/src/studio-core/widgets/GridControlBase.tsx
@@ -1586,7 +1586,7 @@ export class GridControlBase extends MDControlBase {
                 return item.name === name;
             });
             if (curColumn.hasOwnProperty('enableCond')) {
-                return (data.rowDataState && Object.is(data.rowDataState, 'create')) ? (curColumn.enableCond & 1) !== 1 : (curColumn.enableCond & 2) !== 2;
+                return data.srfuf == 1 ? (curColumn.enableCond & 2) !== 2 : (curColumn.enableCond & 1) !== 1;
             } else {
                 return false;
             }
