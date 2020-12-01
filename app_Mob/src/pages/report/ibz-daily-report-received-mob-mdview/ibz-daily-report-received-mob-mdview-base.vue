88e5be7ab0003a4d7a3f3810e14f97644307146b
ibizdev <ibizdev@ibizsys.net>
2020-12-01 16:57:52 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/pages/report/ibz-daily-report-received-mob-mdview/ibz-daily-report-received-mob-mdview-base.vue
+++ app_Mob/src/pages/report/ibz-daily-report-received-mob-mdview/ibz-daily-report-received-mob-mdview-base.vue
@@ -35,7 +35,7 @@
             loaddraftAction=""
             loadAction="Get"
             createAction="Create"
-            fetchAction="FetchDefault" 
+            fetchAction="FetchMyDaily" 
             :isMutli="!isSingleSelect"
             :isNeedLoaddingText="!isPortalView"
             :showBusyIndicator="true" 
