7cc670d4bb05ede3ec80d831092832e262550369
ibizdev <ibizdev@ibizsys.net>
2020-12-01 08:45:12 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/job/CreateReportUserDailyServiceJobHandler.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/job/CreateReportUserDailyServiceJobHandler.java
@@ -12,9 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @Component("CreateReportUserDailyServiceJobHandler")
 public class CreateReportUserDailyServiceJobHandler implements IJobsHandler {
 
+    @Autowired
+    @Lazy
+    protected cn.ibizlab.pms.core.report.service.IIbzDailyService ibzdailyService;
 
     @Override
     public JobsResponse execute(String tenantId, String param) throws JobsException {
+        cn.ibizlab.pms.core.report.domain.IbzDaily entity=new cn.ibizlab.pms.core.report.domain.IbzDaily();
+        entity.set("tenantid",tenantId);
+        entity.set("param",param);
+        ibzdailyService.createUserDaily(entity);
         log.info("执行 DemoJobHandler tenantId=" + tenantId + ",param=" + param);
         return JobsResponse.ok();
     }
