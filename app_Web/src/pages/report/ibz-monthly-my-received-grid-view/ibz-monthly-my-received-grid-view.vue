44338cb5cf0508958903adebcceb6dec195c0e57
ibizdev <ibizdev@ibizsys.net>
2020-12-01 11:39:39 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ app_Web/src/pages/report/ibz-monthly-my-received-grid-view/ibz-monthly-my-received-grid-view.vue
@@ -0,0 +1,22 @@
+<template src="./ibz-monthly-my-received-grid-view.html" />
+<script lang="tsx">
+import { Component } from 'vue-property-decorator';
+import { VueLifeCycleProcessing } from '@/studio-core';
+import { IbzMonthlyMyReceivedGridViewBase } from './ibz-monthly-my-received-grid-view-base';
+import view_grid from '@widgets/ibz-monthly/my-received-grid/my-received-grid.vue';
+
+/**
+ * 月报表格视图视图
+ *
+ * @export
+ * @class IbzMonthlyMyReceivedGridView
+ * @extends {IbzMonthlyMyReceivedGridViewBase}
+ */
+@Component({
+    components: {
+        view_grid, 
+    }
+})
+@VueLifeCycleProcessing()
+export default class IbzMonthlyMyReceivedGridView extends IbzMonthlyMyReceivedGridViewBase {}
+</script>
