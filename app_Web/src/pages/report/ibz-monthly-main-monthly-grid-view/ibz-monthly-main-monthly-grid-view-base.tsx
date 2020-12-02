edc97749b7cef8b258244f838efcbf3d5f2ef3a9
ibizdev <ibizdev@ibizsys.net>
2020-12-02 11:37:38 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/report/ibz-monthly-main-monthly-grid-view/ibz-monthly-main-monthly-grid-view-base.tsx
+++ app_Web/src/pages/report/ibz-monthly-main-monthly-grid-view/ibz-monthly-main-monthly-grid-view-base.tsx
@@ -69,6 +69,20 @@ export class IbzMonthlyMainMonthlyGridViewBase extends GridViewBase {
      */
     public appUIService: IbzMonthlyUIService = new IbzMonthlyUIService(this.$store);
 
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof IbzMonthlyMainMonthlyGridViewBase
+	 */
+    protected customViewParams: any = {
+        'n_account_eq': {
+            isRawValue: false,
+            value: 'srfloginname',
+        }
+    };
+
     /**
      * 视图模型数据
      *
