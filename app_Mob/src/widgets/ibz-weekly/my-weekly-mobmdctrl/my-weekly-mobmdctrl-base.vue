2b7c4c8cf172411c152cb1936f1e76024226b709
ibizdev <ibizdev@ibizsys.net>
2020-12-03 09:03:05 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/ibz-weekly/my-weekly-mobmdctrl/my-weekly-mobmdctrl-base.vue
+++ app_Mob/src/widgets/ibz-weekly/my-weekly-mobmdctrl/my-weekly-mobmdctrl-base.vue
@@ -1060,7 +1060,7 @@ export default class MyWeeklyBase extends Vue implements ControlInterface {
      * @memberof MyWeeklyBase
      */  
     public ActionModel:any ={
-        mobEdit1: { name: 'mobEdit1',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: '', target: 'SINGLEKEY',icon:'',isShowCaption:true,isShowIcon:false}
+        mobEdit1: { name: 'mobEdit1',disabled: false, visabled: true,noprivdisplaymode:2,dataaccaction: '', target: 'SINGLEKEY',icon:'edit',isShowCaption:false,isShowIcon:true}
     };
 
     
