0796aab0f02ab8d498c467a3a3abc83cb10c00ba
ibizdev <ibizdev@ibizsys.net>
2020-12-04 00:01:13 +0800

    huwei_0806 部署微服务应用 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/ibiz-core/service/indexeddb-service.ts
+++ app_Mob/src/ibiz-core/service/indexeddb-service.ts
@@ -27,7 +27,7 @@ export class IndexedDBService extends IndexedDBServiceBase {
         if (IndexedDBService.instance) {
             return IndexedDBService.instance;
         }
-        super('Dec 3, 2020');
+        super('Dec 4, 2020');
     }
 
     /**
