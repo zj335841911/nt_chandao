5704edc13e30cfe73d9d527e4e9e127b93f6c98b
ibizdev <ibizdev@ibizsys.net>
2020-12-04 16:12:17 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/report/ibz-daily-daily-grid-view/ibz-daily-daily-grid-view-base.tsx
+++ app_Web/src/pages/report/ibz-daily-daily-grid-view/ibz-daily-daily-grid-view-base.tsx
@@ -126,10 +126,11 @@ export class IbzDailyDailyGridViewBase extends GridViewBase {
      * @memberof IbzDailyDailyGridView
      */
     public toolBarModels: any = {
-        deuiaction1_createuserdaily: { name: 'deuiaction1_createuserdaily', caption: '生成日报', 'isShowCaption': false, 'isShowIcon': true, tooltip: '生成日报', disabled: false, type: 'DEUIACTION', visible: true,noprivdisplaymode:2,dataaccaction: 'SRFUR__ADMIN', uiaction: { tag: 'CreateUserDaily', target: 'NONE', class: '' } },
+        deuiaction1_createuserdaily: { name: 'deuiaction1_createuserdaily', caption: '生成日报', 'isShowCaption': true, 'isShowIcon': true, tooltip: '生成日报', disabled: false, type: 'DEUIACTION', visible: true,noprivdisplaymode:2,dataaccaction: 'SRFUR__ADMIN', uiaction: { tag: 'CreateUserDaily', target: 'NONE', class: '' } },
 
-        seperator1: {  name: 'seperator1', type: 'SEPERATOR', visible: true, dataaccaction: '', uiaction: { } },
         deuiaction2_create: { name: 'deuiaction2_create', caption: '新建', 'isShowCaption': true, 'isShowIcon': true, tooltip: '新建', iconcls: 'fa fa-plus', icon: '', disabled: false, type: 'DEUIACTION', visible: true,noprivdisplaymode:2,dataaccaction: '', uiaction: { tag: 'create', target: 'NONE', class: '' } },
+
+        deuiaction7: { name: 'deuiaction7', caption: '导出', 'isShowCaption': true, 'isShowIcon': true, tooltip: '导出', iconcls: 'fa fa-file-excel-o', icon: '', disabled: false, type: 'DEUIACTION', visible: true,noprivdisplaymode:2,dataaccaction: '', uiaction: { tag: 'ExportExcel', target: '' }, MaxRowCount: 1000, class: '' },
 
     };
 
@@ -210,6 +211,9 @@ export class IbzDailyDailyGridViewBase extends GridViewBase {
         }
         if (Object.is($event.tag, 'deuiaction2_create')) {
             this.toolbar_deuiaction2_create_click(null, '', $event2);
+        }
+        if (Object.is($event.tag, 'deuiaction7')) {
+            this.toolbar_deuiaction7_click(null, '', $event2);
         }
     }
 
@@ -360,6 +364,34 @@ export class IbzDailyDailyGridViewBase extends GridViewBase {
     }
 
     /**
+     * 逻辑事件
+     *
+     * @param {*} [params={}]
+     * @param {*} [tag]
+     * @param {*} [$event]
+     * @memberof 
+     */
+    public toolbar_deuiaction7_click(params: any = {}, tag?: any, $event?: any) {
+        // 参数
+        // 取数
+        let datas: any[] = [];
+        let xData: any = null;
+        // _this 指向容器对象
+        const _this: any = this;
+        let paramJO:any = {};
+        let contextJO:any = {};
+        xData = this.$refs.grid;
+        if (xData.getDatas && xData.getDatas instanceof Function) {
+            datas = [...xData.getDatas()];
+        }
+        if(params){
+          datas = [params];
+        }
+        // 界面行为
+        this.ExportExcel(datas, contextJO,paramJO,  $event, xData,this,"IbzDaily");
+    }
+
+    /**
      * 打开新建数据视图
      *
      * @param {any[]} args
@@ -458,4 +490,22 @@ export class IbzDailyDailyGridViewBase extends GridViewBase {
     }
 
 
+    /**
+     * 导出
+     *
+     * @param {any[]} args 当前数据
+     * @param {any} contextJO 行为附加上下文
+     * @param {*} [params] 附加参数
+     * @param {*} [$event] 事件源
+     * @param {*} [xData]  执行行为所需当前部件
+     * @param {*} [actionContext]  执行行为上下文
+     * @memberof IbzDailyDailyGridViewBase
+     */
+    public ExportExcel(args: any[],contextJO?:any, params?: any, $event?: any, xData?: any,actionContext?:any,srfParentDeName?:string) {
+        const _this: any = this;
+        if (!xData || !(xData.exportExcel instanceof Function) || !$event) {
+            return ;
+        }
+        xData.exportExcel($event.exportparms);
+    }
 }
