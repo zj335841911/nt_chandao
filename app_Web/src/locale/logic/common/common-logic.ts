1ec7c0c2a15766e0f10f29a31434ce3989aaa530
Mosher <yuyang_mos@163.com>
2020-12-09 14:57:50 +0800

    调整模式切换时多语言替换逻辑 --- fix1

--- app_Web/src/locale/logic/common/common-logic.ts
+++ app_Web/src/locale/logic/common/common-logic.ts
@@ -4,27 +4,29 @@ import store from '@/store';
 const CommonLogicOwnData = {
     appcommonhandle: function(param1: any, param2: any) {
         const appData = store.getters.getAppData();
-        let srfmstatus: Array<string> = appData.settings.srfmstatus.split('_');
-        if(!srfmstatus || srfmstatus.length === 0) {
-            return param1;
-        }
-        if(Object.is(srfmstatus[0], 'project')) {
-            if(param1.search('产品') !== -1) {
-                param1 = param1.replace('产品', '项目');
-            } else {
-                switch (srfmstatus[1]) {
-                    case 'iteration': param1 = param1.replace('项目', '迭代'); break;
-                    case 'sprint': param1 = param1.replace('项目', '冲刺'); break;
-                }
+        if(appData.settings && appData.settings.srfmstatus) {
+            let srfmstatus: Array<string> = appData.settings.srfmstatus.split('_');
+            if(!srfmstatus || srfmstatus.length === 0) {
+                return param1;
             }
-        } else if(Object.is(srfmstatus[0], 'product')) {
-            if(param1.search('项目') !== -1) {
-                param1 = param1.replace('项目', '产品');
-            } else {
-                switch (srfmstatus[1]) {
-                    case 'project': param1 = param1.replace('产品', '项目'); break;
-                    case 'iteration': param1 = param1.replace('产品', '迭代'); break;
-                    case 'sprint': param1 = param1.replace('产品', '冲刺'); break;
+            if(Object.is(srfmstatus[0], 'project')) {
+                if(param1.search('产品') !== -1) {
+                    param1 = param1.replace('产品', '项目');
+                } else {
+                    switch (srfmstatus[1]) {
+                        case 'iteration': param1 = param1.replace('项目', '迭代'); break;
+                        case 'sprint': param1 = param1.replace('项目', '冲刺'); break;
+                    }
+                }
+            } else if(Object.is(srfmstatus[0], 'product')) {
+                if(param1.search('项目') !== -1) {
+                    param1 = param1.replace('项目', '产品');
+                } else {
+                    switch (srfmstatus[1]) {
+                        case 'project': param1 = param1.replace('产品', '项目'); break;
+                        case 'iteration': param1 = param1.replace('产品', '迭代'); break;
+                        case 'sprint': param1 = param1.replace('产品', '冲刺'); break;
+                    }
                 }
             }
         }
