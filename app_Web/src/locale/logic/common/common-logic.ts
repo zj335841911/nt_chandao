75e3fb9877793685e9fd790f8ef9d886e103df26
Mosher <yuyang_mos@163.com>
2020-12-10 09:10:59 +0800

    模式切换时拦截代码表项

--- app_Web/src/locale/logic/common/common-logic.ts
+++ app_Web/src/locale/logic/common/common-logic.ts
@@ -9,6 +9,13 @@ const CommonLogicOwnData = {
             if(!srfmstatus || srfmstatus.length === 0) {
                 return param1;
             }
+            if(param1.search("产品 - 项目") !== -1 || 
+                param1.search("产品 - 迭代") !== -1 || 
+                param1.search("项目 - 迭代") !== -1 || 
+                param1.search("产品 - 冲刺") !== -1 ||
+                param1.search("项目 - 冲刺") !== -1) {
+                return param1;
+            }
             if(Object.is(srfmstatus[0], 'project')) {
                 if(param1.search('产品') !== -1) {
                     param1 = param1.replace('产品', '项目');
