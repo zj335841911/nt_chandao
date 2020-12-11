d4a428e2a3bfc545f7b6c6b1bd2382177849048c
ibizdev <ibizdev@ibizsys.net>
2020-12-11 17:32:09 +0800

    tony001 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/task/main-grid/main-grid-base.tsx
+++ app_Web/src/widgets/task/main-grid/main-grid-base.tsx
@@ -1,4 +1,4 @@
 !!!!模版产生代码错误:----
 FTL stack trace ("~" means nesting-related):
-	- Failed at: ${quickToolbar.getPSControlLogics()[0...  [in template "TEMPLCODE_en_US" at line 1040, column 64]
+	- Failed at: #assign ctrlLogic = quickToolbar.getP...  [in template "TEMPLCODE_en_US" at line 1040, column 13]
 ----
