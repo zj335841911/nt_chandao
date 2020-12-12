3d6b992fbb59da36601b3432016bfdb48bc5c676
Mosher <moseternal@gmail.com>
2020-12-12 12:54:20 +0800

    重新拉取国际化逻辑

--- app_Web/src/locale/lanres/userCustom/userCustom_en_US.ts
+++ app_Web/src/locale/lanres/userCustom/userCustom_en_US.ts
@@ -2,7 +2,10 @@
 import commonLogic from '@/locale/logic/common/common-logic';
 function userCustom(){
     const data:any = {
-        
+        noCloseProduct: commonLogic.appcommonhandle("未关闭的产品", null),
+        noCloseProject: commonLogic.appcommonhandle("未关闭的项目", null),
+        allProduct: commonLogic.appcommonhandle("所有产品", null),
+        allProject: commonLogic.appcommonhandle("所有项目", null)
     }
     return data;
 }
