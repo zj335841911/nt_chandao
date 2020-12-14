2d581648509c131617a6b7d6d110ccafd4e02e92
ibizdev <ibizdev@ibizsys.net>
2020-12-14 09:48:32 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/ibiz-core/service/indexeddb-service.ts
+++ app_Mob/src/ibiz-core/service/indexeddb-service.ts
@@ -27,7 +27,7 @@ export class IndexedDBService extends IndexedDBServiceBase {
         if (IndexedDBService.instance) {
             return IndexedDBService.instance;
         }
-        super('Dec 13, 2020');
+        super('Dec 14, 2020');
     }
 
     /**
