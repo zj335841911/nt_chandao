7da8ce80ae56b2c05a3f148ce4b12d920c7d9d20
ibizdev <ibizdev@ibizsys.net>
2020-12-14 11:12:10 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/locale/lang/zh-CN-base.ts
+++ app_Mob/src/locale/lang/zh-CN-base.ts
@@ -1,8 +1,10 @@
 !!!!模版产生代码错误:----
+Tip: It's the step after the last dot that caused this error, not those before it.
+----
 Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
 ----
 
 ----
 FTL stack trace ("~" means nesting-related):
-	- Failed at: #if obj.getCapPSLanguageRes?? && obj...  [in template "TEMPLCODE_en_US" at line 57, column 26]
+	- Failed at: #elseif obj.getCaption()??  [in template "TEMPLCODE_en_US" at line 57, column 311]
 ----
