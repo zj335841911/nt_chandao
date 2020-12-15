b46c831be0ad006e9dfc3628bd0adcd309e205fb
ibizdev <ibizdev@ibizsys.net>
2020-12-15 16:18:03 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/pages/zentao/story-usr3-mob-pickup-mdview/story-usr3-mob-pickup-mdview.vue
+++ app_Mob/src/pages/zentao/story-usr3-mob-pickup-mdview/story-usr3-mob-pickup-mdview.vue
@@ -2,7 +2,7 @@
 import { Component } from 'vue-property-decorator';
 import StoryUsr3MobPickupMDViewBase from './story-usr3-mob-pickup-mdview-base.vue';
 
-import view_mdctrl from '@widgets/story/mob-mobmdctrl/mob-mobmdctrl.vue';
+import view_mdctrl from '@widgets/story/mob-project-link-story-mobmdctrl/mob-project-link-story-mobmdctrl.vue';
 @Component({
     components: {
         view_mdctrl, 
---
+++ app_Mob/src/widgets/story/mob-project-link-story-mobmdctrl/mob-project-link-story-mobmdctrl.vue
@@ -0,0 +1,14 @@
+<script lang='ts'>
+import { Component } from 'vue-property-decorator';
+import MOB_ProjectLinkStoryBase from './mob-project-link-story-mobmdctrl-base.vue';
+
+ 
+@Component({
+    components: {
+         
+    }
+})
+export default class MOB_ProjectLinkStory extends MOB_ProjectLinkStoryBase {
+
+}
+</script>
