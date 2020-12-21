3b706b3997c6c3dd8445838df5ba72c96c17b33f
ibizdev <ibizdev@ibizsys.net>
2020-12-21 16:07:18 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/widgets/bug-stats/bug-resolved-gird-grid/bug-resolved-gird-grid-base.tsx
+++ app_Web/src/widgets/bug-stats/bug-resolved-gird-grid/bug-resolved-gird-grid-base.tsx
@@ -95,6 +95,22 @@ export class BugResolvedGirdGridBase extends GridControlBase {
     protected localStorageTag: string = 'ibz_bugstats_bugresolvedgird_grid';
 
     /**
+     * 是否支持分页
+     *
+     * @type {boolean}
+     * @memberof BugResolvedGirdGridBase
+     */
+    public isEnablePagingBar: boolean = false;
+
+    /**
+     * 分页条数
+     *
+     * @type {number}
+     * @memberof BugResolvedGirdGridBase
+     */
+    public limit: number = 500;
+
+    /**
      * 所有列成员
      *
      * @type {any[]}
