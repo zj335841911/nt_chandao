3f7b1fb1080fa13d51ca44a577081c3bc0c51a44
wulonghu <wulonghu@wulonghu>
2020-12-21 18:39:38 +0800

    测试用例统计中对用例通过率的合计计算

--- app_Web/src/widgets/case-stats/test-case-stats-grid/test-case-stats-grid.vue
+++ app_Web/src/widgets/case-stats/test-case-stats-grid/test-case-stats-grid.vue
@@ -18,5 +18,94 @@ import { TestCaseStatsGridBase } from './test-case-stats-grid-base';
     },
 })
 @VueLifeCycleProcessing()
-export default class TestCaseStatsGrid extends TestCaseStatsGridBase {}
+export default class TestCaseStatsGrid extends TestCaseStatsGridBase {
+    /**
+     * 合计行绘制
+     *
+     * @param {any} param
+     * @memberof TestCaseStatsGridBase
+     */
+    public getSummaries(param:any){
+        const { columns, data } = param;
+        const sums:Array<any> = [];
+        columns.forEach((column:any, index:number) => {
+            if (index === 0) {
+                sums[index] = (this.$t('app.gridpage.sum') as string);
+                return;
+            }
+            if(index === (columns.length - 1)){
+                sums[index] = '';
+                return;
+            }
+            const values = data.map((item:any) => Number(item[column.property]));
+            if (!values.every((value:any) => isNaN(value))) {
+                if(Object.is(column.property,'totalcase')){
+                    let tempData = values.reduce((prev:any, curr:any) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    sums[index] = tempData;
+                }
+                if(Object.is(column.property,'passcase')){
+                    let tempData = values.reduce((prev:any, curr:any) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    sums[index] = tempData;
+                }
+                if(Object.is(column.property,'failcase')){
+                    let tempData = values.reduce((prev:any, curr:any) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    sums[index] = tempData;
+                }
+                if(Object.is(column.property,'blockedcase')){
+                    let tempData = values.reduce((prev:any, curr:any) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    sums[index] = tempData;
+                }
+                if(Object.is(column.property,'totalruncase')){
+                    let tempData = values.reduce((prev:any, curr:any) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    sums[index] = tempData;
+                }
+            } else {
+                sums[index] = 'N/A';
+            }
+        });
+        if(sums && sums[5]){
+            let sumss = ((sums[2] / sums[5]) * 100).toFixed(2);
+            sumss += "%";
+            sums[6] = sumss;
+        } else {
+          sums[6] = 'N/A';
+        }
+        return sums;
+    }
+}
 </script>
