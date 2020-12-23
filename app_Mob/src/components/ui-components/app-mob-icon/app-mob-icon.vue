a215517cd866e85c6734a5bbad4bdb0a03e3d6d3
ibizdev <ibizdev@ibizsys.net>
2020-12-23 15:08:19 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

---
+++ app_Mob/src/components/ui-components/app-mob-icon/app-mob-icon.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app-mob-icon">
+        <ion-icon :name="name" :size="size" :slot="slotP" @click.stop="onClick" ></ion-icon>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import { Subject } from 'rxjs';
+import { Http } from "@/ibiz-core/utils";
+@Component({})
+export default class AppMobIcon extends Vue {
+    /**
+     * name属性
+     * 
+     * @type {*}
+     * @memberof AppMobIcon
+     */
+    @Prop() public name: any;
+
+    /**
+     * slot
+     * 
+     * @type {*}
+     * @memberof AppMobIcon
+     */
+    @Prop() public slotP: any;
+
+    /**
+     * size
+     * 
+     * @type {*}
+     * @memberof AppMobIcon
+     */
+    @Prop() public size: any;
+
+    /**
+     * 点击事件
+     * 
+     * @memberof AppMobIcon
+     */
+    public onClick(){
+        this.$emit('onClick');
+    }   
+}
+</script>
+<style lang='less'>
+@import './app-mob-icon.less';
+</style>
