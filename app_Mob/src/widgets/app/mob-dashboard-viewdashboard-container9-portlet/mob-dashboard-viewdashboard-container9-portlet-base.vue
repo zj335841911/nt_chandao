e5dbe493374357304876cf2f5ea238754d3944d2
ibizdev <ibizdev@ibizsys.net>
2020-12-25 16:31:58 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/app/mob-dashboard-viewdashboard-container9-portlet/mob-dashboard-viewdashboard-container9-portlet-base.vue
+++ app_Mob/src/widgets/app/mob-dashboard-viewdashboard-container9-portlet/mob-dashboard-viewdashboard-container9-portlet-base.vue
@@ -3,7 +3,7 @@
         <ion-list class='app-mob-portlet ibzmyterritory-dashboard_container9 '>
             <ion-list-header v-if="editTitle"  class='app-mob-portlet__header'>
                 <ion-input v-if="isEditTitle" :value="editTitle" @ionChange="titleChange"></ion-input>
-                <span v-if="!isEditTitle"><span v-if="customizeTitle">{{customizeTitle}}</span><span v-else>{{$t('app.views.${this.viewName.toLowerCase()}.mobdashboardviewdashboard_container9_portlet')}}</span></span>
+                <span v-if="!isEditTitle"><span v-if="customizeTitle">{{customizeTitle}}</span><span v-else>{{$t(`app.views.${this.viewName.toLowerCase()}.mobdashboardviewdashboard_container9_portlet`)}}</span></span>
                 <div v-if="actionBarModelData && actionBarModelData.length> 0" class="portlet__header_right">
                     <app-mob-icon v-if="!isEditTitle" name="ellipsis-horizontal-outline" @onClick="open"></app-mob-icon>
                 </div>
@@ -366,7 +366,7 @@ export default class MobDashboardViewdashboard_container9Base extends Vue implem
         if(this.customizeTitle){
             return this.customizeTitle
         }
-        return (this.$t('app.views.${this.viewName.toLowerCase()}.mobdashboardviewdashboard_container9_portlet') as string)
+        return (this.$t(`app.views.${this.viewName.toLowerCase()}.mobdashboardviewdashboard_container9_portlet`) as string)
     }
 
     /**
