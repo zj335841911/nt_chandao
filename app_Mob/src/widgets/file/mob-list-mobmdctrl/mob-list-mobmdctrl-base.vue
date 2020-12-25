02c83073c046ce201ca25e96b886a50bff186acb
ibizdev <ibizdev@ibizsys.net>
2020-12-25 14:27:47 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/widgets/file/mob-list-mobmdctrl/mob-list-mobmdctrl-base.vue
+++ app_Mob/src/widgets/file/mob-list-mobmdctrl/mob-list-mobmdctrl-base.vue
@@ -1,14 +1,7 @@
 <template>
     <div  class="app-mob-mdctrl file-mdctrl ">
         <div class="app-mob-mdctrl-mdctrl" ref="mdctrl">
-                    !!!!模版产生代码错误:----
-Tip: If the failing expression is known to be legally refer to something that's sometimes null or missing, either specify a default value like myOptionalVar!myDefault, or use <#if myOptionalVar??>when-present<#else>when-missing</#if>. (These only cover the last step of the expression; to cover the whole expression, use parenthesis: (myOptionalVar.foo)!myDefault, (myOptionalVar.foo)??
-----
-
-----
-FTL stack trace ("~" means nesting-related):
-	- Failed at: #if editor.getEditorParam("isEnableDe...  [in template "TEMPLCODE_zh_CN" at line 3, column 5]
-----
+                    <app-mob-upload-list :items="items" @delete="remove" />
              <div  v-if="items.length == 0" class="no-data">
             </div>
             <div v-show=" loadStatus && !allLoaded && isNeedLoaddingText" class="loadding" >
