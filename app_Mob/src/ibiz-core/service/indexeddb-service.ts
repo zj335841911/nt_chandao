add0cec965444d183dd13d4f03e1dc5499c1c4f6
ibizdev <ibizdev@ibizsys.net>
2020-12-25 09:21:19 +0800

    KK951314 发布系统代码 [后台服务体系，iBiz软件生产管理（Mob）]

--- app_Mob/src/ibiz-core/service/indexeddb-service.ts
+++ app_Mob/src/ibiz-core/service/indexeddb-service.ts
@@ -27,7 +27,7 @@ export class IndexedDBService extends IndexedDBServiceBase {
         if (IndexedDBService.instance) {
             return IndexedDBService.instance;
         }
-        super('2020-12-24');
+        super('2020-12-25');
     }
 
     /**
