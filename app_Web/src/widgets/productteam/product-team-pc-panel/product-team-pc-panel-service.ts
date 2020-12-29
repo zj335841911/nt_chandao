71de72737157ef565e85bc0fc51c10ce52169bdd
ibizdev <ibizdev@ibizsys.net>
2020-12-29 15:13:48 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ app_Web/src/widgets/productteam/product-team-pc-panel/product-team-pc-panel-service.ts
@@ -0,0 +1,11 @@
+import { Http } from '@/utils';
+import ControlService from '@/widgets/control-service';
+
+/**
+ * ProductTeamPc 部件服务对象
+ *
+ * @export
+ * @class ProductTeamPcService
+ */
+export default class ProductTeamPcService extends ControlService {
+}
