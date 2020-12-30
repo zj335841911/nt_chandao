703fa305dc8078bdf99a68eb9a454b1c4ab73845
胡维 <huwei_0315@example.com>
2020-12-30 09:18:17 +0800

    自定生成项目日报

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/IbizproProjectDailyExService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/extensions/service/IbizproProjectDailyExService.java
@@ -1,11 +1,23 @@
 package cn.ibizlab.pms.core.extensions.service;
 
 import cn.ibizlab.pms.core.ibizpro.service.impl.IbizproProjectDailyServiceImpl;
+import cn.ibizlab.pms.core.zentao.domain.Project;
+import cn.ibizlab.pms.core.zentao.domain.Task;
+import cn.ibizlab.pms.core.zentao.domain.TaskEstimate;
+import cn.ibizlab.pms.core.zentao.service.IProjectService;
+import cn.ibizlab.pms.core.zentao.service.ITaskEstimateService;
+import cn.ibizlab.pms.core.zentao.service.ITaskService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import cn.ibizlab.pms.core.ibizpro.domain.IbizproProjectDaily;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.context.annotation.Primary;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -15,6 +27,15 @@ import java.util.*;
 @Primary
 @Service("IbizproProjectDailyExService")
 public class IbizproProjectDailyExService extends IbizproProjectDailyServiceImpl {
+
+    @Autowired
+    IProjectService iProjectService;
+
+    @Autowired
+    ITaskEstimateService iTaskEstimateService;
+
+    @Autowired
+    ITaskService iTaskService;
 
     @Override
     protected Class currentModelClass() {
@@ -29,7 +50,82 @@ public class IbizproProjectDailyExService extends IbizproProjectDailyServiceImpl
     @Override
     @Transactional
     public IbizproProjectDaily sumProjectDaily(IbizproProjectDaily et) {
-        return super.sumProjectDaily(et);
+        // 计算上一天的日报
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        calendar.getTime();
+        Date date = calendar.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = dateFormat.format(date);
+        List<Project> projectList =  iProjectService.list(new QueryWrapper<Project>().last(String.format(" and EXISTS(select 1 from zt_taskestimate tt left join zt_task t2 on tt.task = t2.id where t2.project = zt_project.id and tt.date = '%1$s')", strDate)));
+        Timestamp timestamp = new Timestamp(date.getTime());
+        List<IbizproProjectDaily> ibizproProjectDailies = new ArrayList<>();
+        for(Project project : projectList) {
+            IbizproProjectDaily ibizproProjectDaily = new IbizproProjectDaily();
+            ibizproProjectDaily.setProject(project.getId());
+            ibizproProjectDaily.setDate(timestamp);
+            ibizproProjectDaily.setPm(project.getPm());
+            List<Task> taskList = getTaskList(project.getId(), strDate);
+            ibizproProjectDaily.setTasks(getCurTasks(taskList));
+            ibizproProjectDaily.setTotalestimates(getCusrTotalestimates(taskList, strDate));
+            ibizproProjectDaily.setIbizproprojectdailyname(String.format("%1$s-%2$s的项目日报", project.getName(), strDate));
+            ibizproProjectDailies.add(ibizproProjectDaily);
+        }
+        if(ibizproProjectDailies.size() > 0) {
+            this.createBatch(ibizproProjectDailies);
+        }
+        return et;
     }
+
+    /**
+     * 获取所有的任务
+     *
+     * @param project
+     * @param strDate
+     * @return
+     */
+    public List<Task> getTaskList(Long project, String strDate) {
+
+        return iTaskService.list(new QueryWrapper<Task>().eq("project", project).last(String.format(" and SUPPROREPORT = '1' and pm is not null and EXISTS(select 1 from zt_taskestimate tt where tt.task = zt_task.id and tt.date = '%1$s')", strDate)));
+    }
+
+    /**
+     * 获取任务列
+     *
+     * @param tasklists
+     * @return
+     */
+    public String getCurTasks(List<Task> tasklists) {
+        String tasks = "";
+        for(Task task : tasklists) {
+            if(!"".equals(tasks)) {
+                tasks += ",";
+            }
+            tasks += task.getId();
+
+        }
+        return tasks;
+    }
+
+    /**
+     * 获取所有工时
+     *
+     * @param tasklists
+     * @param strDate
+     * @return
+     */
+    public  Double getCusrTotalestimates(List<Task> tasklists, String strDate) {
+        Double totalestimates = 0.0d;
+        for(Task task : tasklists) {
+            List<TaskEstimate> taskEstimateList = iTaskEstimateService.list(new QueryWrapper<TaskEstimate>().eq("task", task.getId()).eq("date",strDate ));
+            for(TaskEstimate taskEstimate : taskEstimateList) {
+                totalestimates += taskEstimate.getConsumed();
+            }
+        }
+        return totalestimates;
+    }
+
+
+
 }
 
