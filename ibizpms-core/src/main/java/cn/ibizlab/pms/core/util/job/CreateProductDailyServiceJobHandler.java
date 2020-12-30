fa3b6a1d07555c70a6313e3811277672c2488845
ibizdev <ibizdev@ibizsys.net>
2020-12-30 15:19:42 +0800

    wulonghu 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/util/job/CreateProductDailyServiceJobHandler.java
@@ -0,0 +1,28 @@
+package cn.ibizlab.pms.core.util.job;
+
+import com.baomidou.jobs.api.JobsResponse;
+import com.baomidou.jobs.exception.JobsException;
+import com.baomidou.jobs.handler.IJobsHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component("CreateProductDailyServiceJobHandler")
+public class CreateProductDailyServiceJobHandler implements IJobsHandler {
+
+    @Autowired
+    @Lazy
+    protected cn.ibizlab.pms.core.ibizpro.service.IIbizproProductDailyService ibizproproductdailyService;
+
+    @Override
+    public JobsResponse execute(String tenantId, String param) throws JobsException {
+        cn.ibizlab.pms.core.ibizpro.domain.IbizproProductDaily entity=new cn.ibizlab.pms.core.ibizpro.domain.IbizproProductDaily();
+        entity.set("tenantid",tenantId);
+        entity.set("param",param);
+        ibizproproductdailyService.statsProductDaily(entity);
+        log.info("执行 DemoJobHandler tenantId=" + tenantId + ",param=" + param);
+        return JobsResponse.ok();
+    }
+}
