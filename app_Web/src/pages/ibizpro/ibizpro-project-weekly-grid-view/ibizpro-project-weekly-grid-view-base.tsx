96e8ed53e2624815fe04cd527d156b10b1aac30e
ibizdev <ibizdev@ibizsys.net>
2020-12-30 16:00:16 +0800

    csd123_123 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/ibizpro/ibizpro-project-weekly-grid-view/ibizpro-project-weekly-grid-view-base.tsx
+++ app_Web/src/pages/ibizpro/ibizpro-project-weekly-grid-view/ibizpro-project-weekly-grid-view-base.tsx
@@ -69,6 +69,20 @@ export class IbizproProjectWeeklyGridViewBase extends GridViewBase {
      */
     public appUIService: IbizproProjectWeeklyUIService = new IbizproProjectWeeklyUIService(this.$store);
 
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof IbizproProjectWeeklyGridViewBase
+	 */
+    protected customViewParams: any = {
+        'n_pm_eq': {
+            isRawValue: false,
+            value: 'srfloginname',
+        }
+    };
+
     /**
      * 视图模型数据
      *
