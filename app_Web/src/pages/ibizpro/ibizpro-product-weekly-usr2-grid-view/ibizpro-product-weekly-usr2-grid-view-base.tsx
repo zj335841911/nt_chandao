a231be6b46f94e03dd7f76da262009cfbf91758a
ibizdev <ibizdev@ibizsys.net>
2021-01-03 18:22:23 +0800

    huwei_0806 部署微服务应用 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/ibizpro/ibizpro-product-weekly-usr2-grid-view/ibizpro-product-weekly-usr2-grid-view-base.tsx
+++ app_Web/src/pages/ibizpro/ibizpro-product-weekly-usr2-grid-view/ibizpro-product-weekly-usr2-grid-view-base.tsx
@@ -69,6 +69,20 @@ export class IbizproProductWeeklyUsr2GridViewBase extends GridViewBase {
      */
     public appUIService: IbizproProductWeeklyUIService = new IbizproProductWeeklyUIService(this.$store);
 
+	/**
+	 * 自定义视图导航参数集合
+	 *
+     * @protected
+	 * @type {*}
+	 * @memberof IbizproProductWeeklyUsr2GridViewBase
+	 */
+    protected customViewParams: any = {
+        'n_po_eq': {
+            isRawValue: false,
+            value: 'srfloginname',
+        }
+    };
+
     /**
      * 视图模型数据
      *
--- app_Web/src/widgets/ibizpro-product-weekly/main-grid/main-grid-base.tsx
+++ app_Web/src/widgets/ibizpro-product-weekly/main-grid/main-grid-base.tsx
@@ -95,6 +95,22 @@ export class MainGridBase extends GridControlBase {
     protected localStorageTag: string = 'ibizpro_productweekly_main_grid';
 
     /**
+     * 排序方向
+     *
+     * @type {string}
+     * @memberof MainGridBase
+     */
+    public minorSortDir: string = 'DESC';
+
+    /**
+     * 排序字段
+     *
+     * @type {string}
+     * @memberof MainGridBase
+     */
+    public minorSortPSDEF: string = 'date';
+
+    /**
      * 所有列成员
      *
      * @type {any[]}
