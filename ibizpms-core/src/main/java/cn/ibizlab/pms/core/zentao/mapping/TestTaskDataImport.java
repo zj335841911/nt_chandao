f5380ae7218477d7d0bfb2962d0cb254c467fd30
ibizdev <ibizdev@ibizsys.net>
2021-01-13 18:44:10 +0800

    tony001 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
@@ -8,15 +8,18 @@ import java.util.List;
 public interface TestTaskDataImport {
     @Mappings({
         @Mapping(target = "id", source = "id"),
-        @Mapping(target = "owner", source = "owner"),
+        @Mapping(target = "substatus", source = "substatus"),
+        @Mapping(target = "begin", source = "begin"),
         @Mapping(target = "desc", source = "desc"),
-        @Mapping(target = "comment", source = "comment"),
+        @Mapping(target = "deleted", source = "deleted"),
+        @Mapping(target = "owner", source = "owner"),
         @Mapping(target = "mailto", source = "mailto"),
-        @Mapping(target = "product", source = "product"),
+        @Mapping(target = "comment", source = "comment"),
         @Mapping(target = "productname", source = "productname"),
         @Mapping(target = "report", source = "report"),
-        @Mapping(target = "pri", source = "pri"),
+        @Mapping(target = "product", source = "product"),
         @Mapping(target = "end", source = "end"),
+        @Mapping(target = "pri", source = "pri"),
         @Mapping(target = "status", source = "status"),
         @Mapping(target = "build", source = "build"),
         @Mapping(target = "name", source = "name"),
@@ -24,9 +27,6 @@ public interface TestTaskDataImport {
         @Mapping(target = "projecttname", source = "projecttname"),
         @Mapping(target = "buildname", source = "buildname"),
         @Mapping(target = "project", source = "project"),
-        @Mapping(target = "substatus", source = "substatus"),
-        @Mapping(target = "deleted", source = "deleted"),
-        @Mapping(target = "begin", source = "begin"),
     })
     @BeanMapping(ignoreByDefault = true)
     TestTask toDomain(TestTask entity);
