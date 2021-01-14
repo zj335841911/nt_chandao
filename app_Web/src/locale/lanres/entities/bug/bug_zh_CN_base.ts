f30bfef0b3565c6fc56393b94b25582943f9e3ea
ibizdev <ibizdev@ibizsys.net>
2021-01-14 18:15:44 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/locale/lanres/entities/bug/bug_zh_CN_base.ts
+++ app_Web/src/locale/lanres/entities/bug/bug_zh_CN_base.ts
@@ -754,6 +754,7 @@ function getLocaleResourceBase(){
 					branch: commonLogic.appcommonhandle("平台/分支",null), 
 					branchname: commonLogic.appcommonhandle("平台/分支",null), 
 					modulename1: commonLogic.appcommonhandle("模块名称",null), 
+					module: commonLogic.appcommonhandle("所属模块",null), 
 					plan: commonLogic.appcommonhandle("所属计划",null), 
 					type: commonLogic.appcommonhandle("Bug类型",null), 
 					severity: commonLogic.appcommonhandle("严重程度",null), 
