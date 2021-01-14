82393a97ab391573bf43c0b5eae07c2f49626152
ibizdev <ibizdev@ibizsys.net>
2021-01-14 18:46:19 +0800

    huajiyu 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- app_Web/src/pages/zentao/bug-grid-view/bug-grid-view-base.tsx
+++ app_Web/src/pages/zentao/bug-grid-view/bug-grid-view-base.tsx
@@ -472,12 +472,20 @@ export class BugGridViewBase extends GridViewBase {
      * @memberof BugGridView
      */
     public opendata(args: any[],fullargs?:any[],params?: any, $event?: any, xData?: any) {
-        const localContext: any = null;
-        const localViewParam: any =null;
+        const localContext: any = {MODULENAME:"%modulename%"};
+        const localViewParam: any ={modulename:"%modulename%"};
         const data: any = {};
         let tempContext = JSON.parse(JSON.stringify(this.context));
         if(args.length >0){
             Object.assign(tempContext,args[0]);
+        }
+        if(localContext && Object.keys(localContext).length >0){
+            let _context:any = this.$util.computedNavData(args[0],this.context,this.viewparams,localContext);
+            Object.assign(tempContext,_context);
+        }
+        if(localViewParam && Object.keys(localViewParam).length >0){
+            let _param:any = this.$util.computedNavData(args[0],this.context,this.viewparams,localViewParam);
+            Object.assign(data,_param);
         }
         let deResParameters: any[] = [];
         if(tempContext.product && true){
