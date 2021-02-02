cc9bd6a23eb0079cb0e3c6e573ae0eaa8ea6878a
ibizdev <ibizdev@ibizsys.net>
2021-02-02 17:12:42 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/service/IIbizproIndexService.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/ibizpro/service/IIbizproIndexService.java
@@ -18,12 +18,11 @@ import org.springframework.cache.annotation.CacheEvict;
 import cn.ibizlab.pms.core.ibizpro.domain.IbizproIndex;
 import cn.ibizlab.pms.core.ibizpro.filter.IbizproIndexSearchContext;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
  * 实体[IbizproIndex] 服务对象接口
  */
-public interface IIbizproIndexService extends IService<IbizproIndex> {
+public interface IIbizproIndexService {
 
     /**
      * 业务实体显示文本名称
@@ -49,23 +48,8 @@ public interface IIbizproIndexService extends IService<IbizproIndex> {
     Page<IbizproIndex> searchDefault(IbizproIndexSearchContext context);
     Page<IbizproIndex> searchESquery(IbizproIndexSearchContext context);
     Page<IbizproIndex> searchIndexDER(IbizproIndexSearchContext context);
-    /**
-     * 自定义查询SQL
-     * @param sql  select * from table where id =#{et.param}
-     * @param param 参数列表  param.put("param", "1");
-     * @return select * from table where id = '1'
-     */
-    List<JSONObject> select(String sql, Map param);
-    /**
-     * 自定义SQL
-     * @param sql  update table  set name ='test' where id =#{et.param}
-     * @param param 参数列表  param.put("param", "1");
-     * @return     update table  set name ='test' where id = '1'
-     */
-    boolean execute(String sql, Map param);
 
-    List<IbizproIndex> getIbizproindexByIds(List<Long> ids);
-    List<IbizproIndex> getIbizproindexByEntities(List<IbizproIndex> entities);
 }
+
 
 
