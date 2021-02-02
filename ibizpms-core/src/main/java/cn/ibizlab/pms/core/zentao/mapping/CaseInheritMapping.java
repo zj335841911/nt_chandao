4f1400e14e2dcd87e0a99cac25a007da29efe4ed
ibizdev <ibizdev@ibizsys.net>
2021-02-02 18:25:55 +0800

    huwei_0806 发布系统代码 [后台服务体系，iBiz软件生产管理]

---
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/CaseInheritMapping.java
@@ -0,0 +1,37 @@
+
+
+package cn.ibizlab.pms.core.zentao.mapping;
+
+import org.mapstruct.*;
+import cn.ibizlab.pms.core.zentao.domain.Case;
+import cn.ibizlab.pms.core.ibizpro.domain.IbizproIndex;
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface CaseInheritMapping {
+
+    @Mappings({
+        @Mapping(source ="id",target = "indexid"),
+        @Mapping(source ="title",target = "indexname"),
+        @Mapping(target ="focusNull",ignore = true),
+        @Mapping(source ="deleted",target = "deleted"),
+        @Mapping(source ="precondition",target = "indexdesc"),
+        @Mapping(source ="product",target = "product"),
+    })
+    IbizproIndex toIbizproindex(Case minorEntity);
+
+    @Mappings({
+        @Mapping(source ="indexid" ,target = "id"),
+        @Mapping(source ="indexname" ,target = "title"),
+        @Mapping(target ="focusNull",ignore = true),
+        @Mapping(source ="indexdesc",target = "precondition"),
+    })
+    Case toCase(IbizproIndex majorEntity);
+
+    List<IbizproIndex> toIbizproindex(List<Case> minorEntities);
+
+    List<Case> toCase(List<IbizproIndex> majorEntities);
+
+}
+
+
