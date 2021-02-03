2ddca17d96d990c6c7d94d6c8c83ae523156f0be
RedPing97 <1978141412@qq.com>
2021-02-03 18:14:13 +0800

    表格分组默认打开分组项

--- app_Web/src/widgets/task/task-group-grid/task-group-grid.vue
+++ app_Web/src/widgets/task/task-group-grid/task-group-grid.vue
@@ -1,4 +1,293 @@
-<template src="./task-group-grid.html" />
+<template>
+<div :class="{ 'grid': true, 'show-paging-bar': isEnablePagingBar, 'hidden-paging-bar': !isEnablePagingBar }">
+    <i-form>
+        <el-table v-if="isDisplay === true"
+            :default-sort="{ prop: minorSortPSDEF, order: Object.is(minorSortDir, 'ASC') ? 'ascending' : Object.is(minorSortDir, 'DESC') ? 'descending' : '' }"  
+            @sort-change="onSortChange($event)"  
+            :border="isDragendCol"
+            :class="true && items.length > 0 ? 'total-row-table' : ''"
+            :show-summary="true && items.length > 0"
+            :summary-method="getSummaries"
+            :span-method="arraySpanMethod.bind(_self)"
+            :tree-props="{children: 'children', hasChildren: 'children?true:false'}"
+            default-expand-all
+            row-key="groupById"
+            :highlight-current-row ="isSingleSelect"
+            :row-class-name="getRowClassName.bind(_self)"
+            :cell-class-name="getCellClassName.bind(_self)"
+            size="mini"
+            stripe
+            :height="tableHeight"
+            @row-click="rowClick($event)"  
+            @select-all="selectAll($event)"  
+            @select="select.apply(_self, arguments)"  
+            @row-class-name="onRowClassName($event)"  
+            @row-dblclick="rowDBLClick($event)"  
+            ref="multipleTable" :data="items" :show-header="!isHideHeader"
+        >
+            <template slot="empty">
+                {{$t('entities.task.taskgroup_grid.nodata')}} 
+                <span class="quick-toolbar">
+                </span>
+            </template>
+            <template v-if="!isSingleSelect">
+                <el-table-column align="center" type="selection" :width="checkboxColWidth"></el-table-column>
+            </template>
+            <template>
+            <el-table-column show-overflow-tooltip prop="group" label="分组" :min-width="80">
+                <template v-slot="{row,column,$index}">
+                    <span>{{ row.group }}</span>
+                </template>
+            </el-table-column>
+            </template>
+            <template v-if="getColumnState('status1')">
+                <el-table-column show-overflow-tooltip :prop="'status1'" :label="$t('entities.task.taskgroup_grid.columns.status1')" :width="100"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.status1')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                        <template >
+            <codelist :value="row.status1" tag="TaskStatusCK" codelistType="STATIC" ></codelist>
+                        </template>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('id')">
+                <el-table-column show-overflow-tooltip :prop="'id'" :label="$t('entities.task.taskgroup_grid.columns.id')" :width="150"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.id')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                            <span>{{row.id}}</span>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('pri')">
+                <el-table-column show-overflow-tooltip :prop="'pri'" :label="$t('entities.task.taskgroup_grid.columns.pri')" :width="80"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.pri')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                        <template >
+            <codelist :value="row.pri" tag="Task__pri" codelistType="STATIC" ></codelist>
+                        </template>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('name')">
+                <el-table-column show-overflow-tooltip :prop="'name'" :label="$t('entities.task.taskgroup_grid.columns.name')" :min-width="500"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.name')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                        <app-column-link
+                            deKeyField="task"
+                            :context="JSON.parse(JSON.stringify(context))"
+                            :viewparams="JSON.parse(JSON.stringify(viewparams))"
+                            :data="row"
+                            :linkview="{viewname: 'task-main-dashboard-view', height: 850,width: 1400,title: $t('entities.task.views.maindashboardview.title'),placement: 'DRAWER_TOP', isRedirectView: false,deResParameters: [
+            { pathName: 'projectmodules', parameterName: 'projectmodule' },
+            ]
+            ,parameters: [
+            { pathName: 'tasks', parameterName: 'task' },
+            { pathName: 'maindashboardview', parameterName: 'maindashboardview' }
+            ]}"
+                            valueitem="srfkey"
+                        >
+                            <span>{{row.name}}</span>
+                        </app-column-link>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('assignedto')">
+                <el-table-column show-overflow-tooltip :prop="'assignedto'" :label="$t('entities.task.taskgroup_grid.columns.assignedto')" :width="150"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                           <i class="fa fa-hand-o-right"></i>
+                        {{$t('entities.task.taskgroup_grid.columns.assignedto')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                        <template >
+                                <a v-show="row.AssignTask.visible" :disabled="row.AssignTask.disabled" :style="{'display': 'block'}" @click="uiAction(row, 'AssignTask', $event)">
+            <codelist :value="row.assignedto" tag="UserRealNameUnAssignTo_Gird" codelistType="DYNAMIC" renderMode="STR" valueSeparator="," textSeparator="," ></codelist>
+                                </a>
+                        </template>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('finishedby')">
+                <el-table-column show-overflow-tooltip :prop="'finishedby'" :label="$t('entities.task.taskgroup_grid.columns.finishedby')" :width="150"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.finishedby')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                        <template >
+            <codelist :value="row.finishedby" tag="UserRealName_Gird" codelistType="DYNAMIC" renderMode="STR" valueSeparator="," textSeparator="," ></codelist>
+                        </template>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('estimate')">
+                <el-table-column show-overflow-tooltip :prop="'estimate'" :label="$t('entities.task.taskgroup_grid.columns.estimate')" :width="100"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.estimate')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                            <app-format-data dataType="FLOAT" precision="0" :data="row.estimate"></app-format-data>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('consumed')">
+                <el-table-column show-overflow-tooltip :prop="'consumed'" :label="$t('entities.task.taskgroup_grid.columns.consumed')" :width="100"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.consumed')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                            <app-format-data dataType="FLOAT" precision="2" :data="row.consumed"></app-format-data>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('left')">
+                <el-table-column show-overflow-tooltip :prop="'left'" :label="$t('entities.task.taskgroup_grid.columns.left')" :width="100"  :align="'left'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.left')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                            <app-format-data dataType="FLOAT" precision="2" :data="row.left"></app-format-data>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('deadline')">
+                <el-table-column show-overflow-tooltip :prop="'deadline'" :label="$t('entities.task.taskgroup_grid.columns.deadline')" :width="120"  :align="'left'" :sortable="'custom'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.deadline')}}
+                       </span>
+                    </template>
+                    <template v-slot="{row,column,$index}">
+                            <app-format-data format="YYYY-MM-DD" :data="row.deadline"></app-format-data>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="getColumnState('uagridcolumn1')">
+                <el-table-column :column-key="'uagridcolumn1'" fixed="right" :label="$t('entities.task.taskgroup_grid.columns.uagridcolumn1')" :width="280"  :align="'center'">
+                    <template v-slot:header="{column}">
+                       <span class="column-header ">
+                        {{$t('entities.task.taskgroup_grid.columns.uagridcolumn1')}}
+                       </span>
+                    </template>
+                    <template slot-scope="scope">
+                        <div style="text-align: center;">
+                            <a v-show="scope.row.TaskToBug.visible" :disabled="scope.row.TaskToBug.disabled" @click="uiAction(scope.row, 'TaskToBug', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_tasktobug')">
+                                <i class="fa fa-bug"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.confirmStoryChange.visible" type="vertical"></divider>
+                            <a v-show="scope.row.confirmStoryChange.visible" :disabled="scope.row.confirmStoryChange.disabled" @click="uiAction(scope.row, 'confirmStoryChange', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_confirmstorychange')">
+                                <i class="fa fa-search"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.MStartTaskDash1.visible" type="vertical"></divider>
+                            <a v-show="scope.row.MStartTaskDash1.visible" :disabled="scope.row.MStartTaskDash1.disabled" @click="uiAction(scope.row, 'MStartTaskDash1', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_mstarttaskdash1')">
+                                <i class="fa fa-play-circle-o"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.CloseTask.visible" type="vertical"></divider>
+                            <a v-show="scope.row.CloseTask.visible" :disabled="scope.row.CloseTask.disabled" @click="uiAction(scope.row, 'CloseTask', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_closetask')">
+                                <i class="fa fa-power-off"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.done.visible" type="vertical"></divider>
+                            <a v-show="scope.row.done.visible" :disabled="scope.row.done.disabled" @click="uiAction(scope.row, 'done', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_done')">
+                                <i class="fa fa-check-square-o"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.MainEdit.visible" type="vertical"></divider>
+                            <a v-show="scope.row.MainEdit.visible" :disabled="scope.row.MainEdit.disabled" @click="uiAction(scope.row, 'MainEdit', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_mainedit')">
+                                <i class="fa fa-edit"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.NewSubTask.visible" type="vertical"></divider>
+                            <a v-show="scope.row.NewSubTask.visible" :disabled="scope.row.NewSubTask.disabled" @click="uiAction(scope.row, 'NewSubTask', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_newsubtask')">
+                                <i class="iconfont studio-icon-subnets"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.Copy.visible" type="vertical"></divider>
+                            <a v-show="scope.row.Copy.visible" :disabled="scope.row.Copy.disabled" @click="uiAction(scope.row, 'Copy', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.copy')">
+                                <i class="fa fa-copy"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.TaskFavorites.visible" type="vertical"></divider>
+                            <a v-show="scope.row.TaskFavorites.visible" :disabled="scope.row.TaskFavorites.disabled" @click="uiAction(scope.row, 'TaskFavorites', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_taskfavorites')">
+                                <i class="fa fa-star-o"></i>
+                                
+                            </a>
+                            <divider v-show="scope.row.TaskNFavorites.visible" type="vertical"></divider>
+                            <a v-show="scope.row.TaskNFavorites.visible" :disabled="scope.row.TaskNFavorites.disabled" @click="uiAction(scope.row, 'TaskNFavorites', $event)" :title="$t('entities.task.taskgroup_grid.uiactions.task_tasknfavorites')">
+                                <i class="fa fa-star"></i>
+                                
+                            </a>
+                        </div>
+                    </template>
+                </el-table-column>
+            </template>
+            <template v-if="adaptiveState">
+                <el-table-column></el-table-column>
+            </template>
+        </el-table>
+        <row class="grid-pagination" v-show="items.length > 0">
+            <page class="pull-right" @on-change="pageOnChange($event)" 
+                @on-page-size-change="onPageSizeChange($event)"
+                :total="totalRecord"
+                show-sizer :current="curPage" :page-size="limit"
+                :page-size-opts="[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]" show-elevator show-total>
+                <span>
+                    <span class="page-column">
+                        <poptip transfer-class-name="grid-column" transfer placement="top-start">
+                            <i-button icon="md-menu">{{$t('app.gridpage.choicecolumns')}}</i-button>
+                            <div slot="content">
+                                <template v-for="col in allColumns">
+                                    <div :key="col.name"><el-checkbox v-model="col.show" @change="onColChange()">{{$t(col.langtag)}}</el-checkbox></div>
+                                </template>
+                            </div>
+                        </poptip>
+                    </span>
+                    <span class="page-button"><i-button icon="md-refresh" :title="$t('app.gridpage.refresh')" @click="pageRefresh()"></i-button></span>&nbsp;
+                    <span>
+                        {{$t('app.gridpage.show')}}&nbsp;
+                        <span>
+                            <template v-if="items.length === 1">
+                            1
+                            </template>
+                            <template v-else>
+                                <span>{{(curPage - 1) * limit + 1}}&nbsp;-&nbsp;{{totalRecord > curPage * limit ? curPage * limit : totalRecord}}</span>
+                            </template>
+                        </span>&nbsp;
+                        {{$t('app.gridpage.records')}}，{{$t('app.gridpage.totle')}}&nbsp;{{totalRecord}}&nbsp;{{$t('app.gridpage.records')}}
+                    </span>
+                </span>
+            </page>
+        </row>
+    </i-form>
+</div>
+</template>
 <script lang="tsx">
 import { Component } from 'vue-property-decorator';
 import { VueLifeCycleProcessing } from '@/studio-core';
