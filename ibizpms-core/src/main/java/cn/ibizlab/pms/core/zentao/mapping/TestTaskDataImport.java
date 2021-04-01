76b50d4df2350003a808d25dd4e540b8d6f85581
ibizdev <ibizdev@ibizsys.net>
2021-04-01 11:23:17 +0800

    huwei_0806 部署微服务接口 [后台服务体系，应用Api]

--- ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
+++ ibizpms-core/src/main/java/cn/ibizlab/pms/core/zentao/mapping/TestTaskDataImport.java
@@ -8,7 +8,12 @@ import java.util.List;
 public interface TestTaskDataImport {
     @Mappings({
         @Mapping(target = "id", source = "id"),
-        @Mapping(target = "project", source = "project"),
+        @Mapping(target = "product", source = "product"),
+        @Mapping(target = "auto", source = "auto"),
+        @Mapping(target = "report", source = "report"),
+        @Mapping(target = "status", source = "status"),
+        @Mapping(target = "productname", source = "productname"),
+        @Mapping(target = "pri", source = "pri"),
         @Mapping(target = "end", source = "end"),
         @Mapping(target = "desc", source = "desc"),
         @Mapping(target = "mailto", source = "mailto"),
@@ -19,14 +24,9 @@ public interface TestTaskDataImport {
         @Mapping(target = "buildname", source = "buildname"),
         @Mapping(target = "projecttname", source = "projecttname"),
         @Mapping(target = "comment", source = "comment"),
-        @Mapping(target = "build", source = "build"),
-        @Mapping(target = "product", source = "product"),
-        @Mapping(target = "report", source = "report"),
-        @Mapping(target = "auto", source = "auto"),
-        @Mapping(target = "productname", source = "productname"),
-        @Mapping(target = "status", source = "status"),
         @Mapping(target = "substatus", source = "substatus"),
-        @Mapping(target = "pri", source = "pri"),
+        @Mapping(target = "build", source = "build"),
+        @Mapping(target = "project", source = "project"),
     })
     @BeanMapping(ignoreByDefault = true)
     TestTask toDomain(TestTask entity);
